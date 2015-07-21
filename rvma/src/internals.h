ab7bec258e91449b50596d969e63dbc9169ed640
Jeff Hammond <jeff.science@gmail.com>
2015-07-21 08:13:28 -0700

    refactoring and namespacing in RVMA

--- rvma/src/internals.h
+++ rvma/src/internals.h
@@ -16,3 +16,37 @@ typedef struct {
 } rvma_global_state_t;
 
 extern rvma_global_state_t RVMA_GLOBAL_STATE;
+
+typedef enum
+{
+    RVMA_MSG_INFO_TAG,
+    RVMA_MSG_FLUSH_TAG,
+    RVMA_MSG_GET_INDEX_TAG,
+    RVMA_MSG_PUT_INDEX_TAG,
+    RVMA_MSG_GET_RAW_TAG,
+    RVMA_MSG_PUT_RAW_TAG,
+    RVMA_MSG_LAST_TAG
+}
+rvma_msg_tag_e;
+
+typedef enum
+{
+    RVMA_MSG_FLUSH,
+    RVMA_MSG_GET_INDEX,
+    RVMA_MSG_PUT_INDEX,
+    RVMA_MSG_GET_RAW,
+    RVMA_MSG_PUT_RAW,
+    RVMA_MSG_CHT_EXIT,
+    RVMA_MSG_LAST
+}
+rvma_msg_type_e;
+
+typedef struct
+{
+    rvma_msg_type_e  type;
+    void *           address;
+    int              count;
+    MPI_Datatype     dt;
+}
+rvma_msg_info_t;
+
