b6ec74bfa02bf2d6c38c3eea7ac01deebad25530
spapadop <stavrosp@csail.mit.edu>
2016-10-24 18:24:55 -0400

    Minor fix on error message

--- core/src/array/array_sorted_read_state.cc
+++ core/src/array/array_sorted_read_state.cc
@@ -2687,7 +2687,7 @@ int ArraySortedReadState::send_aio_request(int aio_id) {
 
   // Send the AIO request to the clone array
   if(array_clone->aio_read(&(aio_request_[aio_id])) != TILEDB_AR_OK) {
-    // TODO: get error message: tiledb_asrs_errmsg = tiledb_ar_msg;
+    tiledb_asrs_errmsg = tiledb_ar_errmsg;
     return TILEDB_ASRS_ERR;
   }
 
