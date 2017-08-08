8d414e6e368f8461c3335101a58330f5089ea95e
Nikos Papailiou <npapa@cslab.ntua.gr>
2017-08-08 12:49:24 +0300

    hdfs integration

--- scripts/install-travis-deps.sh
+++ scripts/install-travis-deps.sh
@@ -20,7 +20,7 @@ cmake --build .
 sudo cmake --build . --target install
 
 # Install clang-format
-sudo add-apt-repository 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty main'
-wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-sudo apt-get update -qq 
-sudo apt-get install -qq -y clang-format-5.0
+#sudo add-apt-repository 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-5.0 main'
+#wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+#sudo apt-get update -qq 
+#sudo apt-get install -qq -y clang-format-5.0
