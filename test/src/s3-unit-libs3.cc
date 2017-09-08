40401bfdc62c55366782ce8107dc8546ddd591e1
Jake Bolewski <jakebolewski@gmail.com>
2017-09-08 12:04:42 -0600

    add libs3 as an externally build and linked library, modify CmakeLists.txt to build libs3 when -DUSE_S3 flag is set

---
+++ test/src/s3-unit-libs3.cc
@@ -0,0 +1,40 @@
+/**
+ * @file   s3-unit-libs3.cc
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2017 TileDB, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ * Tests for libs3
+ */
+
+#include "catch.hpp"
+#include "libs3.h"
+
+TEST_CASE("Test Setup / Teardown of libS3", "[s3]") {
+  S3Status st = S3_initialize("s3", S3_INIT_ALL, "localhost");
+  REQUIRE(st == S3StatusOK);
+  S3_deinitialize();
+}
