c7ff3c40f6348b1283faea80bb0c96e58f859c4a
Jake Bolewski <jakebolewski@gmail.com>
2017-09-26 09:49:08 -0600

    run clang-format on Travis

--- scripts/install-clangformat.sh
+++ scripts/install-clangformat.sh
@@ -2,6 +2,6 @@
 
 # Install clang-format
 sudo add-apt-repository 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-5.0 main'
-wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-get update -qq 
 sudo apt-get install -qq -y clang-format-5.0
