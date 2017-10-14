bdc422040d2e79679013908b7a7b17b8a36807b2
Jake Bolewski <jakebolewski@gmail.com>
2017-10-13 21:17:17 -0600

    clean-up install deps script
    add homebrew

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,28 +1,68 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -n "$(command -v apt-get)" ]
-then
-  sudo apt-get -y install cmake lcov zlib1g-dev libbz2-dev liblz4-dev || exit
-elif [ -n "$(command -v yum)" ]
-then
-  sudo yum -y install wget cmake zlib-devel bzip2-devel lz4-devel || exit
-fi
+die() {
+  echo "$@" 1>&2 ; popd 2>/dev/null; exit 1
+}
 
-TMPBUILD=`mktemp -d -t TILEDB_BUILD_XXXX`
+build_install_zstd() {
+  TEMP=`mktemp -d` || die "failed to create zstd tmp build dir"
+  pushd $TEMP
+  wget https://github.com/facebook/zstd/archive/v1.3.2.tar.gz
+  tar xzvf v1.3.2.tar.gz || die "failed to uncompress zstd repo"
+  cd zstd-1.3.2 && mkdir build && cd build
+  sudo make install PREFIX='/usr' || die "zstd build install failed"
+  popd && rm -rf $TEMP
+}
 
-# Install Zstandard
-cd $TMPBUILD &&
-wget https://github.com/facebook/zstd/archive/v1.0.0.tar.gz &&
-tar xf v1.0.0.tar.gz &&
-cd zstd-1.0.0 &&
-sudo make install PREFIX='/usr'
+build_install_blosc() {
+  TEMP=`mktemp -d` || die "failed to create blosc tmp build dir"
+  pushd $TEMP
+  wget https://github.com/Blosc/c-blosc/archive/v1.12.1.tar.gz
+  tar xvzf v1.12.1.tar.gz || die "failed to uncompress blosc repo"
+  cd c-blosc-1.12.1 && mkdir build && cd build
+  cmake -DCMAKE_INSTALL_PREFIX='/usr' .. || die "blosc cmake failed"
+  sudo cmake --build . --target install || die "bloc build install failed"
+  popd && rm -rf $TEMP
+}
 
-#Install Blosc
-cd $TMPBUILD &&
-git clone https://github.com/Blosc/c-blosc  &&
-cd c-blosc &&
-mkdir build &&
-cd build &&
-cmake -DCMAKE_INSTALL_PREFIX='/usr' .. &&
-cmake --build . &&
-sudo cmake --build . --target install
+install_apt_pkgs() {
+  sudo apt-get -y install g++ wget cmake \
+    zlib1g-dev libbz2-dev liblz4-dev || die "could not install apt pkg dependencies"
+}
+
+install_yum_pkgs() {
+  sudo yum -y install g++ wget cmake \
+    zlib-devel bzip2-devel lz4-devel || die "could not install yum pkg dependencies"
+}
+
+install_brew_pkgs() {
+  brew install wget cmake lzlib lz4 bzip2 zstd || die "could not install brew pkg dependencies"
+}
+
+install_deps() {
+  if [ -n "$(command -v apt-get)" ]; then
+    install_apt_pkgs 
+    # zstd is in later versions of Ubuntu
+    if [[ $(apt-cache search libzstd-dev) ]]; then
+      sudo apt-get -y install libzstd-dev
+    else
+      build_install_zstd
+    fi
+    build_install_blosc
+  elif [ -n "$(command -v yum)" ]; then
+    install_yum_pkgs
+    build_install_zstd
+    build_install_blosc
+  elif [ -n "$(command -v brew)" ]; then
+    install_brew_pkgs
+    build_install_blosc
+  else
+    die "unsupported package management system"
+  fi 
+}
+
+run() {
+  install_deps
+}
+
+run 
--- scripts/install-libhdfs3.sh
+++ scripts/install-libhdfs3.sh
@@ -4,27 +4,27 @@ die() {
   echo "$@" 1>&2 ; popd 2>/dev/null; exit 1
 }
 
-install_depends() {
-  sudo apt-get update &&
-  sudo apt-get install -qq cmake debhelper fakeroot \
+install_deps_apt() {
+  sudo apt-get install -y wget cmake debhelper fakeroot \
     libxml2-dev uuid-dev \
     protobuf-compiler libprotobuf-dev \
-    libgsasl7-dev || die "cannot install dependencies"
+    libgsasl7-dev
 }
 
 build_pkg_install() {
-  TEMP=`mktemp -d` || die
-  git clone https://github.com/Pivotal-Data-Attic/pivotalrd-libhdfs3 $TEMP/libhdfs3 || die "cannot clone libhdfs3 repo"
-  pushd $TEMP/libhdfs3
-  rm -rf build && mkdir -p build && cd build || die "cannot create build directory"
+  TEMP=`mktemp -d` || die "cannot create temporary directory"
+  pushd $TEMP
+  wget  https://github.com/Pivotal-Data-Attic/pivotalrd-libhdfs3/archive/v2.2.31.tar.gz
+  tar xzvf v2.2.31.tar.gz || die "failed to extract libhdfs3 sources"
+  mkdir -p pivotalrd-libhdfs3-2.2.31/build && cd pivotalrd-libhdfs3-2.2.31/build
   ../bootstrap || die "bootstrap failed"
-  make debian-package || die "failed make debian package"
-  sudo dpkg -i ../../libhdfs3*.deb || die "failed to install debian packages" 
+  make debian-package || die "failed to make debian package"
+  sudo dpkg -i ../../libhdfs3*.deb || die "failed to install libhdfs3 debian packages"
   popd
 }
 
 run() {
-  install_depends || die "failed to install libhdfs3 dependencies"
+  install_deps_apt || die "failed to install libhdfs3 dependencies"
   build_pkg_install || die "failed to build and install libhdfs3"
 }
 
