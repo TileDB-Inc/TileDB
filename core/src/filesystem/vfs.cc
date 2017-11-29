b9ed31bb5708e2a243262ba5879ec59a415c15a6
Nikos Papailiou <npapa@cslab.ntua.gr>
2017-11-29 12:57:04 +0200

    detailed logging

--- core/src/filesystem/vfs.cc
+++ core/src/filesystem/vfs.cc
@@ -240,7 +240,9 @@ bool VFS::is_dir(const URI& uri) const {
   }
   if (uri.is_s3()) {
 #ifdef HAVE_S3
-    return s3::is_dir(uri);
+    bool ret = s3::is_dir(uri);
+    std::cout<<ret<<std::endl;
+    return ret;
 #else
     return false;
 #endif
@@ -261,7 +263,9 @@ bool VFS::is_file(const URI& uri) const {
   }
   if (uri.is_s3()) {
 #ifdef HAVE_S3
-    return s3::is_file(uri);
+    bool ret = s3::is_file(uri);
+    std::cout<<ret<<std::endl;
+    return ret;
 #else
     return false;
 #endif
@@ -281,7 +285,7 @@ Status VFS::ls(const URI& parent, std::vector<URI>* uris) const {
 #endif
   } else if (parent.is_s3()) {
 #ifdef HAVE_S3
-    RETURN_NOT_OK(s3::ls(parent, &files));
+    RETURN_NOT_OK(s3::ls(parent, &paths));
 #else
     return Status::VFSError("TileDB was built without S3 support");
 #endif
