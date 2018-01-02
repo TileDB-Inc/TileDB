e9c02952c474300c193d8f51789a7291a110c77f
Tyler Denniston <tyler@tiledb.io>
2018-01-02 15:39:47 -0500

    URI tests

--- test/src/unit-uri.cc
+++ test/src/unit-uri.cc
@@ -4,22 +4,22 @@
 using namespace tiledb;
 
 TEST_CASE("URI: Test file URIs", "[uri]") {
-  URI uri;
-  uri = URI("noslashes");
-  REQUIRE(!uri.is_invalid());
-  CHECK(uri.is_file());
+  URI uri("file:///path");
+  CHECK(!uri.is_invalid());
+  CHECK(URI::is_file(uri.to_string()));
+  uri = URI("path");
+  CHECK(!uri.is_invalid());
+  CHECK(URI::is_file(uri.to_string()));
   uri = URI("");
-  REQUIRE(uri.is_invalid());
-  // uri = "C:\\dirname";
-  // win32_or_invalid(uri);
-  // uri = "g:\\dirname";
-  // win32_or_invalid(uri);
-  // uri = "C:\\Program Files (x86)\\TileDB\\";
-  // win32_or_invalid(uri);
-  // uri = "file:///path/to/file";
-  // REQUIRE(uri.is_posix());
-  // uri = "hdfs://sub.domain.com:8080/some/path";
-  // REQUIRE(uri.is_hdfs());
-  // uri = "s3://sub.domain.com:8080/some/path";
-  // REQUIRE(uri.is_s3());
+  CHECK(uri.is_invalid());
+  uri = URI("file://path");
+  CHECK(uri.is_invalid());
+  uri = URI("file:///path/../relative");
+  CHECK(!uri.is_invalid());
+  CHECK(URI::is_file(uri.to_string()));
+  CHECK(uri.to_string() == "file:///relative");
+  uri = URI("file:///path/.././relative/./path");
+  CHECK(!uri.is_invalid());
+  CHECK(URI::is_file(uri.to_string()));
+  CHECK(uri.to_string() == "file:///relative/path");
 }
