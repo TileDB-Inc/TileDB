9c71179980572e32694a6c6da66926de1dcbf536
Tyler Denniston <tyler@tiledb.io>
2018-03-09 18:09:07 -0500

    Add simple stats framework, only used by VFS currently.

---
+++ tiledb/sm/misc/stats.cc
@@ -0,0 +1,40 @@
+#include "tiledb/sm/misc/stats.h"
+
+namespace tiledb {
+namespace sm {
+namespace stats {
+
+Statistics all_stats;
+
+Statistics::Statistics() {
+  enabled_ = false;
+  reset();
+}
+
+void Statistics::dump(FILE* out) const {
+  fprintf(
+      out,
+      "======================= TileDB Statistics Report "
+      "=======================\n");
+  fprintf(out, "Function statistics:\n");
+  fprintf(
+      out, "%-30s%20s%22s\n", "  Function name", "# calls", "Total time (ns)");
+  fprintf(
+      out,
+      "  "
+      "----------------------------------------------------------------------"
+      "\n");
+  dump_all_counters(out);
+}
+
+bool Statistics::enabled() const {
+  return enabled_;
+}
+
+void Statistics::set_enabled(bool enabled) {
+  enabled_ = enabled;
+}
+
+}  // namespace stats
+}  // namespace sm
+}  // namespace tiledb
