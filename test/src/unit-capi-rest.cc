cdec0e6ca617effe72c097ab8d84e016207bbf8a
Seth Shelnutt <seth@tiledb.io>
2018-08-13 16:52:39 -0400

    Add global write rest test

--- test/src/unit-capi-rest.cc
+++ test/src/unit-capi-rest.cc
@@ -85,12 +85,12 @@ struct ArraySchemaRest {
   const char* DIM2_NAME = "d2";
   const tiledb_datatype_t DIM_TYPE = TILEDB_INT64;
   const char* DIM_TYPE_STR = "INT64";
-  const int64_t DIM_DOMAIN[4] = {0, 99, 20, 60};
+  const int64_t DIM_DOMAIN[4] = {0, 4, 20, 60};
   const char* DIM1_DOMAIN_STR = "[0,99]";
   const char* DIM2_DOMAIN_STR = "[20,60]";
   const uint64_t DIM_DOMAIN_SIZE = sizeof(DIM_DOMAIN) / DIM_NUM;
-  const int64_t TILE_EXTENTS[2] = {10, 5};
-  const char* DIM1_TILE_EXTENT_STR = "10";
+  const int64_t TILE_EXTENTS[2] = {5, 5};
+  const char* DIM1_TILE_EXTENT_STR = "5";
   const char* DIM2_TILE_EXTENT_STR = "5";
   const uint64_t TILE_EXTENT_SIZE = sizeof(TILE_EXTENTS) / DIM_NUM;
   const char* REST_SERVER = "http://localhost:8080";
@@ -367,6 +367,104 @@ TEST_CASE_METHOD(
   REQUIRE(data_buffer[1] == 2);
   REQUIRE(data_buffer[2] == 3);
   REQUIRE(data_buffer[3] == 4);
+
+  tiledb::rest::delete_array_schema_from_rest(
+      REST_SERVER, array_name, tiledb::sm::SerializationType::JSON);
+
+  // Clean up
+  tiledb_array_free(&array);
+  tiledb_array_schema_free(&array_schema);
+  tiledb_query_free(&query);
+}
+
+TEST_CASE_METHOD(
+    ArraySchemaRest,
+    "C API: Test query rest api global write",
+    "[capi], [rest], [json]") {
+  // Create array schema
+  tiledb_array_schema_t* array_schema = create_array_schema_simple();
+
+  std::string array_name;
+  if (supports_s3_) {
+    // Create array schema
+    array_name = "s3://tiledb-seth-test/query_rest_test";
+  } else {
+    array_name = "file:///tmp/company1/project1/query_rest_test";
+  }
+
+  // Create array
+  int rc = tiledb_array_create(ctx_, array_name.c_str(), array_schema);
+  REQUIRE(rc == TILEDB_OK);
+
+  tiledb_array_t* array;
+  rc = tiledb_array_alloc(ctx_, array_name.c_str(), &array);
+  REQUIRE(rc == TILEDB_OK);
+  rc = tiledb_array_open(ctx_, array, TILEDB_WRITE);
+  REQUIRE(rc == TILEDB_OK);
+
+  // Prepare some data for the array
+  int data[] = {1, 2, 3, 4, 5};
+  uint64_t data_size = sizeof(data);
+
+  // Create the query
+  tiledb_query_t* query;
+  rc = tiledb_query_alloc(ctx_, array, TILEDB_WRITE, &query);
+  REQUIRE(rc == TILEDB_OK);
+  rc = tiledb_query_set_layout(ctx_, query, TILEDB_GLOBAL_ORDER);
+  REQUIRE(rc == TILEDB_OK);
+
+  // Slice only rows 1, 2, 3, 4
+  int64_t subarray[] = {1, 4};
+  /*rc = tiledb_query_set_subarray(ctx_, query, subarray);
+  REQUIRE(rc == TILEDB_OK);*/
+  rc = tiledb_query_set_buffer(ctx_, query, "a1", data, &data_size);
+  REQUIRE(rc == TILEDB_OK);
+
+  // Submit query
+  rc = tiledb_query_submit(ctx_, query);
+  REQUIRE(rc == TILEDB_OK);
+
+  // Finalize query
+  rc = tiledb_query_finalize(ctx_, query);
+  REQUIRE(rc == TILEDB_OK);
+
+  // Close array
+  rc = tiledb_array_close(ctx_, array);
+  REQUIRE(rc == TILEDB_OK);
+
+  rc = tiledb_array_open(ctx_, array, TILEDB_READ);
+  REQUIRE(rc == TILEDB_OK);
+  tiledb_query_free(&query);
+
+  // Create the query
+  rc = tiledb_query_alloc(ctx_, array, TILEDB_READ, &query);
+  REQUIRE(rc == TILEDB_OK);
+  rc = tiledb_query_set_layout(ctx_, query, TILEDB_ROW_MAJOR);
+  REQUIRE(rc == TILEDB_OK);
+
+  int data_buffer[4] = {0};
+  uint64_t data_buffer_size = sizeof(data_buffer);
+  rc = tiledb_query_set_buffer(
+      ctx_, query, "a1", data_buffer, &data_buffer_size);
+  REQUIRE(rc == TILEDB_OK);
+
+  // Slice only rows 1, 2 and cols 2, 3, 4
+  rc = tiledb_query_set_subarray(ctx_, query, subarray);
+  REQUIRE(rc == TILEDB_OK);
+
+  // Submit query
+  rc = tiledb_query_submit(ctx_, query);
+  REQUIRE(rc == TILEDB_OK);
+
+  int has_results;
+  rc = tiledb_query_has_results(ctx_, query, &has_results);
+  CHECK(rc == TILEDB_OK);
+  CHECK(has_results);
+
+  REQUIRE(data_buffer[0] == 2);
+  REQUIRE(data_buffer[1] == 3);
+  REQUIRE(data_buffer[2] == 4);
+  REQUIRE(data_buffer[3] == 5);
 
   tiledb::rest::delete_array_schema_from_rest(
       REST_SERVER, array_name, tiledb::sm::SerializationType::JSON);
