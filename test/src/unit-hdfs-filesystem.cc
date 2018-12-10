596bcd25c3d60fd2d6553b34fdd3aac1a1b75387
Tyler Denniston <tyler@tiledb.io>
2018-12-10 12:32:07 -0500

    Re-enable HDFS unit test.
    This required minor updates to the test itself.

--- test/src/unit-hdfs-filesystem.cc
+++ test/src/unit-hdfs-filesystem.cc
@@ -40,35 +40,43 @@
 #include <iostream>
 
 using namespace tiledb::sm;
+using namespace tiledb::sm::hdfs;
 
 TEST_CASE("Test HDFS filesystem", "[hdfs]") {
-  hdfsFS fs;
   const Config::HDFSParams config;
+  HDFS hdfs;
 
-  Status st = hdfs::connect(fs, config);
+  Status st = hdfs.init(config);
   REQUIRE(st.ok());
 
-  if (hdfs::is_dir(fs, URI("hdfs:///tiledb_test"))) {
-    st = hdfs::remove_dir(fs, URI("hdfs:///tiledb_test"));
+  bool is_dir;
+  st = hdfs.is_dir(URI("hdfs:///tiledb_test"), &is_dir);
+  CHECK(st.ok());
+  if (is_dir) {
+    st = hdfs.remove_dir(URI("hdfs:///tiledb_test"));
     CHECK(st.ok());
   }
 
-  st = hdfs::create_dir(fs, URI("hdfs:///tiledb_test"));
+  st = hdfs.create_dir(URI("hdfs:///tiledb_test"));
   CHECK(st.ok());
 
-  CHECK(hdfs::is_dir(fs, URI("hdfs:///tiledb_test")));
+  CHECK(hdfs.is_dir(URI("hdfs:///tiledb_test"), &is_dir).ok());
+  CHECK(is_dir);
 
-  st = hdfs::create_dir(fs, URI("hdfs:///tiledb_test"));
+  st = hdfs.create_dir(URI("hdfs:///tiledb_test"));
   CHECK(!st.ok());
 
-  st = hdfs::touch(fs, URI("hdfs:///tiledb_test_file"));
-  CHECK(st.ok());
-  CHECK(hdfs::is_file(fs, URI("hdfs:///tiledb_test_file")));
-
-  st = hdfs::remove_file(fs, URI("hdfs:///tiledb_test_file"));
+  st = hdfs.touch(URI("hdfs:///tiledb_test_file"));
   CHECK(st.ok());
 
-  st = hdfs::touch(fs, URI("hdfs:///tiledb_test/tiledb_test_file"));
+  bool is_file;
+  CHECK(hdfs.is_file(URI("hdfs:///tiledb_test_file"), &is_file).ok());
+  CHECK(is_file);
+
+  st = hdfs.remove_file(URI("hdfs:///tiledb_test_file"));
+  CHECK(st.ok());
+
+  st = hdfs.touch(URI("hdfs:///tiledb_test/tiledb_test_file"));
   CHECK(st.ok());
 
   tSize buffer_size = 100000;
@@ -76,16 +84,13 @@ TEST_CASE("Test HDFS filesystem", "[hdfs]") {
   for (int i = 0; i < buffer_size; i++) {
     write_buffer[i] = 'a' + (i % 26);
   }
-  st = hdfs::write(
-      fs,
-      URI("hdfs:///tiledb_test/tiledb_test_file"),
-      write_buffer,
-      buffer_size);
+  st = hdfs.write(
+      URI("hdfs:///tiledb_test/tiledb_test_file"), write_buffer, buffer_size);
   CHECK(st.ok());
 
   auto read_buffer = new char[26];
-  st = hdfs::read(
-      fs, URI("hdfs:///tiledb_test/tiledb_test_file"), 0, read_buffer, 26);
+  st = hdfs.read(
+      URI("hdfs:///tiledb_test/tiledb_test_file"), 0, read_buffer, 26);
   CHECK(st.ok());
 
   bool allok = true;
@@ -97,8 +102,8 @@ TEST_CASE("Test HDFS filesystem", "[hdfs]") {
   }
   CHECK(allok);
 
-  st = hdfs::read(
-      fs, URI("hdfs:///tiledb_test/tiledb_test_file"), 11, read_buffer, 26);
+  st = hdfs.read(
+      URI("hdfs:///tiledb_test/tiledb_test_file"), 11, read_buffer, 26);
   CHECK(st.ok());
 
   allok = true;
@@ -111,26 +116,25 @@ TEST_CASE("Test HDFS filesystem", "[hdfs]") {
   CHECK(allok);
 
   std::vector<std::string> paths;
-  st = hdfs::ls(fs, URI("hdfs:///"), &paths);
+  st = hdfs.ls(URI("hdfs:///"), &paths);
   CHECK(st.ok());
   CHECK(paths.size() > 0);
 
   uint64_t nbytes = 0;
-  st =
-      hdfs::file_size(fs, URI("hdfs:///tiledb_test/tiledb_test_file"), &nbytes);
+  st = hdfs.file_size(URI("hdfs:///tiledb_test/tiledb_test_file"), &nbytes);
   CHECK(st.ok());
   CHECK(nbytes == buffer_size);
 
-  st = hdfs::remove_file(fs, URI("hdfs:///tiledb_test/i_dont_exist"));
+  st = hdfs.remove_file(URI("hdfs:///tiledb_test/i_dont_exist"));
   CHECK(!st.ok());
 
-  st = hdfs::remove_file(fs, URI("hdfs:///tiledb_test/tiledb_test_file"));
+  st = hdfs.remove_file(URI("hdfs:///tiledb_test/tiledb_test_file"));
   CHECK(st.ok());
 
-  st = hdfs::remove_dir(fs, URI("hdfs:///tiledb_test"));
+  st = hdfs.remove_dir(URI("hdfs:///tiledb_test"));
   CHECK(st.ok());
 
-  st = hdfs::disconnect(fs);
+  st = hdfs.disconnect();
   CHECK(st.ok());
 }
 
