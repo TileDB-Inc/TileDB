717519ad63014c5dc0d75b4800ab411399d88d59
Tyler Denniston <tyler@tiledb.io>
2019-02-04 15:30:23 -0500

    Add C++ API test for incomplete multi-subarray reads.

--- test/src/unit-cppapi-subarray.cc
+++ test/src/unit-cppapi-subarray.cc
@@ -155,3 +155,138 @@ TEST_CASE("C++ API: Test subarray", "[cppapi], [sparse], [subarray]") {
   if (vfs.is_dir(array_name))
     vfs.remove_dir(array_name);
 }
+
+TEST_CASE(
+    "C++ API: Test subarray (incomplete)",
+    "[cppapi], [sparse], [subarray], [incomplete]") {
+  const std::string array_name = "cpp_unit_array";
+  Context ctx;
+  VFS vfs(ctx);
+
+  if (vfs.is_dir(array_name))
+    vfs.remove_dir(array_name);
+
+  // Create
+  Domain domain(ctx);
+  domain.add_dimension(Dimension::create<int>(ctx, "rows", {{0, 100}}, 101))
+      .add_dimension(
+          Dimension::create<int>(ctx, "cols", {{0, 100000}}, 100001));
+  ArraySchema schema(ctx, TILEDB_SPARSE);
+  schema.set_domain(domain)
+      .set_order({{TILEDB_COL_MAJOR, TILEDB_COL_MAJOR}})
+      .set_capacity(10000);
+  schema.add_attribute(Attribute::create<char>(ctx, "a"));
+  Array::create(array_name, schema);
+
+  // Write
+  std::vector<char> data_w = {
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'};
+  std::vector<int> coords_w = {0, 12277, 0, 12771, 0, 13374, 0, 13395, 0, 13413,
+                               0, 13451, 0, 13519, 0, 13544, 0, 13689, 0, 17479,
+                               0, 17486, 1, 12277, 1, 12771, 1, 13389};
+  Array array_w(ctx, array_name, TILEDB_WRITE);
+  Query query_w(ctx, array_w);
+  query_w.set_coordinates(coords_w)
+      .set_layout(TILEDB_UNORDERED)
+      .set_buffer("a", data_w);
+  query_w.submit();
+  query_w.finalize();
+  array_w.close();
+
+  // Open array for reading
+  Array array(ctx, array_name, TILEDB_READ);
+  Query query(ctx, array);
+  query.set_layout(TILEDB_GLOBAL_ORDER);
+
+  // Set up subarray for read
+  int row_range[] = {0, 1};
+  int col_range0[] = {12277, 13499};
+  int col_range1[] = {13500, 17486};
+  Subarray subarray(ctx, array, TILEDB_UNORDERED);
+  subarray.add_range(0, row_range);
+  subarray.add_range(1, col_range0);
+  subarray.add_range(1, col_range1);
+  query.set_subarray(subarray);
+
+  // Allocate buffers large enough to hold 2 cells at a time.
+  std::vector<char> data(2, '\0');
+  std::vector<int> coords(4);
+  query.set_coordinates(coords).set_buffer("a", data);
+
+  // Submit query
+  auto st = query.submit();
+  REQUIRE(st == Query::Status::INCOMPLETE);
+  auto result_elts = query.result_buffer_elements();
+  auto result_num = result_elts[TILEDB_COORDS].second / 2;
+  REQUIRE(result_num == 2);
+  REQUIRE(data[0] == 'a');
+  REQUIRE(data[1] == 'l');
+
+  // Resubmit
+  st = query.submit();
+  REQUIRE(st == Query::Status::INCOMPLETE);
+  result_elts = query.result_buffer_elements();
+  result_num = result_elts[TILEDB_COORDS].second / 2;
+  REQUIRE(result_num == 2);
+  REQUIRE(data[0] == 'b');
+  REQUIRE(data[1] == 'm');
+
+  // Resubmit
+  st = query.submit();
+  REQUIRE(st == Query::Status::INCOMPLETE);
+  result_elts = query.result_buffer_elements();
+  result_num = result_elts[TILEDB_COORDS].second / 2;
+  REQUIRE(result_num == 1);
+  REQUIRE(data[0] == 'c');
+
+  // Resubmit
+  st = query.submit();
+  REQUIRE(st == Query::Status::INCOMPLETE);
+  result_elts = query.result_buffer_elements();
+  result_num = result_elts[TILEDB_COORDS].second / 2;
+  REQUIRE(result_num == 2);
+  REQUIRE(data[0] == 'n');
+  REQUIRE(data[1] == 'd');
+
+  // Resubmit
+  st = query.submit();
+  REQUIRE(st == Query::Status::INCOMPLETE);
+  result_elts = query.result_buffer_elements();
+  result_num = result_elts[TILEDB_COORDS].second / 2;
+  REQUIRE(result_num == 1);
+  REQUIRE(data[0] == 'e');
+
+  // Resubmit
+  st = query.submit();
+  REQUIRE(st == Query::Status::INCOMPLETE);
+  result_elts = query.result_buffer_elements();
+  result_num = result_elts[TILEDB_COORDS].second / 2;
+  REQUIRE(result_num == 1);
+  REQUIRE(data[0] == 'f');
+
+  // Resubmit
+  st = query.submit();
+  REQUIRE(st == Query::Status::INCOMPLETE);
+  result_elts = query.result_buffer_elements();
+  result_num = result_elts[TILEDB_COORDS].second / 2;
+  REQUIRE(result_num == 2);
+  REQUIRE(data[0] == 'g');
+  REQUIRE(data[1] == 'h');
+
+  // Resubmit
+  st = query.submit();
+  REQUIRE(st == Query::Status::INCOMPLETE);
+  result_elts = query.result_buffer_elements();
+  result_num = result_elts[TILEDB_COORDS].second / 2;
+  REQUIRE(result_num == 1);
+  REQUIRE(data[0] == 'i');
+
+  // Resubmit
+  st = query.submit();
+  REQUIRE(st == Query::Status::COMPLETE);
+  result_elts = query.result_buffer_elements();
+  result_num = result_elts[TILEDB_COORDS].second / 2;
+  REQUIRE(result_num == 2);
+  REQUIRE(data[0] == 'j');
+  REQUIRE(data[1] == 'k');
+}
