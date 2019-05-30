d58848d232cc464a06ebfd1fbe0390fe2d73fdd3
Seth Shelnutt <seth@tiledb.io>
2019-05-30 08:45:05 -0400

    Allow disabling ssl validation for rest. #1287

--- tiledb/sm/rest/curl.cc
+++ tiledb/sm/rest/curl.cc
@@ -33,6 +33,7 @@
 #include "tiledb/sm/rest/curl.h"
 #include "tiledb/sm/misc/logger.h"
 #include "tiledb/sm/misc/stats.h"
+#include "tiledb/sm/misc/utils.h"
 
 #include <cstring>
 
@@ -127,6 +128,21 @@ Status Curl::init(const Config* config) {
   if (rc != CURLE_OK)
     return LOG_STATUS(Status::RestError(
         "Error initializing libcurl; failed to set CURLOPT_ERRORBUFFER"));
+
+  // Ignore ssl validation if the user has set rest.ignore_ssl_validation = true
+  const char* ignore_ssl_validation_str = nullptr;
+  RETURN_NOT_OK(
+      config_->get("rest.ignore_ssl_validation", &ignore_ssl_validation_str))
+
+  bool ignore_ssl_validation = false;
+  if (ignore_ssl_validation_str != nullptr)
+    RETURN_NOT_OK(tiledb::sm::utils::parse::convert(
+        ignore_ssl_validation_str, &ignore_ssl_validation));
+
+  if (ignore_ssl_validation) {
+    curl_easy_setopt(curl_.get(), CURLOPT_SSL_VERIFYHOST, 0);
+    curl_easy_setopt(curl_.get(), CURLOPT_SSL_VERIFYPEER, 0);
+  }
 
   return Status::Ok();
 }
