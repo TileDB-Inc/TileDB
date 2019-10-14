5f0623f4d304260538b13052215048e1477a1a0c
Isaiah Norton <isaiah@tiledb.io>
2019-10-14 15:38:03 -0400

    Add tiledb_unit success hook to set CI variable
    This commit adds a Catch2 completion event listener, which sets a
    job-level variable TILEDB_CI_SUCCESS=1 in the azure pipelines
    environment (note: only available in subsequent tasks).
    
    Then add a separate CI task which checks for this variable.

--- test/src/unit.cc
+++ test/src/unit.cc
@@ -1,2 +1,22 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+
+// Successful completion hook
+#include <cstdlib>
+#include <iostream>
+
+struct CICompletionStatusListener : Catch::TestEventListenerBase {
+  using TestEventListenerBase::TestEventListenerBase;  // inherit constructor
+
+  void testRunEnded(Catch::TestRunStats const& testRunStats) override {
+    if (std::getenv("AGENT_NAME") != nullptr) {
+      if (testRunStats.totals.testCases.allOk() == 1) {
+        // set TILEDB_CI_SUCCESS job-level variable in azure pipelines
+        // note: this variable is only set in subsequest tasks.
+        std::cout << "##vso[task.setvariable variable=TILEDB_CI_SUCCESS]1"
+                  << std::endl;
+      }
+    }
+  }
+};
+CATCH_REGISTER_LISTENER(CICompletionStatusListener)
