d3a83d84ee7016c50a87eb9e412c611a43586997
Isaiah Norton <isaiah@tiledb.io>
2019-10-14 14:42:31 -0400

    Windows CI fixes
    - Fix Windows CI directories
    - Minor clean-up for azure (quickstart tests)
    - Run Minio with certs (HTTPS) on Windows
    - Add powershell Invoke-WebRequest test to make sure minio is running
    - Remove merge cruft from azure-windows.yml
    - Disable S3 build on Windows

--- scripts/install-minio.ps1
+++ scripts/install-minio.ps1
@@ -21,7 +21,7 @@ function Get-ScriptsDirectory {
 $TileDBRootDirectory = Split-Path -Parent (Get-ScriptsDirectory)
 $InstallPrefix = Join-Path $TileDBRootDirectory "dist"
 $StagingDirectory = Join-Path (Get-ScriptsDirectory) "deps-staging"
-
+$CertsDirectory = Join-Path $TileDBRootDirectory "test/inputs/test_certs"
 New-Item -ItemType Directory -Path (Join-Path $InstallPrefix bin) -ea 0
 
 function DownloadFile([string] $URL, [string] $Dest) {
@@ -31,17 +31,17 @@ function DownloadFile([string] $URL, [string] $Dest) {
 
 function DownloadIfNotExists([string] $Path, [string] $URL) {
     if (!(Test-Path $Path)) {
-	# Minio uses TLS1.2
-	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-	DownloadFile $URL $Path
+        # Minio uses TLS1.2
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        DownloadFile $URL $Path
     }
 }
 function Install-Minio {
     $MinioRoot = (Join-Path $StagingDirectory "minio")
     $DownloadMinioDest = Join-Path $MinioRoot "minio.exe"
     if (!(Test-Path $MinioRoot)) {
-	New-Item -ItemType Directory -Path $MinioRoot
-	DownloadIfNotExists $DownloadMinioDest "https://dl.minio.io/server/minio/release/windows-amd64/minio.exe"
+        New-Item -ItemType Directory -Path $MinioRoot
+        DownloadIfNotExists $DownloadMinioDest "https://dl.minio.io/server/minio/release/windows-amd64/minio.exe"
     }
     Copy-Item $DownloadMinioDest (Join-Path $InstallPrefix "bin")
 }
@@ -58,17 +58,27 @@ function Run-Minio {
     $ServerConfigDir = Join-Path $InstallPrefix "etc\minio"
     $ServerDataDir = Join-Path $InstallPrefix "usr\local\share\minio"
     if (!(Test-Path $ServerDataDir)) {
-	New-Item -ItemType Directory -Path $ServerDataDir
+        New-Item -ItemType Directory -Path $ServerDataDir
     }
     if (!(Test-Path $ServerConfigDir)) {
-	New-Item -ItemType Directory -Path $ServerConfigDir
+        New-Item -ItemType Directory -Path $ServerConfigDir
     }
-    Start-Process -FilePath $ExePath -ArgumentList "server --address 127.0.0.1:9999 --config-dir `"$ServerConfigDir`" `"$ServerDataDir`""
+    Start-Process -FilePath $ExePath -ArgumentList "server --address 127.0.0.1:9999 --config-dir `"$ServerConfigDir`" --certs-dir `"$CertsDir`" `"$ServerDataDir`""
+
+    Start-Sleep 1.0
+
+    if ($PSVersionTable.PSVersion.Major -ge 6) {
+        Write-Host "Checking Minio with '-SkipCertificateCheck':"
+        Invoke-WebRequest -Uri https://127.0.0.1:9999 -SkipCertificateCheck
+        Write-Host "--------------------------------------------------------------------------------"
+    } else {
+        Write-Host "Skipping minio check (powershell version < 6)"
+    }
 }
 
 function Install-All-Deps {
     if (!(Test-Path $StagingDirectory)) {
-	New-Item -ItemType Directory -Path $StagingDirectory
+        New-Item -ItemType Directory -Path $StagingDirectory
     }
     Install-Minio
     Export-Env
