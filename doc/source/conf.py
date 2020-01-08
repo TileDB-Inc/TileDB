46683b8d2ad37ba681d8885730f098c7d049990c
Isaiah Norton <isaiah@tiledb.io>
2020-01-08 13:18:43 -0500

    Bump version to 1.7.3

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -73,7 +73,7 @@ author = 'TileDB, Inc.'
 # The short X.Y version.
 version = '1.7'
 # The full version, including alpha/beta/rc tags.
-release = '1.7.0'
+release = '1.7.3'
 
 # Breathe extension configuration.
 tiledb_dir = '../../'
