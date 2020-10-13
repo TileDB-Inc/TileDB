4d240c16a3dcadc6b97ca04d0e03abe3b614aeed
Rebekah <bekadavis9@gmail.com>
2020-10-13 13:30:59 -0400

    S3 copy support

--- tiledb/sm/filesystem/s3.cc
+++ tiledb/sm/filesystem/s3.cc
@@ -596,6 +596,27 @@ Status S3::move_dir(const URI& old_uri, const URI& new_uri) {
   return Status::Ok();
 }
 
+Status S3::copy_file(const URI& old_uri, const URI& new_uri) {
+  RETURN_NOT_OK(init_client());
+
+  RETURN_NOT_OK(copy_object(old_uri, new_uri));
+  return Status::Ok();
+}
+
+Status S3::copy_dir(const URI& old_uri, const URI& new_uri) {
+  RETURN_NOT_OK(init_client());
+
+  std::vector<std::string> paths;
+  RETURN_NOT_OK(ls(old_uri, &paths, ""));
+  for (const auto& path : paths) {
+    auto suffix = path.substr(old_uri.to_string().size());
+    auto new_path = new_uri.join_path(suffix);
+    RETURN_NOT_OK(copy_object(URI(path), URI(new_path)));
+  }
+
+  return Status::Ok();
+}
+
 Status S3::object_size(const URI& uri, uint64_t* nbytes) const {
   RETURN_NOT_OK(init_client());
 
