b1597cc1226a5be690e75e01dfd448f9f78cc1d5
Seth Shelnutt <Shelnutt2@gmail.com>
2020-12-28 07:47:18 -0500

    Update spdlog to 1.8.2
    Previously we were using spdlog 0.16.3. The older version was not
    causing problems with TileDB itself but we no longer played nicely with
    system installations of spdlog. Specifically users on
    fedora/alpine/arch/gento and osx might have had newer version of spdlog.
    There was not API breakage but the internal vs external fmt library was
    a problem. The new spdlog has a cmake target variable to declare how the
    fmt library should be handled and allows us to now set the proper
    compile time definitions needed for the header only library. The newer
    library also has proper cmake target.

--- cmake/Modules/FindSpdlog_EP.cmake
+++ cmake/Modules/FindSpdlog_EP.cmake
@@ -28,52 +28,62 @@
 # This module defines:
 #   - SPDLOG_INCLUDE_DIR, directory containing headers
 #   - SPDLOG_FOUND, whether Spdlog has been found
-#   - The Spdlog::Spdlog imported target
+#   - The spdlog::spdlog_header_only imported target
 
-# Search the path set during the superbuild for the EP.
-set(SPDLOG_PATHS ${TILEDB_EP_SOURCE_DIR}/ep_spdlog/include)
-
+# First try the CMake find module.
 if (NOT TILEDB_FORCE_ALL_DEPS OR TILEDB_SPDLOG_EP_BUILT)
-  find_path(SPDLOG_INCLUDE_DIR
-    NAMES spdlog/spdlog.h
-    PATHS ${SPDLOG_PATHS}
-    ${TILEDB_DEPS_NO_DEFAULT_PATH}
-  )
+  if (TILEDB_SPDLOG_EP_BUILT)
+    # If we built it from the source always force no default path
+    SET(TILEDB_SPDLOG_NO_DEFAULT_PATH NO_DEFAULT_PATH)
+  else()
+    SET(TILEDB_SPDLOG_NO_DEFAULT_PATH ${TILEDB_DEPS_NO_DEFAULT_PATH})
+  endif()
+
+  find_package(spdlog
+          PATHS ${TILEDB_EP_INSTALL_PREFIX}
+          ${TILEDB_SPDLOG_NO_DEFAULT_PATH}
+          )
+  set(SPDLOG_FOUND ${spdlog_FOUND})
 endif()
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Spdlog
-  REQUIRED_VARS SPDLOG_INCLUDE_DIR
-)
-
-if (NOT SPDLOG_FOUND AND TILEDB_SUPERBUILD)
-  message(STATUS "Adding Spdlog as an external project")
-  ExternalProject_Add(ep_spdlog
-    PREFIX "externals"
-    # Set download name to avoid collisions with only the version number in the filename
-    DOWNLOAD_NAME ep_spdlog.zip
-    URL "https://github.com/gabime/spdlog/archive/v0.16.3.zip"
-    URL_HASH SHA1=00a732da1449c15b787491a924d63590c1649710
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    UPDATE_COMMAND ""
-    LOG_DOWNLOAD TRUE
-    LOG_OUTPUT_ON_FAILURE ${TILEDB_LOG_OUTPUT_ON_FAILURE}
-  )
-  list(APPEND TILEDB_EXTERNAL_PROJECTS ep_spdlog)
-  list(APPEND FORWARD_EP_CMAKE_ARGS
-    -DTILEDB_SPDLOG_EP_BUILT=TRUE
-  )
+if (NOT SPDLOG_FOUND)
+  if(TILEDB_SUPERBUILD)
+    message(STATUS "Adding spdlog as an external project")
+    ExternalProject_Add(ep_spdlog
+      PREFIX "externals"
+      # Set download name to avoid collisions with only the version number in the filename
+      DOWNLOAD_NAME ep_spdlog.zip
+      URL "https://github.com/gabime/spdlog/archive/v1.8.2.zip"
+      URL_HASH SHA1=970d58ccd9ba0d2f86e3b5c405fedfcb82949906
+      LOG_DOWNLOAD TRUE
+      LOG_OUTPUT_ON_FAILURE ${TILEDB_LOG_OUTPUT_ON_FAILURE}
+    )
+    list(APPEND EXTERNAL_PROJECTS ep_spdlog)
+    list(APPEND FORWARD_EP_CMAKE_ARGS
+            -DTILEDB_SPDLOG_EP_BUILT=TRUE
+            )
+  else()
+    message(FATAL_ERROR "Unable to find spdlog")
+  endif()
 endif()
 
-if (SPDLOG_FOUND AND NOT TARGET Spdlog::Spdlog)
-  add_library(Spdlog::Spdlog INTERFACE IMPORTED)
+if (spdlog_FOUND AND NOT TARGET spdlog::spdlog_header_only)
+  add_library(spdlog::spdlog_header_only INTERFACE IMPORTED)
   find_package(fmt QUIET)
   if (${fmt_FOUND})
-    target_link_libraries(Spdlog::Spdlog INTERFACE fmt::fmt)
+    target_link_libraries(spdlog::spdlog_header_only INTERFACE fmt::fmt)
   endif()
-  set_target_properties(Spdlog::Spdlog PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${SPDLOG_INCLUDE_DIR}"
-  )
+  set_target_properties(spdlog::spdlog_header_only PROPERTIES
+          INTERFACE_INCLUDE_DIRECTORIES "${SPDLOG_INCLUDE_DIR}"
+          )
+  # If the target is defined we need to handle external fmt build types
+elseif(TARGET spdlog::spdlog_header_only)
+  if (SPDLOG_FMT_EXTERNAL)
+    # Since we are using header only we need to define this
+    add_definitions("-DSPDLOG_FMT_EXTERNAL=1")
+    find_package(fmt REQUIRED)
+    if (${fmt_FOUND})
+      target_link_libraries(spdlog::spdlog_header_only INTERFACE fmt::fmt)
+    endif()
+  endif()
 endif()
