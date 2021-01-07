ba1408be9bab441a90243c79d71dd54c20c54b85
joe maley <joe@tiledb.io>
2021-01-07 12:09:10 -0500

    2.2.1 history + version (#2005)
    Co-authored-by: Joe Maley <joe@tiledb.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -73,7 +73,7 @@ author = 'TileDB, Inc.'
 # The short X.Y version.
 version = '2.2'
 # The full version, including alpha/beta/rc tags.
-release = '2.2.0'
+release = '2.2.1'
 
 # Breathe extension configuration.
 tiledb_dir = '../../'
