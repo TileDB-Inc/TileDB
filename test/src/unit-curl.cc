8bc70f2344942d309a5be4010f97e4275ed9b4f3
ktsitsi <kostas.t@tiledb.com>
2021-01-12 12:18:17 +0200

    Format

--- test/src/unit-curl.cc
+++ test/src/unit-curl.cc
@@ -43,9 +43,10 @@
 using namespace tiledb::sm;
 
 TEST_CASE("CURL: Test curl's header parsing callback", "[curl]") {
-
-  //Initialize data that in real life scenario would be initialized by RestClient
-  char res_data[] = "Location: https://test.url.domain/v1/arrays/testns/test_arr";
+  // Initialize data that in real life scenario would be initialized by
+  // RestClient
+  char res_data[] =
+      "Location: https://test.url.domain/v1/arrays/testns/test_arr";
   size_t size = 50;
   size_t count = 50;
 
@@ -54,35 +55,39 @@ TEST_CASE("CURL: Test curl's header parsing callback", "[curl]") {
   userdata.uri = &ns_array;
   std::mutex redirect_mtx_;
   std::unordered_map<std::string, std::string> redirect_meta_;
-  userdata.redirect_uri_map = & redirect_meta_;
+  userdata.redirect_uri_map = &redirect_meta_;
   userdata.redirect_uri_map_lock = &redirect_mtx_;
 
   size_t result = write_header_callback(&res_data, size, count, &userdata);
 
-  //The return value should be equal to size * count
+  // The return value should be equal to size * count
   CHECK(result == size * count);
-  //The uri is not mutated inside the callback function
+  // The uri is not mutated inside the callback function
   CHECK(*userdata.uri == ns_array);
-  //The redirect map holds a record of ns_array key and redirection url value
+  // The redirect map holds a record of ns_array key and redirection url value
   CHECK(userdata.redirect_uri_map->count(ns_array) == 1);
-  //The value of this redirection record matches the redirection url
-  CHECK(userdata.redirect_uri_map->find(ns_array)->second ==  "https://test.url.domain");
+  // The value of this redirection record matches the redirection url
+  CHECK(
+      userdata.redirect_uri_map->find(ns_array)->second ==
+      "https://test.url.domain");
 
-  //Initialize data that in real life scenario would be initialized by RestClient
+  // Initialize data that in real life scenario would be initialized by
+  // RestClient
   char res_data_s3[] = "Location: tiledb://my_username/s3://my_bucket/my_array";
   ns_array = "my_bucket:my_array";
   userdata.uri = &ns_array;
   result = write_header_callback(&res_data_s3, size, count, &userdata);
 
-  //The return value should be equal to size * count
+  // The return value should be equal to size * count
   CHECK(result == size * count);
-  //The uri is not mutated inside the callback function
+  // The uri is not mutated inside the callback function
   CHECK(*userdata.uri == ns_array);
-  //The redirect map holds a record of ns_array key and redirection url value
+  // The redirect map holds a record of ns_array key and redirection url value
   CHECK(userdata.redirect_uri_map->count(ns_array) == 1);
-  //The redirect map holds both records
+  // The redirect map holds both records
   CHECK(userdata.redirect_uri_map->size() == 2);
-  //The value of this redirection record matches the redirection url
-  CHECK(userdata.redirect_uri_map->find(ns_array)->second ==  "tiledb://my_username");
-
+  // The value of this redirection record matches the redirection url
+  CHECK(
+      userdata.redirect_uri_map->find(ns_array)->second ==
+      "tiledb://my_username");
 }
