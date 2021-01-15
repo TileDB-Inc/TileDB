5f6582784773c76566778e7a702cdc87896353d1
Ypatia Tsavliri <ypatia@tiledb.com>
2021-01-15 18:48:12 +0200

    Update copyrights to 2021

--- scripts/markdown-link-prs.sh
+++ scripts/markdown-link-prs.sh
@@ -3,7 +3,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2019-2020 TileDB, Inc.
+# Copyright (c) 2019-2021 TileDB, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
--- tiledb/common/status.h
+++ tiledb/common/status.h
@@ -5,7 +5,7 @@
  *
  * The BSD License
  *
- * @copyright Copyright (c) 2017-2020 TileDB, Inc.
+ * @copyright Copyright (c) 2017-2021 TileDB, Inc.
  *            Copyright (c) 2011 The LevelDB Authors. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
