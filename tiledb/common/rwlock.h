53a3d56703ee63d471125fe7dc2fedb64e1439f9
joe maley <joe@tiledb.io>
2021-02-03 08:53:50 -0500

    RAII read-write lock (#2065)
    This introduces `UniqueReadLock` and `UniqueWriteLock` for scoped locking on
    `tiledb::common::RWLock`.
    
    Co-authored-by: Joe Maley <joe@tiledb.com>

--- tiledb/common/rwlock.h
+++ tiledb/common/rwlock.h
@@ -27,22 +27,44 @@
  *
  * @section DESCRIPTION
  *
- * This file defines a rwlock implementation.
+ * This file defines and implements a write-preferring read-write lock.
  */
+
+#ifndef TILEDB_RWLOCK_H
+#define TILEDB_RWLOCK_H
 
 #include <condition_variable>
 #include <mutex>
 
+#include "tiledb/common/macros.h"
+
+namespace tiledb {
+namespace common {
+
 class RWLock {
  public:
+  /* ********************************* */
+  /*     CONSTRUCTORS & DESTRUCTORS    */
+  /* ********************************* */
+
+  /** Default constructor. */
   RWLock()
       : writer_(false)
       , waiting_writers_(0)
       , readers_(0) {
   }
 
+  /** Default destructor. */
   ~RWLock() = default;
 
+  DISABLE_COPY_AND_COPY_ASSIGN(RWLock);
+  DISABLE_MOVE_AND_MOVE_ASSIGN(RWLock);
+
+  /* ********************************* */
+  /*                API                */
+  /* ********************************* */
+
+  /** Blocks until acquiring a read lock. */
   void read_lock() {
     std::unique_lock<std::mutex> ul(mutex_);
     while (waiting_writers_ > 0 || writer_)
@@ -50,12 +72,17 @@ class RWLock {
     ++readers_;
   }
 
+  /**
+   * Releases a read lock, must be called after
+   * `read_lock`.
+   */
   void read_unlock() {
     std::unique_lock<std::mutex> ul(mutex_);
     if (--readers_ == 0)
       cv_.notify_all();
   }
 
+  /** Blocks until acquiring a write lock. */
   void write_lock() {
     std::unique_lock<std::mutex> ul(mutex_);
     ++waiting_writers_;
@@ -65,12 +92,17 @@ class RWLock {
     writer_ = true;
   }
 
+  /**
+   * Releases a write lock, must be called after
+   * `write_lock`.
+   */
   void write_unlock() {
     std::unique_lock<std::mutex> ul(mutex_);
     writer_ = false;
     cv_.notify_all();
   }
 
+  /** Synchronously downgrades a write lock to a read lock. */
   void write_downgrade() {
     std::unique_lock<std::mutex> ul(mutex_);
     ++readers_;
@@ -79,15 +111,27 @@ class RWLock {
   }
 
  private:
-  bool writer_;
+  /* ********************************* */
+  /*         PRIVATE ATTRIBUTES        */
+  /* ********************************* */
 
-  uint64_t waiting_writers_;
-
-  uint64_t readers_;
-
-  /** Mutex */
+  /** Protects all member variables. */
   std::mutex mutex_;
 
-  /** Conditional variable for tracking changes in read/write lock counts */
+  /** Conditional variable to signal lock activity. */
   std::condition_variable cv_;
+
+  /** True if a write lock is held. */
+  bool writer_;
+
+  /** The number of writers waiting in `write_lock`. */
+  uint64_t waiting_writers_;
+
+  /** The number of outstanding read locks. */
+  uint64_t readers_;
 };
+
+}  // namespace common
+}  // namespace tiledb
+
+#endif  // TILEDB_RWLOCK_H
