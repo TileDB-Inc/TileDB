59a8cb6736ed0fe0b85e6b45daaeea1591f0a8da
Seth Shelnutt <Shelnutt2@gmail.com>
2021-02-11 06:58:24 -0500

    Update version in dev to 2.3

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -71,9 +71,9 @@ copyright = '2021 TileDB, Inc'
 author = 'TileDB, Inc.'
 
 # The short X.Y version.
-version = '2.2'
+version = '2.3'
 # The full version, including alpha/beta/rc tags.
-release = '2.2.3'
+release = '2.3.0'
 
 # Breathe extension configuration.
 tiledb_dir = '../../'
