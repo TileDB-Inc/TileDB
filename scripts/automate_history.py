9f116903cd655b5ae3122bfa744e3d3203168e01
George Sakkis <george.sakkis@tiledb.com>
2021-04-06 19:53:27 +0300

    Refactor scripts/automate_history.py

--- scripts/automate_history.py
+++ scripts/automate_history.py
@@ -1,148 +1,91 @@
 import sys
-import hashlib
+
+_type_mapping = {
+    "FORMAT": "## Disk Format",
+    "BREAKING_API": "## Breaking C API changes",
+    "BREAKING_BEHAVIOR": "## Breaking behavior",
+    "FEATURE": "## New features",
+    "IMPROVEMENT": "## Improvements",
+    "DEPRECATION": "## Deprecations",
+    "BUG": "## Bug fixes",
+    "C_API": "### C API",
+    "CPP_API": "### C++ API",
+}
 
 
-def print_and_die(message):
-    print(message)
-    sys.exit(1)
-
-
-def parse_history(pr_message):
-    type_tag = "TYPE:"
-    description_tag = "DESC:"
-
+def parse_pr_message(pr_message, type_tag="TYPE:", description_tag="DESC:"):
     h_types = []
     h_descriptions = []
-
-    lines = pr_message.split("\n")
-    for line in lines:
+    for line in pr_message.strip().split("\n"):
         line = line.strip()
         if line.startswith(type_tag):
             h_types.append(line[len(type_tag) :].strip())
-        if line.startswith(description_tag):
+        elif line.startswith(description_tag):
             h_descriptions.append(line[len(description_tag) :].strip())
 
-    if len(h_types) == 0:
-        print_and_die(
-            'Unable to locate "' + type_tag + '" in PR message:\n' + pr_message
-        )
+    if not h_types:
+        sys.exit(f'Unable to locate "{type_tag}" in PR message:\n{pr_message}')
 
-    if len(h_descriptions) == 0:
-        print_and_die(
-            'Unable to locate "' + description_tag + '" in PR message:\n' + pr_message
-        )
+    if not h_descriptions:
+        sys.exit(f'Unable to locate "{description_tag}" in PR message:\n{pr_message}')
 
     if len(h_types) != len(h_descriptions):
-        print_and_die("Mismatched number of history types and descriptions")
+        sys.exit("Mismatched number of history types and descriptions")
 
     return h_types, h_descriptions
-
-
-def parse_type(h_type):
-    if h_type == "FORMAT":
-        return "## Disk Format"
-
-    if h_type == "BREAKING_API":
-        return "## Breaking C API changes"
-
-    if h_type == "BREAKING_BEHAVIOR":
-        return "## Breaking behavior"
-
-    if h_type == "FEATURE":
-        return "## New features"
-
-    if h_type == "IMPROVEMENT":
-        return "## Improvements"
-
-    if h_type == "DEPRECATION":
-        return "## Deprecations"
-
-    if h_type == "BUG":
-        return "## Bug fixes"
-
-    if h_type == "C_API":
-        return "### C API"
-
-    if h_type == "CPP_API":
-        return "### C++ API"
-
-    print_and_die("Unknown history type: " + h_type)
-
-
-# Returns a formatted line to insert into HISTORY.md
-def format_history_line(pr_number, h_description):
-    return (
-        "* "
-        + h_description
-        + " [#"
-        + pr_number
-        + "](https://github.com/TileDB-Inc/TileDB/pull/"
-        + pr_number
-        + ")"
-    )
 
 
 # Updates the HISTORY.md file.
 def update_history(pr_number, h_types, h_descriptions):
     # Read `HISTORY.md` into memory.
-    lines = ""
     with open("HISTORY.md") as f:
         lines = f.read().rstrip("\n").split("\n")
 
-    # Take the MD5 of `lines` so that we can determine
-    # if this routine has modified `HISTORY.md`.
-    orig_md5 = hashlib.md5()
-    for line in lines:
-        orig_md5.update(str.encode(line))
+    # Remove all existing lines for this pull request
+    pr_number = pr_number.strip()
+    pull_url = f"https://github.com/TileDB-Inc/TileDB/pull/{pr_number}"
+    new_lines = [line for line in lines if pull_url not in line]
 
-    # Remove all existing lines for this pull request.
-    existing_line_substr = "https://github.com/TileDB-Inc/TileDB/pull/" + pr_number
-    lines = [line for line in lines if not existing_line_substr in line]
-
-    for i, h_type in enumerate(h_types):
-        h_description = h_descriptions[i]
-
-        history_header = parse_type(h_type)
-        history_line = format_history_line(pr_number, h_description)
+    for h_type, h_description in zip(h_types, h_descriptions):
+        try:
+            history_header = _type_mapping[h_type]
+        except KeyError:
+            sys.exit(f"Unknown history type: {h_type}")
 
         # The HISTORY.md always starts with in-progress changes first.
         # We will locate the first line that starts with `history_header`
-        for j in range(len(lines)):
-            if lines[j].strip().startswith(history_header):
-                lines = lines[0 : j + 1] + [history_line] + lines[j + 1 :]
+        for i, line in enumerate(new_lines):
+            if line.strip().startswith(history_header):
+                new_lines.insert(i + 1, f"* {h_description} [#{pr_number}]({pull_url})")
                 break
 
-    # Take the new MD5 of `lines` so that we can determine
-    # if this routine has modified `HISTORY.md`.
-    new_md5 = hashlib.md5()
-    for line in lines:
-        new_md5.update(str.encode(line))
-
-    if orig_md5.digest() == new_md5.digest():
+    if lines == new_lines:
         return False
 
     # Overwrite `HISTORY.md` with the new lines
     with open("HISTORY.md", "w") as f:
-        for line in lines:
-            f.write(line + "\n")
+        for line in new_lines:
+            print(line, file=f)
 
     return True
 
 
-if len(sys.argv) < 3:
-    print_and_die("usage: " + sys.argv[0] + " <PR number> <PR message>")
+def main():
+    try:
+        pr_number, pr_message = sys.argv[1:]
+    except ValueError:
+        sys.exit(f"usage: {sys.argv[0]} <PR number> <PR message>")
 
-pr_number = sys.argv[1].strip()
-pr_message = sys.argv[2].strip()
+    if "NO_HISTORY" in pr_message:
+        # If the user has included the reserved keyword "NO_HISTORY" anywhere in the
+        # PR description, do not modify the history. Leave `h_updated` as `False`.
+        h_updated = False
+    else:
+        h_updated = update_history(pr_number, *parse_pr_message(pr_message))
 
-h_updated = False
-if "NO_HISTORY" in pr_message:
-    # If the user has included the reserved keyword "NO_HISTORY" anywhere in the
-    # PR description, do not modify the history. Leave `h_updated` as `False`.
-    pass
-else:
-    h_types, h_descriptions = parse_history(pr_message)
-    h_updated = update_history(pr_number, h_types, h_descriptions)
+    # Set the Github Action variable `HISTORY_UPDATED`
+    print(f"::set-output name=HISTORY_UPDATED::{h_updated}")
 
-# Set the Github Action variable `HISTORY_UPDATED`
-print("::set-output name=HISTORY_UPDATED::" + str(h_updated))
+
+if __name__ == "__main__":
+    main()
