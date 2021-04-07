a47d80ca0c1e7795b3e13ba9f61a3bef43d29278
George Sakkis <george.sakkis@tiledb.com>
2021-04-07 12:06:42 +0300

    scripts/automate_history.py: Add a --check-only flag to just check if the PR body is valid without changing HISTORY.md

--- scripts/automate_history.py
+++ scripts/automate_history.py
@@ -1,4 +1,8 @@
+import argparse
 import sys
+from collections import defaultdict
+from typing import Mapping, Sequence, Tuple
+
 
 _type_mapping = {
     "FORMAT": "## Disk Format",
@@ -13,78 +17,105 @@ _type_mapping = {
 }
 
 
-def parse_pr_message(pr_message, type_tag="TYPE:", description_tag="DESC:"):
-    h_types = []
-    h_descriptions = []
-    for line in pr_message.strip().split("\n"):
+def parse_pr_body(
+    body: str, type_tag: str = "TYPE:", description_tag: str = "DESC:"
+) -> Tuple[Sequence[str], Sequence[str]]:
+    headers = []
+    descriptions = []
+    for line in body.strip().split("\n"):
         line = line.strip()
         if line.startswith(type_tag):
-            h_types.append(line[len(type_tag) :].strip())
+            change_type = line[len(type_tag) :].strip()
+            try:
+                headers.append(_type_mapping[change_type])
+            except KeyError:
+                raise ValueError(f"Unknown history type: {change_type}")
+
         elif line.startswith(description_tag):
-            h_descriptions.append(line[len(description_tag) :].strip())
+            descriptions.append(line[len(description_tag) :].strip())
 
-    if not h_types:
-        sys.exit(f'Unable to locate "{type_tag}" in PR message:\n{pr_message}')
+    if not headers:
+        raise ValueError(f'Unable to locate "{type_tag}" in PR body:\n{body}')
 
-    if not h_descriptions:
-        sys.exit(f'Unable to locate "{description_tag}" in PR message:\n{pr_message}')
+    if not descriptions:
+        raise ValueError(f'Unable to locate "{description_tag}" in PR body:\n{body}')
 
-    if len(h_types) != len(h_descriptions):
-        sys.exit("Mismatched number of history types and descriptions")
+    if len(headers) != len(descriptions):
+        raise ValueError("Mismatched number of history types and descriptions")
 
-    return h_types, h_descriptions
+    return headers, descriptions
 
 
 # Updates the HISTORY.md file.
-def update_history(pr_number, h_types, h_descriptions):
+def update_history(
+    pr_number: int,
+    headers: Sequence[str],
+    descriptions: Sequence[str],
+) -> Mapping[str, Sequence[str]]:
     # Read `HISTORY.md` into memory.
     with open("HISTORY.md") as f:
         lines = f.read().rstrip("\n").split("\n")
 
     # Remove all existing lines for this pull request
-    pr_number = pr_number.strip()
     pull_url = f"https://github.com/TileDB-Inc/TileDB/pull/{pr_number}"
     new_lines = [line for line in lines if pull_url not in line]
 
-    for h_type, h_description in zip(h_types, h_descriptions):
-        try:
-            history_header = _type_mapping[h_type]
-        except KeyError:
-            sys.exit(f"Unknown history type: {h_type}")
-
+    additions = defaultdict(list)
+    for header, description in zip(headers, descriptions):
         # The HISTORY.md always starts with in-progress changes first.
-        # We will locate the first line that starts with `history_header`
+        # We will locate the first line that starts with `header`
         for i, line in enumerate(new_lines):
-            if line.strip().startswith(history_header):
-                new_lines.insert(i + 1, f"* {h_description} [#{pr_number}]({pull_url})")
+            if line.strip().startswith(header):
+                new_line = f"* {description} [#{pr_number}]({pull_url})"
+                new_lines.insert(i + 1, new_line)
+                additions[header].append(new_line)
                 break
 
     if lines == new_lines:
-        return False
+        additions.clear()
+    else:
+        # Overwrite `HISTORY.md` with the new lines
+        with open("HISTORY.md", "w") as f:
+            for line in new_lines:
+                print(line, file=f)
 
-    # Overwrite `HISTORY.md` with the new lines
-    with open("HISTORY.md", "w") as f:
-        for line in new_lines:
-            print(line, file=f)
-
-    return True
+    return additions
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="Check PR description and (optionally) update HISTORY.md"
+    )
+    parser.add_argument("number", type=int, help="The PR number")
+    parser.add_argument("body", help="The PR text description body")
+    parser.add_argument(
+        "--check-only",
+        action="store_true",
+        help="Only check if the PR body is valid without updating HISTORY.md",
+    )
+    args = parser.parse_args()
+
+    # If the reserved keyword "NO_HISTORY" is included anywhere in the PR body,
+    # do not check it for validity or modify the history
+    if "NO_HISTORY" in args.body:
+        return
+
     try:
-        pr_number, pr_message = sys.argv[1:]
-    except ValueError:
-        sys.exit(f"usage: {sys.argv[0]} <PR number> <PR message>")
+        headers, descriptions = parse_pr_body(args.body)
+    except ValueError as ex:
+        sys.exit(ex)
 
-    if "NO_HISTORY" in pr_message:
-        # If the user has included the reserved keyword "NO_HISTORY" anywhere in the
-        # PR description, do not modify the history. Leave `h_updated` as `False`.
-        h_updated = False
-    else:
-        h_updated = update_history(pr_number, *parse_pr_message(pr_message))
+    if args.check_only:
+        return
 
-    # Set the Github Action variable `HISTORY_UPDATED`
-    print(f"::set-output name=HISTORY_UPDATED::{h_updated}")
+    additions = update_history(args.number, headers, descriptions)
+    if additions:
+        print("Updated HISTORY.md with the following additions:")
+        for header, lines in additions.items():
+            print()
+            print(header)
+            for line in reversed(lines):
+                print(line)
 
 
 if __name__ == "__main__":
