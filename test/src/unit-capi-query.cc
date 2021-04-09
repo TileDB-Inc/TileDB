a0971fc2c6b7db9487c2ae1dcebca315aa337ee0
joe maley <joe@tiledb.io>
2021-04-09 06:13:22 -0400

    [BACKPORT] Deep-copy return value from tiledb_query_get_array (#2188)
    Currently, the `tiledb_query_get_array` returns a newly-allocated C structure
    that contains a pointer to the internal array. This is problematic because
    we cannot free the C structure with `tiledb_array_free`. This change makes the
    API return a deep copy.`
    
    ---
    
    TYPE: C_API
    DESC: tiledb_query_get_array now returns a deep-copy

--- test/src/unit-capi-query.cc
+++ test/src/unit-capi-query.cc
@@ -506,7 +506,7 @@ TEST_CASE_METHOD(
   tiledb_array_t* rarray;
   rc = tiledb_query_get_array(ctx_, query, &rarray);
   REQUIRE(rc == TILEDB_OK);
-  CHECK(rarray->array_ == array->array_);
+  CHECK(rarray->array_ != array->array_);
 
   tiledb_array_schema_t* rschema;
   rc = tiledb_array_get_schema(ctx_, rarray, &rschema);
@@ -532,5 +532,6 @@ TEST_CASE_METHOD(
 
   tiledb_query_free(&query);
   tiledb_array_free(&array);
+  tiledb_array_free(&rarray);
   remove_temp_dir(temp_dir);
 }
