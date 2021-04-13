80e2cd3f3b8ebb64462551c075c895edd87763bf
joe maley <joe@tiledb.io>
2021-04-13 09:56:13 -0400

    Config param to skip splitting on estimated result sizes (#2203)
    This introduces a new, advanced config parameter "sm.skip_est_size_partitioning".
    This is intentionally undocumented in the c/cpp-api headers because it is an
    advanced tuning parameter. This is useful for reads where the result size
    estimate is too large and causes unnecessary partition splits.
    
    ---
    
    TYPE: IMPROVEMENT
    DESC: Introduces config parameter "sm.skip_est_size_partitioning"
    
    Co-authored-by: Joe Maley <joe@tiledb.com>

--- test/src/unit-capi-config.cc
+++ test/src/unit-capi-config.cc
@@ -243,6 +243,7 @@ void check_save_to_file() {
   ss << "sm.memory_budget_var 10737418240\n";
   ss << "sm.read_range_oob warn\n";
   ss << "sm.skip_checksum_validation false\n";
+  ss << "sm.skip_est_size_partitioning false\n";
   ss << "sm.sub_partitioner_memory_budget 0\n";
   ss << "sm.tile_cache_size 10000000\n";
   ss << "sm.vacuum.mode fragments\n";
@@ -524,6 +525,7 @@ TEST_CASE("C API: Test config iter", "[capi], [config]") {
   all_param_values["sm.check_coord_oob"] = "true";
   all_param_values["sm.check_global_order"] = "true";
   all_param_values["sm.tile_cache_size"] = "100";
+  all_param_values["sm.skip_est_size_partitioning"] = "false";
   all_param_values["sm.memory_budget"] = "5368709120";
   all_param_values["sm.memory_budget_var"] = "10737418240";
   all_param_values["sm.sub_partitioner_memory_budget"] = "0";
