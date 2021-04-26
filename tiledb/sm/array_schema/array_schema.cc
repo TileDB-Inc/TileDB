f65db6e5cf584840f5a8b192bc453716df40bcb5
bdeng.xt@gmail.com <bdeng@DESKTOP-NT5F11P.localdomain>
2021-04-26 15:21:07 -0400

    run clang-format version 5

--- tiledb/sm/array_schema/array_schema.cc
+++ tiledb/sm/array_schema/array_schema.cc
@@ -645,11 +645,11 @@ Status ArraySchema::set_domain(Domain* domain) {
           datatype_str(type) + "'"));
     }
   }
-  
+
   if (cell_order_ != Layout::HILBERT) {
     RETURN_NOT_OK(domain->set_null_tile_extents_to_range());
   }
-  
+
   // Set domain
   tdb_delete(domain_);
   domain_ = tdb_new(Domain, domain);
