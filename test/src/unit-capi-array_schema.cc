0e2931f0c7035a423a772bd93f8d49af80912af8
bdeng-xt <bdeng.xt@gmail.com>
2021-05-06 23:41:36 -0400

    fix some unit test errors

--- test/src/unit-capi-array_schema.cc
+++ test/src/unit-capi-array_schema.cc
@@ -101,6 +101,15 @@ struct ArraySchemaFx {
   // Vector of supported filsystems
   const std::vector<std::unique_ptr<SupportedFs>> fs_vec_;
 
+  //struct for information of another directory
+  struct schema_file_struct {
+    tiledb_ctx_t* ctx;
+    tiledb_vfs_t* vfs;
+    std::string path;    
+  };
+
+  static int get_schema_file_struct(const char* path, void* data);
+
   // Functions
   ArraySchemaFx();
   ~ArraySchemaFx();
@@ -903,6 +912,18 @@ std::string ArraySchemaFx::random_name(const std::string& prefix) {
   return ss.str();
 }
 
+int ArraySchemaFx::get_schema_file_struct(const char* path, void* data) {
+  auto data_struct=(ArraySchemaFx::schema_file_struct*)data;
+  auto ctx = data_struct->ctx;
+  auto vfs = data_struct->vfs;
+  int is_dir;
+  int rc = tiledb_vfs_is_dir(ctx,vfs,path,&is_dir);
+  CHECK(rc == TILEDB_OK);
+
+  data_struct->path = path;
+  return 1;  
+}
+
 TEST_CASE_METHOD(
     ArraySchemaFx,
     "C API: Test array schema creation and retrieval",
@@ -1365,9 +1386,13 @@ TEST_CASE_METHOD(
   tiledb_array_schema_free(&array_schema);
 
   // Corrupt the array schema
-  std::string schema_path = array_name + "/__array_schema.tdb";
+  std::string schema_path = array_name + "/" + tiledb::sm::constants::array_schema_folder_name;
   std::string to_write = "garbage";
   tiledb_vfs_fh_t* fh;
+  schema_file_struct data_struct={ctx_,vfs_,""};
+  rc = tiledb_vfs_ls(ctx_,vfs_,schema_path.c_str(),&get_schema_file_struct,&data_struct);
+  schema_path = data_struct.path;
+
   rc = tiledb_vfs_open(ctx_, vfs_, schema_path.c_str(), TILEDB_VFS_WRITE, &fh);
   REQUIRE(rc == TILEDB_OK);
   rc = tiledb_vfs_write(ctx_, fh, to_write.c_str(), to_write.size());
