ce41347afa859f575ded96df71c8e18dc1546218
Eric Hughes <eric.hughes@tiledb.com>
2021-05-19 17:33:25 -0600

    Fixed an incorrect loop causing index out of bound in test/src/unit-DenseTiler.cc

--- test/src/unit-DenseTiler.cc
+++ test/src/unit-DenseTiler.cc
@@ -2488,7 +2488,7 @@ TEST_CASE_METHOD(
   c_data1_3_val[6] = 'o';
   c_data1_3_val[7] = 'o';
   c_data1_3_val[8] = 'o';
-  for (int i = 9; i <= 57; ++i)
+  for (int i = 9; i < 57; ++i)
     c_data1_3_val[i] = 0;
   CHECK(check_tile<uint8_t>(&tile1_3_val, c_data1_3_val));
 
