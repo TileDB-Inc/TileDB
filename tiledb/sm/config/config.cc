86123450656c334175fa41cac888dac2d7064e86
Joe Maley <joe@tiledb.com>
2021-05-21 08:38:21 -0400

    Refactor array open-timestamp APIs
    This patch refactors the API for start/end timestamps to use standard
    getter/setter APIs instead of key-values in the config.
    
    Does not include serialization/rest changes.

--- tiledb/sm/config/config.cc
+++ tiledb/sm/config/config.cc
@@ -63,8 +63,6 @@ const std::string Config::REST_RETRY_HTTP_CODES = "503";
 const std::string Config::REST_RETRY_COUNT = "3";
 const std::string Config::REST_RETRY_INITIAL_DELAY_MS = "500";
 const std::string Config::REST_RETRY_DELAY_FACTOR = "1.25";
-const std::string Config::SM_ARRAY_TIMESTAMP_START = "0";
-const std::string Config::SM_ARRAY_TIMESTAMP_END = std::to_string(UINT64_MAX);
 const std::string Config::SM_ENCRYPTION_KEY = "0";
 const std::string Config::SM_ENCRYPTION_TYPE = "NO_ENCRYPTION";
 const std::string Config::SM_DEDUP_COORDS = "false";
@@ -199,8 +197,6 @@ Config::Config() {
   param_values_["rest.retry_delay_factor"] = REST_RETRY_DELAY_FACTOR;
   param_values_["config.env_var_prefix"] = CONFIG_ENVIRONMENT_VARIABLE_PREFIX;
   param_values_["config.logging_level"] = CONFIG_LOGGING_LEVEL;
-  param_values_["sm.array.timestamp_start"] = SM_ARRAY_TIMESTAMP_START;
-  param_values_["sm.array.timestamp_end"] = SM_ARRAY_TIMESTAMP_END;
   param_values_["sm.encryption_key"] = SM_ENCRYPTION_KEY;
   param_values_["sm.encryption_type"] = SM_ENCRYPTION_TYPE;
   param_values_["sm.dedup_coords"] = SM_DEDUP_COORDS;
@@ -446,10 +442,6 @@ Status Config::unset(const std::string& param) {
     param_values_["config.env_var_prefix"] = CONFIG_ENVIRONMENT_VARIABLE_PREFIX;
   } else if (param == "config.logging_level") {
     param_values_["config.logging_level"] = CONFIG_LOGGING_LEVEL;
-  } else if (param == "sm.array.timestamp_start") {
-    param_values_["sm.array.timestamp_start"] = SM_ARRAY_TIMESTAMP_START;
-  } else if (param == "sm.array.timestamp_end") {
-    param_values_["sm.array.timestamp_end"] = SM_ARRAY_TIMESTAMP_END;
   } else if (param == "sm.encryption_key") {
     param_values_["sm.encryption_key"] = SM_ENCRYPTION_KEY;
   } else if (param == "sm.encryption_type") {
@@ -687,10 +679,6 @@ Status Config::sanity_check(
     RETURN_NOT_OK(serialization_type_enum(value, &serialization_type));
   } else if (param == "config.logging_level") {
     RETURN_NOT_OK(utils::parse::convert(value, &v32));
-  } else if (param == "sm.array.timestamp_start") {
-    RETURN_NOT_OK(utils::parse::convert(value, &vuint64));
-  } else if (param == "sm.array.timestamp_end") {
-    RETURN_NOT_OK(utils::parse::convert(value, &vuint64));
   } else if (param == "sm.dedup_coords") {
     RETURN_NOT_OK(utils::parse::convert(value, &v));
   } else if (param == "sm.check_coord_dups") {
