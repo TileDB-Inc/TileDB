f81a2a8b44b0ada141fc4381492e673a8c828ba1
Isaiah Norton <isaiah@tiledb.io>
2021-06-07 09:16:58 -0400

    Remove duplicate function definition

--- tiledb/sm/query/query.cc
+++ tiledb/sm/query/query.cc
@@ -1108,10 +1108,6 @@ QueryType Query::type() const {
   return type_;
 }
 
-Stats* Query::stats() const {
-  return stats_;
-}
-
 const Config* Query::config() const {
   if (type_ == QueryType::READ)
     return reader_.config();
