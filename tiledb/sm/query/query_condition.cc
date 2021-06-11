0685219de37573fccdb3ccde041a50d7678c8011
Seth Shelnutt <Shelnutt2@gmail.com>
2021-06-11 08:31:33 -0400

    Fix check for query condition allowing nulls
    The check previously compared the datatype size to the value size but
    this did not account for nulls. The check is corrected and we added the
    `QueryCondition.check()` functions to unit tests.

--- tiledb/sm/query/query_condition.cc
+++ tiledb/sm/query/query_condition.cc
@@ -131,7 +131,8 @@ Status QueryCondition::check(const ArraySchema* const array_schema) const {
     }
 
     if (attribute->cell_size() != constants::var_size &&
-        attribute->cell_size() != condition_value_size) {
+        attribute->cell_size() != condition_value_size &&
+        !(attribute->nullable() && clause.condition_value_ == nullptr)) {
       return Status::QueryConditionError(
           "Clause condition value size mismatch: " +
           std::to_string(attribute->cell_size()) +
