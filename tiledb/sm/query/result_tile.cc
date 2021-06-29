3e9b7492530bb250d51e5352dbcdfcccaffab1bf
david.hoke <david.hoke@tiledb.com>
2021-06-29 16:37:17 -0400

    clang-format

--- tiledb/sm/query/result_tile.cc
+++ tiledb/sm/query/result_tile.cc
@@ -620,8 +620,8 @@ void ResultTile::compute_results_sparse<char>(
         uint64_t c_offset = 0, c_size = 0;
         for (uint64_t pos = first_c_pos; pos <= last_c_pos; ++pos) {
           c_offset = buff_off[pos];
-          c_size = (pos < coords_num-1) ? buff_off[pos + 1] - c_offset :
-                                        buff_str_size - c_offset;
+          c_size = (pos < coords_num - 1) ? buff_off[pos + 1] - c_offset :
+                                            buff_str_size - c_offset;
           r_bitmap[pos] = str_coord_intersects(
               c_offset, c_size, buff_str, range_start, range_end);
         }
