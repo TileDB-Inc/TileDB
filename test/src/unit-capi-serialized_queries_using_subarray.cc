82683433dd1e9582f521609da03184e875c8e1d9
david.hoke <david.hoke@tiledb.com>
2021-07-21 14:59:16 -0400

    more compensatory changes for Query/Reader/Subarray default layout change that came with merge of 'dev' - RestartCI 1

--- test/src/unit-capi-serialized_queries_using_subarray.cc
+++ test/src/unit-capi-serialized_queries_using_subarray.cc
@@ -471,6 +471,7 @@ TEST_CASE_METHOD(
     std::vector<int32_t> subarray = {1, 10, 1, 10};
 
     Subarray cppapi_subarray(ctx, array);
+    cppapi_subarray.set_layout(query.query_layout());
     cppapi_subarray.set_subarray(subarray);
     query.set_subarray(cppapi_subarray);
     query.set_buffer("a1", a1);
@@ -517,6 +518,7 @@ TEST_CASE_METHOD(
     std::vector<int32_t> subarray = {3, 4, 3, 4};
 
     Subarray cppapi_subarray(ctx, array);
+    cppapi_subarray.set_layout(query.query_layout());
     cppapi_subarray.set_subarray(subarray);
     query.set_subarray(cppapi_subarray);
     query.set_buffer("a1", a1);
@@ -562,6 +564,7 @@ TEST_CASE_METHOD(
     std::vector<uint64_t> a3_offsets(4);
     std::vector<int32_t> subarray = {3, 4, 3, 4};
     Subarray cppapi_subarray(ctx, array);
+    cppapi_subarray.set_layout(query.query_layout());
     cppapi_subarray.set_subarray(subarray);
     query.set_subarray(cppapi_subarray);
 
@@ -755,6 +758,7 @@ TEST_CASE_METHOD(
     std::vector<int32_t> subarray = {1, 10, 1, 10};
 
     Subarray cppapi_subarray(ctx, array);
+    cppapi_subarray.set_layout(query.query_layout());
     cppapi_subarray.add_range(0, subarray[0], subarray[1]);
     cppapi_subarray.add_range(1, subarray[2], subarray[3]);
     query.set_subarray(cppapi_subarray);
@@ -802,6 +806,7 @@ TEST_CASE_METHOD(
     std::vector<int32_t> subarray = {3, 4, 3, 4};
 
     Subarray cppapi_subarray(ctx, array);
+    cppapi_subarray.set_layout(query.query_layout());
     cppapi_subarray.add_range(0, subarray[0], subarray[1]);
     cppapi_subarray.add_range(1, subarray[2], subarray[3]);
     query.set_subarray(cppapi_subarray);
@@ -848,6 +853,7 @@ TEST_CASE_METHOD(
     std::vector<uint64_t> a3_offsets(4);
     std::vector<int32_t> subarray = {3, 4, 3, 4};
     Subarray cppapi_subarray(ctx, array);
+    cppapi_subarray.set_layout(query.query_layout());
     cppapi_subarray.add_range(0, subarray[0], subarray[1]);
     cppapi_subarray.add_range(1, subarray[2], subarray[3]);
     query.set_subarray(cppapi_subarray);
