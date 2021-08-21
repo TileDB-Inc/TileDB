629bea9e4846d8d5ff5fcf22e3b617d9dfa23f15
bin.deng <76831442+bdeng-xt@users.noreply.github.com>
2021-08-21 08:37:41 -0400

    Add tiledb_schema_evolution_t object for schema evolution (#2426)
    * initial version of tiledb_schema_evolution_t object
    
    * changed schema_evolution to array_schema_evolution
    
    * merged with dev and bd/ch8337-column-deletion
    
    * added unit tests for addition of column
    
    * format
    
    * change to use tdb_new

--- tiledb/common/status.h
+++ tiledb/common/status.h
@@ -86,6 +86,7 @@ enum class StatusCode : char {
   StorageManager,
   FragmentMetadata,
   ArraySchema,
+  ArraySchemaEvolution,
   Metadata,
   IO,
   Mem,
@@ -187,6 +188,11 @@ class Status {
   /** Return a ArraySchema error class Status with a given message **/
   static Status ArraySchemaError(const std::string& msg) {
     return Status(StatusCode::ArraySchema, msg, -1);
+  }
+
+  /** Return a ArraySchemaEvolution error class Status with a given message **/
+  static Status ArraySchemaEvolutionError(const std::string& msg) {
+    return Status(StatusCode::ArraySchemaEvolution, msg, -1);
   }
 
   /** Return a Metadata error class Status with a given message **/
