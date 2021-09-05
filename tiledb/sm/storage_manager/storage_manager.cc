cc30e159e7f652c5f28958c6bcf8ca3947286577
bdeng-xt <bdeng.xt@gmail.com>
2021-09-05 00:33:01 -0400

    fix the memory leak

--- tiledb/sm/storage_manager/storage_manager.cc
+++ tiledb/sm/storage_manager/storage_manager.cc
@@ -2253,7 +2253,7 @@ Status StorageManager::store_array_schema(
       0,
       &buff,
       false);
-  buff.disown_data();
+
   GenericTileIO tile_io(this, schema_uri);
   uint64_t nbytes;
   Status st = tile_io.write_generic(&tile, encryption_key, &nbytes);
