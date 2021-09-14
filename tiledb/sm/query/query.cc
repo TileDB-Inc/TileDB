de01e58eb52831c1b87d51d6d14f034a83436124
Seth Shelnutt <seth@tiledb.io>
2021-09-14 12:38:14 -0400

    Support empty string writes for dimensions

--- tiledb/sm/query/query.cc
+++ tiledb/sm/query/query.cc
@@ -184,13 +184,6 @@ Status Query::add_range_var(
     return LOG_STATUS(
         Status::QueryError("Cannot add range; Invalid dimension index"));
 
-  if (start == nullptr || end == nullptr)
-    return LOG_STATUS(Status::QueryError("Cannot add range; Invalid range"));
-
-  if (start_size == 0 || end_size == 0)
-    return LOG_STATUS(Status::QueryError(
-        "Cannot add range; Range start/end cannot have zero length"));
-
   if (!array_schema_->domain()->dimension(dim_idx)->var_size())
     return LOG_STATUS(
         Status::QueryError("Cannot add range; Range must be variable-sized"));
