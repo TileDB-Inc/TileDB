644625c510f31f6cb6c00209e54c3fec8e14b534
dhoke4tdb <david.hoke@tiledb.com>
2021-10-25 13:53:57 -0400

    Revert to state before many efforts trying to diagnoses s3/minio failures on GA windows runners
    This reverts commit c4f0d2cb390dfb275fd1cac731bac049d68a21c1.

--- scripts/install-minio.ps1
+++ scripts/install-minio.ps1
@@ -26,9 +26,6 @@ $TileDBRootDirectory = Split-Path -Parent (Get-ScriptsDirectory)
 $InstallPrefix = Join-Path $TileDBRootDirectory "dist"
 $StagingDirectory = Join-Path (Get-ScriptsDirectory) "deps-staging"
 $CertsDirectory = Join-Path $TileDBRootDirectory "test/inputs/test_certs"
-if(! (Test-Path $InstallPrefix)){
-  New-Item -ItemType Directory -Path $InstallPrefix -ea 0
-}
 New-Item -ItemType Directory -Path (Join-Path $InstallPrefix bin) -ea 0
 
 function DownloadFile([string] $URL, [string] $Dest) {
@@ -46,22 +43,16 @@ function DownloadIfNotExists([string] $Path, [string] $URL) {
 function Install-Minio {
     $MinioRoot = (Join-Path $StagingDirectory "minio")
     $DownloadMinioDest = Join-Path $MinioRoot "minio.exe"
-    $DownloadMinioMCDest = Join-Path $MinioRoot "mc.exe"
     if (!(Test-Path $MinioRoot)) {
         New-Item -ItemType Directory -Path $MinioRoot
         DownloadIfNotExists $DownloadMinioDest "https://dl.minio.io/server/minio/release/windows-amd64/minio.exe"
-        DownloadIfNotExists $DownloadMinioMCDest "https://dl.minio.io/client/mc/release/windows-amd64/mc.exe"
-                                                #"https://dl.min.io/client/mc/release/windows-amd64/mc.exe"
     }
     Copy-Item $DownloadMinioDest (Join-Path $InstallPrefix "bin")
-    Copy-Item $DownloadMinioMCDest (Join-Path $InstallPrefix "bin")
 }
 
 function Export-Env {
     $env:MINIO_ACCESS_KEY = "minio"
     $env:MINIO_SECRET_KEY = "miniosecretkey"
-    $env:MINIO_ROOT_USER = "minio"
-    $env:MINIO_ROOT_PASSWORD = "miniosecretkey"
     $env:AWS_ACCESS_KEY_ID = "minio"
     $env:AWS_SECRET_ACCESS_KEY = "miniosecretkey"
 }
@@ -76,22 +67,9 @@ function Run-Minio {
     if (!(Test-Path $ServerConfigDir)) {
         New-Item -ItemType Directory -Path $ServerConfigDir
     }
-    #Start-Process -FilePath $ExePath -ArgumentList "server --address 127.0.0.1:9999 --config-dir `"$ServerConfigDir`" --certs-dir `"$CertsDirectory`" `"$ServerDataDir`""
-    write-host "whats in CertsDirectory '$CertsDirectory'?"
-    ls $CertsDirectory
-    write-host "starting '$ExePath'"
-    $minioproc = Start-Process -FilePath $ExePath -ArgumentList "server --address 127.0.0.1:9999 --config-dir `"$ServerConfigDir`" --certs-dir `"$CertsDirectory`" `"$ServerDataDir`"" -PassThru -RedirectStandardError miniostderr.txt -RedirectStandardOutput miniostdout.txt
+    Start-Process -FilePath $ExePath -ArgumentList "server --address 127.0.0.1:9999 --config-dir `"$ServerConfigDir`" --certs-dir `"$CertsDir`" `"$ServerDataDir`""
+
     Start-Sleep 1.0
-    
-    write-host "miniostderr.txt"
-    type miniostderr.txt
-    write-host "miniostdout.txt"
-    type miniostdout.txt
-    
-    $minioproc.ID
-    tasklist
-    tasklist | findstr /i minio
-    Get-Process -id $minioproc.ID
 
     if ($PSVersionTable.PSVersion.Major -ge 6) {
         Write-Host "Checking Minio with '-SkipCertificateCheck':"
