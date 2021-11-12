ce42599c3366daf2ca8d0a43195957d65530847e
eric-hughes-tiledb <82400964+eric-hughes-tiledb@users.noreply.github.com>
2021-11-11 19:23:23 -0700

    Convert loose files thread_pool.* into a unit (#2520)
    * Convert loose files thread_pool.* into a unit.
    
    This change uses a proxy header for `thread_pool.h`, a change that can be considered transitional. There are open questions about how to specify common headers. They might require explicit paths in #include directives, or specifying include paths in the build, or some other mechanism, or perhaps even to stick with proxy headers in a common location.
    
    * Add `unit_thread_pool` to `tests` target
    
    * clang-format

--- tiledb/common/thread_pool/test/unit_thread_pool.h
+++ tiledb/common/thread_pool/test/unit_thread_pool.h
@@ -1,11 +1,11 @@
 /**
- * @file thread_pool.h
+ * @file tiledb/common/thread_pool/test/unit_thread_pool.h
  *
  * @section LICENSE
  *
  * The MIT License
  *
- * @copyright Copyright (c) 2018-2021 TileDB, Inc.
+ * @copyright Copyright (c) 2021 TileDB, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,6 +24,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ *
+ * @section DESCRIPTION
  */
 
-#include "tiledb/common/thread_pool/thread_pool.h"
+#include <catch.hpp>
