5f298690423e0b37bad23b3bb51a9f844f47f05b
Seth Shelnutt <seth@tiledb.io>
2021-11-19 17:48:07 -0500

    Update HISTORY and version for 2.4.4

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -73,7 +73,7 @@ author = 'TileDB, Inc.'
 # The short X.Y version.
 version = '2.4'
 # The full version, including alpha/beta/rc tags.
-release = '2.4.3'
+release = '2.4.4'
 
 # Breathe extension configuration.
 tiledb_dir = '../../'
