d80eb817a49602800fa5821f7a31547b8a9ce8bf
Isaiah Norton <isaiah@tiledb.io>
2021-11-30 11:41:35 -0500

    Bump dev tiledb version to 2.7.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -71,9 +71,9 @@ copyright = '2021 TileDB, Inc'
 author = 'TileDB, Inc.'
 
 # The short X.Y version.
-version = '2.5'
+version = '2.7'
 # The full version, including alpha/beta/rc tags.
-release = '2.5.0'
+release = '2.7.0'
 
 # Breathe extension configuration.
 tiledb_dir = '../../'
