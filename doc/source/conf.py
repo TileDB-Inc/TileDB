3449ca9838f30be287fbe63dfd9711f7778a9f22
Seth Shelnutt <seth@tiledb.io>
2021-11-30 19:08:35 -0500

    Update version to 2.6.0 for release-2.6 (#2672)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -71,9 +71,9 @@ copyright = '2021 TileDB, Inc'
 author = 'TileDB, Inc.'
 
 # The short X.Y version.
-version = '2.5'
+version = '2.6'
 # The full version, including alpha/beta/rc tags.
-release = '2.5.2'
+release = '2.6.0'
 
 # Breathe extension configuration.
 tiledb_dir = '../../'
