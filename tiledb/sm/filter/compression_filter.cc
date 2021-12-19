f87d800e2324c3a5df3ae9f4e4d542f9c6b70523
Ypatia Tsavliri <ypatia@tiledb.com>
2021-12-19 15:48:19 +0200

    Add and use blocking resource pool (#2735)
    * Add and use blocking resource pool, fixes sc12843
    
    * Fix multiple issues with locking
    
    * Use single lock to avoid races
    
    * Remove redundant predicate from wait

--- tiledb/sm/filter/compression_filter.cc
+++ tiledb/sm/filter/compression_filter.cc
@@ -436,7 +436,7 @@ void CompressionFilter::init_resource_pool(uint64_t size) {
   std::lock_guard g(zstd_decompress_ctx_pool_mtx_);
   if (zstd_decompress_ctx_pool_ == nullptr) {
     zstd_decompress_ctx_pool_ =
-        tdb::make_shared<ResourcePool<ZStd::ZSTD_Decompress_Context>>(
+        tdb::make_shared<BlockingResourcePool<ZStd::ZSTD_Decompress_Context>>(
             HERE(), size);
   }
 }
