92de8788d2d7411f25196667f5e6dd1687740205
bekadavis9 <31743465+bekadavis9@users.noreply.github.com>
2022-01-13 10:05:03 -0500

    Default compression filter level (#2811)
    * Set default filter compression levels
    
    * Remove defined constants
    
    * Add filter_compressor_level enum to default to INT_MIN
    
    * Change default level macro
    
    * Default level fix

--- tiledb/sm/c_api/tiledb.h
+++ tiledb/sm/c_api/tiledb.h
@@ -453,7 +453,7 @@ tiledb_vfs_mode_from_str(const char* str, tiledb_vfs_mode_t* vfs_mode);
 /** Out of memory */
 #define TILEDB_OOM (-2)
 /** Default compression level */
-#define TILEDB_COMPRESSION_FILTER_DEFAULT_LEVEL (30000)
+#define TILEDB_COMPRESSION_FILTER_DEFAULT_LEVEL (-30000)
 /**@}*/
 
 /**
