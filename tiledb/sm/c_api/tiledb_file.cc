dd4e1ff95296906cb812a886bf565c76837e1f37
david.hoke <david.hoke@tiledb.com>
2022-02-04 19:28:59 -0500

    first cut with 'file' APIs using tiledb_array_t parameter

--- tiledb/sm/c_api/tiledb_file.cc
+++ tiledb/sm/c_api/tiledb_file.cc
@@ -38,10 +38,258 @@
 #include "tiledb/appl/blob_array/blob_array.h"
 #include "tiledb/sm/c_api/tiledb_experimental.h"
 #include "tiledb/sm/c_api/tiledb_helpers.h"
+#include "tiledb/sm/c_api/tiledb_struct_def.h"
 //#include "tiledb/sm/file/file.h"
 #include "tiledb/sm/enums/query_type.h"
 #include "tiledb/sm/enums/encryption_type.h"
 
+
+// 'array' parameter based 'file' api
+TILEDB_EXPORT int32_t tiledb_array_as_file_obtain(
+    tiledb_ctx_t* ctx, tiledb_array_t** array, const char* array_uri, tiledb_config_t *config) {
+  if (sanity_check(ctx) == TILEDB_ERR) {
+    return TILEDB_ERR;
+  }
+  tiledb_array_schema_t* blob_array_schema = nullptr;
+  if (tiledb_array_schema_create_default_blob_array(ctx, &blob_array_schema) ==
+      TILEDB_ERR) {
+    return TILEDB_ERR;
+  }
+
+  if (tiledb_array_alloc(ctx, array_uri, array) == TILEDB_ERR) {
+    //expect tiledb_array_alloc() to have logged any necessary error.
+    tiledb_array_schema_free(&blob_array_schema);
+    return TILEDB_ERR;
+  }
+  // Check array URI
+  auto uri_array = tiledb::sm::URI(array_uri);
+  if (uri_array.is_invalid()) {
+    std::ostringstream errmsg;
+    errmsg << "Failed to create TileDB blob_array object; Invalid array URI \""
+           << uri_array.to_string() << "\"";
+    auto st = Status_Error(errmsg.str());
+    LOG_STATUS(st);
+    save_error(ctx, st);
+    return TILEDB_ERR;
+  }
+  auto stg_mgr = ctx->ctx_->storage_manager();
+  delete (*array)->array_; // release the 'default' allocated array, to be replaced
+  (*array)->array_ = new (std::nothrow) tiledb::appl::BlobArray(uri_array, stg_mgr);
+  if ((*array)->array_ == nullptr) {
+    std::ostringstream errmsg;
+    errmsg << "Failed to allocate TileDB blob_array object";
+    auto st = Status_Error(errmsg.str());
+    LOG_STATUS(st);
+    save_error(ctx, st);
+    tiledb_array_free(array); // already returning an error, ignore any error here.
+    tiledb_array_schema_free(&blob_array_schema);
+    *array = nullptr;
+    return TILEDB_OOM;
+  }
+
+  if (config) {
+    if (tiledb_array_set_config(ctx, *array, config) == TILEDB_ERR) {
+      delete *array;
+      *array = nullptr;
+      return TILEDB_ERR;
+    }
+  }
+
+  if (tiledb_array_open(ctx, *array, TILEDB_READ) == TILEDB_ERR) {
+    //auto cfg = config ? *config : stg_mgr->config();
+    const char* encryption_type_str = nullptr;
+    const char* encryption_key_str = nullptr;
+    tiledb_encryption_type_t encryption_type = TILEDB_NO_ENCRYPTION;
+    if (config) {
+      if (SAVE_ERROR_CATCH(
+              ctx, config->config_->get("sm.encryption_type", &encryption_type_str))) {
+        return TILEDB_ERR;
+      }
+      // tiledb::sm::EncryptionType encryption_type;
+      auto [st, et] = tiledb::sm::encryption_type_enum(encryption_type_str);
+      if (!st.ok()) {
+        return TILEDB_ERR;
+      }
+      encryption_type = static_cast<tiledb_encryption_type_t>(et.value());
+      if (SAVE_ERROR_CATCH(
+              ctx,
+              config->config_->get(
+                  "sm.encryption_key", &encryption_key_str))) {
+        return TILEDB_ERR;
+      }
+    }
+
+    if (tiledb_array_create_with_key(
+          ctx,
+          array_uri,
+          blob_array_schema,
+          encryption_type,
+          encryption_key_str,
+            encryption_type_str ?
+                static_cast<uint32_t>(strlen(encryption_key_str)) :
+                0) == TILEDB_ERR) {
+      tiledb_array_free(array);
+      tiledb_array_schema_free(&blob_array_schema);
+      *array = nullptr;
+      return TILEDB_ERR;
+    }
+  } else {
+    //TBD: verify that it is a blob_array entity!
+    // attribute 'contents' of type TILEDB_BLOB
+    // ...
+    if (tiledb_array_close(ctx, *array) == TILEDB_ERR) {
+      tiledb_array_free(array);
+      *array = nullptr;
+      tiledb_array_schema_free(&blob_array_schema);
+      return TILEDB_ERR;
+    }
+  }
+
+  // returning an allocated tiledb_array_t at '*array' having default blob_array schema;
+
+  return TILEDB_OK;
+}
+
+TILEDB_EXPORT int32_t tiledb_array_as_file_import(
+    tiledb_ctx_t* ctx,
+    tiledb_array_t* array,
+    const char* input_uri_filename) {
+  if (sanity_check(ctx, array) == TILEDB_ERR) {
+    return TILEDB_ERR;
+  }
+  //TDB: Validate that the array rudimentary blob_array requirements...
+  //... has attribute 'contents' of type TILEDB_BLOB
+
+  int32_t is_open = 0;
+  if (tiledb_array_is_open(ctx, array, &is_open) == TILEDB_ERR) {
+    return TILEDB_ERR;
+  }
+  if (is_open) {
+    tiledb_array_close(ctx, array);
+  }
+//  if (tiledb_array_open(ctx, array, TILEDB_WRITE) == TILEDB_ERR) {
+//    return TILEDB_ERR;
+//  }
+  //auto stg_mgr = ctx->ctx_->storage_manager();
+  //TBD: copy not desireable, was there a ref'ing method()
+  auto cfg = array->array_->config();
+  const char* encryption_type_str = nullptr;
+  const char* encryption_key_str = nullptr;
+  if (SAVE_ERROR_CATCH(
+          ctx, cfg.get("sm.encryption_type", &encryption_type_str))) {
+    return TILEDB_ERR;
+  }
+  // tiledb::sm::EncryptionType encryption_type;
+  auto [st, et] = tiledb::sm::encryption_type_enum(encryption_type_str);
+  if (!st.ok()) {
+    return TILEDB_ERR;
+  }
+  auto encryption_type = et.value();
+  if (SAVE_ERROR_CATCH(
+          ctx, cfg.get("sm.encryption_key", &encryption_key_str))) {
+    return TILEDB_ERR;
+  }
+
+  if (tiledb_array_open_with_key(ctx, array, TILEDB_WRITE, static_cast<tiledb_encryption_type_t>(encryption_type), encryption_key_str, static_cast<uint32_t>(strlen(encryption_key_str))) == TILEDB_ERR) {
+    return TILEDB_ERR;
+  }
+
+  auto uri_filename = tiledb::sm::URI(input_uri_filename);
+  if (uri_filename.is_invalid()) {
+    std::ostringstream errmsg;
+    errmsg << "Failed to create TileDB file object; Invalid filename URI \""
+           << uri_filename.to_string() << "\"";
+    auto st = Status_Error(errmsg.str());
+    LOG_STATUS(st);
+    save_error(ctx, st);
+    return TILEDB_ERR;
+  }
+
+  auto blob_array = static_cast<tiledb::appl::BlobArray*>(array->array_);
+
+  int32_t ret_stat = TILEDB_OK;
+  if (SAVE_ERROR_CATCH(ctx, blob_array->save_from_uri(uri_filename, nullptr))) {
+    ret_stat = TILEDB_ERR;
+  }
+
+  //TBD: close or leave open? (may have error (saving) or no error
+  tiledb_array_close(ctx, array);
+
+  return ret_stat;
+}
+
+TILEDB_EXPORT int32_t tiledb_array_as_file_export(
+    tiledb_ctx_t* ctx,
+    tiledb_array_t* array,
+    const char* output_uri_filename) {
+  if (sanity_check(ctx, array) == TILEDB_ERR) {
+    return TILEDB_ERR;
+  }
+  int32_t is_open;
+  if (tiledb_array_is_open(ctx, array, &is_open) == TILEDB_ERR) {
+    tiledb_array_close(ctx, array);
+  }
+  if (tiledb_array_open(ctx, array, TILEDB_READ) == TILEDB_ERR) {
+    return TILEDB_ERR;
+  }
+
+  auto uri_filename = tiledb::sm::URI(output_uri_filename);
+  if (uri_filename.is_invalid()) {
+    std::ostringstream errmsg;
+    errmsg << "Failed to create TileDB file object; Invalid filename URI \""
+           << uri_filename.to_string() << "\"";
+    auto st = Status_Error(errmsg.str());
+    LOG_STATUS(st);
+    save_error(ctx, st);
+    return TILEDB_ERR;
+  }
+
+  auto blob_array = static_cast<tiledb::appl::BlobArray*>(array->array_);
+  int32_t ret_stat = TILEDB_OK;
+  if (SAVE_ERROR_CATCH(ctx, blob_array->export_to_uri(uri_filename, nullptr))) {
+    ret_stat = TILEDB_ERR;
+  }
+
+  // TBD: close or leave open? (may have error (saving) or no error
+  tiledb_array_close(ctx, array);
+
+  return TILEDB_OK;
+}
+
+// stretch - but we need underlying schema created, tho' guess can do directly
+// as done inside here.
+TILEDB_EXPORT int32_t tiledb_array_schema_create_default_blob_array(
+    tiledb_ctx_t* ctx, tiledb_array_schema_t** array_schema) {
+  if (sanity_check(ctx) == TILEDB_ERR)
+    return TILEDB_ERR;
+
+  // Create array schema struct
+  *array_schema = new (std::nothrow) tiledb_array_schema_t;
+  if (*array_schema == nullptr) {
+    auto st = Status_Error("Failed to allocate TileDB array schema object");
+    LOG_STATUS(st);
+    save_error(ctx, st);
+    return TILEDB_OOM;
+  }
+
+  // Create a new ArraySchema object
+  (*array_schema)->array_schema_ =
+      new (std::nothrow) tiledb::appl::BlobArraySchema();
+  if ((*array_schema)->array_schema_ == nullptr) {
+    delete *array_schema;
+    *array_schema = nullptr;
+    auto st = Status_Error("Failed to allocate TileDB array schema object");
+    LOG_STATUS(st);
+    save_error(ctx, st);
+    return TILEDB_OOM;
+  }
+
+  // Success
+  return TILEDB_OK;
+}
+
+#if 0
+// original story API
 TILEDB_EXPORT int32_t
 tiledb_array_as_file_create(
     tiledb_ctx_t* ctx,
@@ -341,37 +589,7 @@ TILEDB_EXPORT int32_t tiledb_array_as_file_export(
   return TILEDB_OK;
 }
 
-//stretch - but we need underlying schema created, tho' guess can do directly as done inside here.
-TILEDB_EXPORT int32_t tiledb_array_schema_create_default_file(
-    tiledb_ctx_t* ctx,
-    tiledb_array_schema_t** array_schema) {
-  if (sanity_check(ctx) == TILEDB_ERR)
-    return TILEDB_ERR;
-
-  // Create array schema struct
-  *array_schema = new (std::nothrow) tiledb_array_schema_t;
-  if (*array_schema == nullptr) {
-    auto st = Status_Error("Failed to allocate TileDB array schema object");
-    LOG_STATUS(st);
-    save_error(ctx, st);
-    return TILEDB_OOM;
-  }
-
-  // Create a new ArraySchema object
-  (*array_schema)->array_schema_ = new (std::nothrow)
-      tiledb::appl::BlobArraySchema();
-  if ((*array_schema)->array_schema_ == nullptr) {
-    delete *array_schema;
-    *array_schema = nullptr;
-    auto st = Status_Error("Failed to allocate TileDB array schema object");
-    LOG_STATUS(st);
-    save_error(ctx, st);
-    return TILEDB_OOM;
-  }
-
-  // Success
-  return TILEDB_OK;
-}
+#endif
 #else
 //Seth's preliminary implementation.
 #include "tiledb/sm/c_api/tiledb_experimental.h"
