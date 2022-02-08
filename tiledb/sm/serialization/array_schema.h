f0dff28ba37df1a482a779433ddf84106cdce5c1
Rebekah <bekadavis9@gmail.com>
2022-02-08 16:01:30 -0500

    Fix Windows VS Serialization Build

--- tiledb/sm/serialization/array_schema.h
+++ tiledb/sm/serialization/array_schema.h
@@ -37,9 +37,13 @@
 
 #include "tiledb/common/status.h"
 
+// clang-format off
 #ifdef TILEDB_SERIALIZATION
+#include <capnp/message.h>
+#include <capnp/serialize.h>
 #include "tiledb/sm/serialization/capnp_utils.h"
 #endif
+// clang-format on
 
 using namespace tiledb::common;
 
