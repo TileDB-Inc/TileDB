91d84f0c53aa9fdf042ee5884add875969b8ce31
Ypatia Tsavliri <ypatia@tiledb.com>
2022-02-11 11:12:16 +0200

    Separate tests and add in object library

--- tiledb/sm/compressors/dict_compressor.h
+++ tiledb/sm/compressors/dict_compressor.h
@@ -53,8 +53,8 @@ class DictEncoding {
    * @tparam T Type of integer that can fit the word ids
    * @param input_buffer Input in form of a memory contiguous sequence of
    * strings
-   * @param Dictionary-encoded output in [N|id1|id2..|idN], each id of size T.
-   * Memory is allocated and owned by the caller
+   * @param Dictionary-encoded output in ids of type T. Memory is allocated and
+   * owned by the caller
    * @return A dictionary in the form of a vector of strings, where indices are
    * the word_ids of each word
    */
@@ -68,19 +68,18 @@ class DictEncoding {
     std::vector<std::string> dict;
     dict.reserve(input.size());
     T word_id = 0;
-    auto out_offset = &output[0];
     // TODO: I can't use unordered_map with string_view before C++20 (I think),
     // more details here:
     // https://stackoverflow.com/questions/34596768/stdunordered-mapfind-using-a-type-different-than-the-key-type/53530846#53530846
-    std::map<std::string_view, T, std::less<>> helper_dict;
-    for (auto word : input) {
-      if (helper_dict.find(word) == helper_dict.end()) {
-        helper_dict[word] = word_id++;
-        dict.emplace_back(std::string(word));
+    std::map<std::string_view, T, std::less<>> word_ids;
+    for (uint64_t i = 0; i < input.size(); i++) {
+      // if we haven't seen that string before, add it to the dictionary
+      if (word_ids.find(input[i]) == word_ids.end()) {
+        dict.emplace_back(std::string(input[i]));
+        word_ids[input[i]] = word_id++;
       }
 
-      utils::endianness::encode_be<T>(helper_dict[word], out_offset);
-      out_offset += sizeof(T);
+      utils::endianness::encode_be<T>(word_ids[input[i]], &output[i]);
     }
 
     // TODO: copy happens here: improve? take it as input?
@@ -91,10 +90,9 @@ class DictEncoding {
    * Decompress strings that are encoded in dictionary format
    *
    * @tparam T Type of integer that can fit the word ids
-   * @param input_buffer Input in [run length|string size|string] RLE format to
-   * decompress
-   * @param The dictionary to be used for decoding a word_id into a string.
-   * Expected type is a vector of strings, where indices are the word_ids of
+   * @param input_buffer Input dictionary-encoded format of ids of type T
+   * @param The dictionary to be used for decoding a word id into a string.
+   * Expected type is a vector of strings, where indices are the ids of
    * each word
    * @param Dictionary-encoded output as a series of ids of size T. Memory is
    * allocated and owned by the caller
@@ -104,17 +102,16 @@ class DictEncoding {
       const span<T> input,
       const std::vector<std::string>& dict,
       span<std::byte> output) {
-    if (input.empty() || output.empty()) {
+    if (input.empty() || output.empty() || dict.size() == 0) {
       return;
     }
 
     T word_id = 0;
     uint64_t in_index = 0, out_index = 0;
     while (in_index < input.size()) {
-      word_id = utils::endianness::decode_be<T>(&input[in_index]);
-      in_index += sizeof(T);
+      word_id = utils::endianness::decode_be<T>(&input[in_index++]);
       assert(word_id < dict.size());
-      auto word = dict[word_id];
+      auto word = std::string_view(dict[word_id]);
       memcpy(&output[out_index], word.data(), word.size());
       out_index += word.size();
     }
