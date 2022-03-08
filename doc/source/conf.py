8ef65636291f603a4b3b6bfd203ff9a687bf9b5a
Seth Shelnutt <Shelnutt2@gmail.com>
2022-03-08 06:42:12 -0500

    Update HISTORY for 2.6.4

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -73,7 +73,7 @@ author = 'TileDB, Inc.'
 # The short X.Y version.
 version = '2.6'
 # The full version, including alpha/beta/rc tags.
-release = '2.6.3'
+release = '2.6.4'
 
 # Breathe extension configuration.
 tiledb_dir = '../../'
