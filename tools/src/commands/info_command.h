d6ea86854b24d0680a97fc414af522876d541625
Eric Hughes <eric.hughes@tiledb.com>
2022-03-30 08:17:02 -0600

    clang-format

--- tools/src/commands/info_command.h
+++ tools/src/commands/info_command.h
@@ -86,8 +86,7 @@ class InfoCommand : public Command {
 
   /** Converts an opaque MBR to a 2D (double) rectangle. */
   std::tuple<double, double, double, double> get_mbr(
-      const sm::NDRange& mbr,
-      const tiledb::sm::Domain& domain) const;
+      const sm::NDRange& mbr, const tiledb::sm::Domain& domain) const;
 
   /**
    * Converts an opaque MBR to a string vector. The vector contents are strings:
@@ -98,8 +97,7 @@ class InfoCommand : public Command {
    * @return String vector representation of MBR.
    */
   std::vector<std::string> mbr_to_string(
-      const sm::NDRange& mbr,
-      const tiledb::sm::Domain& domain) const;
+      const sm::NDRange& mbr, const tiledb::sm::Domain& domain) const;
 };
 
 }  // namespace cli
