6ecf2327b4b854dd48029ddb1e1cc3e8bf35f9f1
Vivian Nguyen <vivian@tiledb.com>
2022-04-14 08:15:36 -0500

    Sparse Index Reader Fix: Check For Empty Buffer (#3051)
    * Sparse Index Reader Fix: Check For Empty Buffer
    
    * Check if the buffer is empty in `SparseIndexReaderBase::add_extra_offset`
      first. Otherwise the dest in `memcpy` will point to a location
      `offsets_bytesize()` prior to the start of the buffer.
    
    * Addressing feedback from @ypatia.
    
    Co-authored-by: Luc Rancourt <lucrancourt@gmail.com>

--- tiledb/sm/query/sparse_index_reader_base.cc
+++ tiledb/sm/query/sparse_index_reader_base.cc
@@ -670,6 +670,11 @@ Status SparseIndexReaderBase::add_extra_offset() {
     if (!array_schema_.var_size(name))
       continue;
 
+    // Do not apply offset for empty results because we will
+    // write backwards and corrupt memory we don't own.
+    if (*it.second.buffer_size_ == 0)
+      continue;
+
     auto buffer = static_cast<unsigned char*>(it.second.buffer_);
     if (offsets_format_mode_ == "bytes") {
       memcpy(
