4e5b59eb87782900524dbddae3b8438a7c45a320
Luc Rancourt <lucrancourt@gmail.com>
2022-05-10 11:53:32 +0200

    Move real unit tests to proper directory.
    ---
    TYPE: IMPROVEMENT
    DESC: Move real unit tests to proper directory.

--- tiledb/sm/query/test/unit_validity_vector.cc
+++ tiledb/sm/query/test/unit_validity_vector.cc
@@ -1,11 +1,11 @@
 /**
- * @file unit-ValidityVector.cc
+ * @file unit_validity_vector.cc
  *
  * @section LICENSE
  *
  * The MIT License
  *
- * @copyright Copyright (c) 2020-2021 TileDB, Inc.
+ * @copyright Copyright (c) 2022 TileDB, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
