a81b050e1b9034ee9eebb618fe556b43c5ae31de
abigalekim <abigale.kim@tiledb.com>
2022-05-11 00:15:36 -0400

    wip

--- tiledb/sm/query/ast/test/unit-query-ast.cc
+++ tiledb/sm/query/ast/test/unit-query-ast.cc
@@ -31,8 +31,8 @@
  */
 #include <iostream>
 
-#include "test/src/helpers.h"
 #include "tiledb/common/common.h"
+#include "test/src/helpers.h"
 #include "tiledb/sm/query/ast/query_ast.h"
 
 #include "catch.hpp"
@@ -44,14 +44,14 @@ TEST_CASE("ASTNode Constructors", "[QueryCondition][ast][constructor]") {
   // ASTNodeVal constructor
   std::string field_name = "x";
   int val = 5;
-  auto node_val = tdb_unique_ptr<ASTNodeVal>(
+  auto node_val = tdb_unique_ptr<ASTNode>(
       tdb_new(ASTNodeVal, field_name, &val, sizeof(val), QueryConditionOp::LT));
   REQUIRE(ast_node_to_str(node_val) == "x LT 05 00 00 00");
 
   // ASTNodeVal constructor
   std::string field_name1 = "y";
   int val1 = 3;
-  auto node_val1 = tdb_unique_ptr<ASTNodeVal>(tdb_new(
+  auto node_val1 = tdb_unique_ptr<ASTNode>(tdb_new(
       ASTNodeVal, field_name1, &val1, sizeof(val1), QueryConditionOp::GT));
   REQUIRE(ast_node_to_str(node_val1) == "y GT 03 00 00 00");
 
@@ -65,27 +65,27 @@ TEST_CASE("ASTNode Constructors", "[QueryCondition][ast][constructor]") {
   // ASTNodeVal constructor
   std::string field_name2 = "a";
   int val2 = 23;
-  auto node_val2 = tdb_unique_ptr<ASTNodeVal>(tdb_new(
+  auto node_val2 = tdb_unique_ptr<ASTNode>(tdb_new(
       ASTNodeVal, field_name2, &val2, sizeof(val2), QueryConditionOp::EQ));
   REQUIRE(ast_node_to_str(node_val2) == "a EQ 17 00 00 00");
 
   // ASTNodeVal constructor
   std::string field_name3 = "b";
   int val3 = 2;
-  auto node_val3 = tdb_unique_ptr<ASTNodeVal>(tdb_new(
+  auto node_val3 = tdb_unique_ptr<ASTNode>(tdb_new(
       ASTNodeVal, field_name3, &val3, sizeof(val3), QueryConditionOp::NE));
   REQUIRE(ast_node_to_str(node_val3) == "b NE 02 00 00 00");
 
   std::string field_name4 = "c";
   int val4 = 8;
-  auto node_val4 = tdb_unique_ptr<ASTNodeVal>(tdb_new(
+  auto node_val4 = tdb_unique_ptr<ASTNode>(tdb_new(
       ASTNodeVal, field_name4, &val4, sizeof(val4), QueryConditionOp::LE));
   REQUIRE(ast_node_to_str(node_val4) == "c LE 08 00 00 00");
 
   auto combined_node_inter1 =
       node_val2->combine(node_val3, QueryConditionCombinationOp::OR);
   auto combined_node1 =
-      node_inter1->combine(node_val4, QueryConditionCombinationOp::OR);
+      combined_node_inter1->combine(node_val4, QueryConditionCombinationOp::OR);
   REQUIRE(
       ast_node_to_str(combined_node1) ==
       "(a EQ 17 00 00 00 OR b NE 02 00 00 00 OR c LE 08 00 00 00)");
@@ -107,21 +107,21 @@ TEST_CASE("ASTNode Constructors", "[QueryCondition][ast][constructor]") {
 TEST_CASE(
     "AST Constructors with depth > 2", "[QueryCondition][ast][constructor]") {
   std::vector<int> vals = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  std::vector<tdb_unique_ptr<ASTNodeVal>> ast_value_vector;
+  std::vector<tdb_unique_ptr<ASTNode>> ast_value_vector;
   for (size_t i = 0; i < 7; ++i) {
-    auto node_val = tdb_unique_ptr<ASTNodeVal>(tdb_new(
+    auto node_val = tdb_unique_ptr<ASTNode>(tdb_new(
         ASTNodeVal, "x", &vals[i], sizeof(vals[i]), QueryConditionOp::EQ));
     ast_value_vector.push_back(std::move(node_val));
   }
 
   for (size_t i = 7; i < vals.size(); ++i) {
-    auto node_val = tdb_unique_ptr<ASTNodeVal>(tdb_new(
+    auto node_val = tdb_unique_ptr<ASTNode>(tdb_new(
         ASTNodeVal, "x", &vals[i], sizeof(vals[i]), QueryConditionOp::NE));
     ast_value_vector.push_back(std::move(node_val));
   }
 
   int x = 6;
-  auto x_neq_six = tdb_unique_ptr<ASTNodeVal>(
+  auto x_neq_six = tdb_unique_ptr<ASTNode>(
       tdb_new(ASTNodeVal, "x", &x, sizeof(x), QueryConditionOp::NE));
 
   auto one_or_two = ast_value_vector[0]->clone()->combine(
