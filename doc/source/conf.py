1855f7ccf79618ad06268c9839e7c3d86ff208ea
Isaiah Norton <isaiah.norton@gmail.com>
2022-05-18 17:03:10 -0400

    Update HISTORY for 2.9.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -71,9 +71,9 @@ copyright = '2021 TileDB, Inc'
 author = 'TileDB, Inc.'
 
 # The short X.Y version.
-version = '2.9'
+version = '2.9.1'
 # The full version, including alpha/beta/rc tags.
-release = '2.9.0'
+release = '2.9.1'
 
 # Breathe extension configuration.
 tiledb_dir = '../../'
