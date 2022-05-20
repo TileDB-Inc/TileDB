a44d43363f8e15cf0868ce6a791beb7809e46eb8
Vivian Nguyen <vivian@tiledb.com>
2022-05-20 08:45:02 -0500

    Update Zlib Download URL

--- cmake/Modules/FindZlib_EP.cmake
+++ cmake/Modules/FindZlib_EP.cmake
@@ -83,8 +83,8 @@ if (NOT ZLIB_FOUND)
 
     ExternalProject_Add(ep_zlib
       PREFIX "externals"
-      URL "http://iweb.dl.sourceforge.net/project/libpng/zlib/1.2.11/zlib1211.zip"
-      URL_HASH SHA1=bccd93ad3cee39c3d08eee68d45b3e11910299f2
+      URL "https://downloads.sourceforge.net/project/libpng/zlib/1.2.11/zlib1211.zip"
+      URL_HASH SHA1=b8d230cc5effe6dffa2807e0e342245b0683f2df
       DOWNLOAD_NAME "zlib1211.zip"
       CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=${TILEDB_EP_INSTALL_PREFIX}
