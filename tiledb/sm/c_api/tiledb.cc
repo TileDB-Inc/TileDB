4a6fcedcd0f7cedaced953f37d9845da131e75ab
Rebekah <bekadavis9@gmail.com>
2022-05-25 17:17:54 -0400

    Catch std::bad_alloc

--- tiledb/sm/c_api/tiledb.cc
+++ tiledb/sm/c_api/tiledb.cc
@@ -5915,13 +5915,13 @@ int32_t tiledb_deserialize_array(
   try {
     (*array)->array_ = make_shared<tiledb::sm::Array>(
         HERE(), uri, ctx->ctx_->storage_manager());
-  } catch (...) {
+  } catch (std::bad_alloc&) {
     auto st = Status_Error(
         "Failed to create TileDB array object; Memory allocation "
         "error");
     LOG_STATUS(st);
     save_error(ctx, st);
-    return TILEDB_ERR;
+    return TILEDB_OOM;
   }
 
   if (SAVE_ERROR_CATCH(
