3ee8dd314854cbd3d217d9c4565f049f955181ea
Luc Rancourt <lucrancourt@gmail.com>
2022-06-23 09:00:52 +0300

    Addressing feedback from @ihnorton, part 2.

--- tiledb/sm/query/query_condition.cc
+++ tiledb/sm/query/query_condition.cc
@@ -1672,31 +1672,14 @@ void QueryCondition::apply_ast_node_sparse(
     ResultTile& result_tile,
     CombinationOp combination_op,
     std::vector<BitmapType>& result_bitmap) const {
-  bool var_size = false, nullable = false;
-
-  // Initialize to timestamps type.
-  Datatype type = Datatype::UINT64;
   std::string node_field_name = node->get_field_name();
-  if (node_field_name != constants::timestamps) {
-    if (array_schema.is_dim(node_field_name)) {
-      const auto dim_ptr = array_schema.dimension_ptr(node_field_name);
-      var_size = dim_ptr->var_size();
-      type = dim_ptr->type();
-    } else {
-      const auto attribute = array_schema.attribute(node_field_name);
-      if (!attribute) {
-        throw std::runtime_error("Unknown attribute " + node_field_name);
-      }
-
-      var_size = attribute->var_size();
-      nullable = attribute->nullable();
-      type = attribute->type();
-    }
-  }
+  const auto nullable = array_schema.is_nullable(node_field_name);
+  const auto var_size = array_schema.var_size(node_field_name);
+  const auto type = array_schema.type(node_field_name);
 
   // Process the validity buffer now.
   if (nullable) {
-    const auto tile_tuple = result_tile.tile_tuple(node->get_field_name());
+    const auto tile_tuple = result_tile.tile_tuple(node_field_name);
     const auto& tile_validity = std::get<2>(*tile_tuple);
     const auto buffer_validity = static_cast<uint8_t*>(tile_validity.data());
 
