29405c0a5fa386a2ba3d0a32ecf2376abab05408
Ypatia Tsavliri <ypatia@tiledb.com>
2022-06-29 16:19:45 +0300

    Add configuration variables for new array open (#3321)

--- tiledb/sm/cpp_api/config.h
+++ tiledb/sm/cpp_api/config.h
@@ -704,13 +704,25 @@ class Config {
    *    failed REST request <br>
    *    **Default**: 1.25
    * - `rest.curl.verbose` <br>
-   * Set curl to run in verbose mode for REST requests <br>
-   * curl will print to stdout with this option
+   *    Set curl to run in verbose mode for REST requests <br>
+   *    curl will print to stdout with this option
+   *    **Default**: false
+   * - `rest.load_metadata_on_array_open` <br>
+   *    If true, array metadata will be loaded and sent to server together with
+   *    the open array <br>
+   *    **Default**: true
+   * - `rest.load_non_empty_domain_on_array_open` <br>
+   *    If true, array non empty domain will be loaded and sent to server
+   *    together with the open array <br>
+   *    **Default**: true
+   * - `rest.use_refactored_array_open` <br>
+   *    If true, the new, experimental REST routes and APIs for opening an array
+   *    will be used <br>
    *    **Default**: false
    * - `filestore.buffer_size` <br>
    *    Specifies the size in bytes of the internal buffers used in the
-   * filestore API. The size should be bigger than the minimum tile size
-   * filestore currently supports, that is currently 1024bytes. <br>
+   *    filestore API. The size should be bigger than the minimum tile size
+   *    filestore currently supports, that is currently 1024bytes. <br>
    *    **Default**: 100MB
    */
   Config& set(const std::string& param, const std::string& value) {
