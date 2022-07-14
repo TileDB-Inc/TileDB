33d2775a84833c2c1ebef49d3cdfee0d8bfbbf1c
Isaiah Norton <isaiah@tiledb.io>
2022-07-14 08:36:26 -0400

    Use zlib from tiledb-deps-mirror with fallback
    Part of SC-11085

--- cmake/Modules/FindZlib_EP.cmake
+++ cmake/Modules/FindZlib_EP.cmake
@@ -83,7 +83,9 @@ if (NOT ZLIB_FOUND)
 
     ExternalProject_Add(ep_zlib
       PREFIX "externals"
-      URL "https://downloads.sourceforge.net/project/libpng/zlib/1.2.11/zlib1211.zip"
+      URL
+        "https://github.com/TileDB-Inc/tiledb-deps-mirror/releases/download/2.10-deps/zlib1211.zip"
+        "https://downloads.sourceforge.net/project/libpng/zlib/1.2.11/zlib1211.zip"
       URL_HASH SHA1=bccd93ad3cee39c3d08eee68d45b3e11910299f2
       DOWNLOAD_NAME "zlib1211.zip"
       CMAKE_ARGS
