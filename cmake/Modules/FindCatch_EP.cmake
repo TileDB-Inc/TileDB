f296529cfa97d5091b737888240918c1d884c574
dhoke4tdb <david.hoke@tiledb.com>
2022-09-02 12:06:40 -0400

    remove kruft, reactivate unit_interval after making necessary change

--- cmake/Modules/FindCatch_EP.cmake
+++ cmake/Modules/FindCatch_EP.cmake
@@ -42,35 +42,22 @@ if (NOT TILEDB_FORCE_ALL_DEPS OR TILEDB_CATCH_EP_BUILT)
   find_path(CATCH_INCLUDE_DIR
     NAMES catch2/catch_all.hpp
     PATHS ${CATCH_PATHS}
-    #PATH_SUFFIXES "catch2"
     ${TILEDB_DEPS_NO_DEFAULT_PATH}
   )
 endif()
 
-if(0)
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Catch2
-  REQUIRED_VARS 
-    CATCH_INCLUDE_DIR 
-    #CATCH_LIBRARIES
-)
-else()
-  find_package(Catch2
-    HINTS
-      ${CATCH_PATHS}
-    )
-  if(Catch2_FOUND)
-    set(CATCH_INCLUDE_DIR ${Catch2_INCLUDE_DIR})
-    set(CATCH_LIBRARIES ${Catch2_LIBRARIES})
-  endif()
+find_package(Catch2
+  HINTS
+    ${CATCH_PATHS}
+  )
+if(Catch2_FOUND)
+  set(CATCH_INCLUDE_DIR ${Catch2_INCLUDE_DIR})
+  set(CATCH_LIBRARIES ${Catch2_LIBRARIES})
 endif()
 
 message("CATCH_INCLUDE_DIR is \"${CATCH_INCLUDE_DIR}\", CATCH_LIBRARIES is \"${CATCH_LIBRARIES}\"")
 
 if(Catch2_FOUND)
-  #print_target_properties(Catch2)
-  print_target_properties(Catch2::Catch2WithMain)
-  print_target_properties(Catch2::Catch2)
   set(TILEDB_CATCH2_SOURCES_DIR ${TILEDB_EP_SOURCE_DIR}/ep_catch/src/catch2)
   message("Catch2_FOUND is ${Catch2_FOUND}, CATCH_INCLUDE_DIR is \"${CATCH_INCLUDE_DIR}\", CATCH_LIBRARIES is \"${CATCH_LIBRARIES}\"")
 else()
@@ -83,8 +70,6 @@ if (NOT CATCH2_FOUND AND TILEDB_SUPERBUILD)
     PREFIX "externals"
     # Set download name to avoid collisions with only the version number in the filename
     DOWNLOAD_NAME ep_catch.zip
-    #URL "https://github.com/catchorg/Catch2/archive/v2.13.8.zip"
-    #URL_HASH SHA1=73adf43795abb7f481f07d307d11ac1fbc7a6015
     URL "https://github.com/catchorg/Catch2/archive/v3.1.0.zip"
     URL_HASH SHA1=b23753594a743feabd4e30f83b31ebb31081092a
     CMAKE_ARGS
@@ -93,9 +78,6 @@ if (NOT CATCH2_FOUND AND TILEDB_SUPERBUILD)
       # https://stackoverflow.com/questions/66227246/catch2-undefined-reference-to-catchstringmaker
       # catch build reportedly defaults to c++14, apparently building as cxx17 avoids...
       -DCMAKE_CXX_STANDARD=17 # to avoid undefined ...Catch::StringMaker
-    #CONFIGURE_COMMAND ""
-    #BUILD_COMMAND ""
-    #INSTALL_COMMAND ""
     UPDATE_COMMAND ""
     LOG_DOWNLOAD TRUE
     LOG_OUTPUT_ON_FAILURE ${TILEDB_LOG_OUTPUT_ON_FAILURE}
