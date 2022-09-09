a392d1afe8622280a967ecbf4b27a9f5eb07b01f
abigalekim <abigale.kim@tiledb.com>
2022-09-08 22:57:39 -0400

    test wip

--- test/src/unit-cppapi-bitsort-filter.cc
+++ test/src/unit-cppapi-bitsort-filter.cc
@@ -30,3 +30,126 @@
  * Tests the C++ API for bitsort filter related functions.
  */
 
+#include <random>
+#include <vector>
+
+#include "catch.hpp"
+#include "tiledb/common/common.h"
+#include "tiledb/sm/cpp_api/tiledb"
+
+using namespace tiledb;
+
+std::string bitsort_array_name = "cpp_unit_array";
+int bitsort_dim_hi = 10;
+
+template <typename T>
+void bitsort_filter_api_test(Context& ctx, tiledb_array_type_t array_type) {
+  Domain domain(ctx);
+  auto d1 = Dimension::create<int>(ctx, "x", {{1, bitsort_dim_hi}}, 4);
+  auto d2 = Dimension::create<int>(ctx, "y", {{1, bitsort_dim_hi}}, 4);
+  auto d3 = Dimension::create<int>(ctx, "z", {{1, bitsort_dim_hi}}, 4);
+  domain.add_dimensions(d1, d2, d3);
+
+  Filter f(ctx, TILEDB_FILTER_BITSORT);
+
+  FilterList filters(ctx);
+  filters.add_filter(f);
+
+  auto a = Attribute::create<T>(ctx, "a");
+  a.set_filter_list(filters);
+
+  ArraySchema schema(ctx, array_type);
+  schema.set_domain(domain);
+  schema.add_attribute(a);
+  Array::create(bitsort_array_name, schema);
+
+  // Setting up the random number generator for the bitsort filter testing.
+  std::mt19937_64 gen(0xADA65ED6);
+  std::uniform_int_distribution<int64_t> dis(
+      std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
+
+  std::vector<int> x_dims;
+  std::vector<int> y_dims;
+  std::vector<int> z_dims;
+  std::vector<T> a_write;
+  std::vector<T> expected_a;
+  for (int x = 0; x < bitsort_dim_hi; ++x) {
+    for (int y = 0; y < bitsort_dim_hi; ++y) {
+        for (int z = 0; z < bitsort_dim_hi; ++z) {
+            x_dims.push_back(x + 1);
+            y_dims.push_back(y + 1);
+            z_dims.push_back(z + 1);
+
+            T f = static_cast<T>(dis(gen));
+            a_write.push_back(f);
+            expected_a.push_back(f);
+        }
+    }
+  }
+
+  tiledb_layout_t layout_type =
+      array_type == TILEDB_SPARSE ? TILEDB_UNORDERED : TILEDB_ROW_MAJOR;
+
+  Array array_w(ctx, bitsort_array_name, TILEDB_WRITE);
+  Query query_w(ctx, array_w);
+  query_w.set_layout(layout_type).set_data_buffer("a", a_write);
+
+  if (array_type == TILEDB_SPARSE) {
+    query_w.set_data_buffer("x", x_dims).set_data_buffer("y", y_dims).set_data_buffer("z", z_dims);
+  }
+
+  query_w.submit();
+  query_w.finalize();
+  array_w.close();
+
+  // Open and read the entire array.
+  std::vector<T> a_data_read(bitsort_dim_hi * bitsort_dim_hi, 0);
+  Array array_r(ctx, bitsort_array_name, TILEDB_READ);
+  Query query_r(ctx, array_r);
+  query_r.set_layout(TILEDB_ROW_MAJOR).set_data_buffer("a", a_data_read);
+
+  if (array_type == TILEDB_DENSE) {
+    int range[] = {1, bitsort_dim_hi};
+    query_r.add_range("x", range[0], range[1])
+        .add_range("y", range[0], range[1])
+        .add_range("z", range[0], range[1]);
+  }
+
+  query_r.submit();
+
+  // Check for results.
+  size_t total_num_elements = static_cast<size_t>(bitsort_dim_hi * bitsort_dim_hi);
+  auto table = query_r.result_buffer_elements();
+  REQUIRE(table.size() == 1);
+  REQUIRE(table["a"].first == 0);
+  REQUIRE(table["a"].second == total_num_elements);
+
+  for (size_t i = 0; i < total_num_elements; ++i) {
+    CHECK(a_data_read[i] == expected_a[i]);
+  }
+
+  query_r.finalize();
+  array_r.close();
+}
+
+TEMPLATE_TEST_CASE(
+    "C++ API: Bitsort Filter list on array",
+    "[cppapi][filter][bitsort]",
+    int8_t,
+    int16_t,
+    int32_t,
+    int64_t) {
+  // Setup.
+  Context ctx;
+  VFS vfs(ctx);
+
+  if (vfs.is_dir(bitsort_array_name))
+    vfs.remove_dir(bitsort_array_name);
+
+  auto array_type = GENERATE(TILEDB_SPARSE, TILEDB_DENSE);
+  bitsort_filter_api_test<TestType>(ctx, array_type);
+
+  // Teardown.
+  if (vfs.is_dir(bitsort_array_name))
+    vfs.remove_dir(bitsort_array_name);
+}
