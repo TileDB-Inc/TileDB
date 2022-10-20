0ec58758c997402df81051d624da68807e85e0cb
david.hoke <david.hoke@tiledb.com>
2022-10-20 07:52:31 -0400

    add output to see returned value from system/try_assert invocation [for when may be unexpected code]

--- test/ci/test_assert.cc
+++ test/ci/test_assert.cc
@@ -49,6 +49,8 @@ std::vector<int> assert_exit_codes{
 
 TEST_CASE("CI: Test assertions configuration", "[ci][assertions]") {
   int retval = system(TILEDB_PATH_TO_TRY_ASSERT "/try_assert");
+  
+  std::cout << "retval is " << retval << " from " << TILEDB_PATH_TO_TRY_ASSERT "/try_assert" << std::endl;
 
 #ifdef TILEDB_ASSERTIONS
   REQUIRE(
