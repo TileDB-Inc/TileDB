9d2acac3634ea29e1ad66e9258d8ee60d2ed5ebc
KiterLuc <67824247+KiterLuc@users.noreply.github.com>
2022-10-29 10:09:21 -0300

    Adding rtree object library. (#3613)
    * Adding rtree object library.
    
    This adds the rtree object library and moves the rtree unit tests from the integration test to a real unit test.
    
    ---
    TYPE: IMPROVEMENT
    DESC: Adding rtree object library.
    
    * Remove artifact from attempt to fix link issues.

--- tiledb/sm/rtree/test/unit_rtree.cc
+++ tiledb/sm/rtree/test/unit_rtree.cc
@@ -1,11 +1,11 @@
 /**
- * @file unit-RTree.cc
+ * @file unit_rtree.cc
  *
  * @section LICENSE
  *
  * The MIT License
  *
- * @copyright Copyright (c) 2017-2021 TileDB, Inc.
+ * @copyright Copyright (c) 2022 TileDB, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
