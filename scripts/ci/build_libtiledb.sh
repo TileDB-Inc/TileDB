fb853228f414aabe6b9d1064d9752883a40c751e
Isaiah Norton <isaiah@tiledb.io>
2022-10-31 13:24:22 -0400

    Make test_support_lib conditional; add tests-disabled CI check

--- scripts/ci/build_libtiledb.sh
+++ scripts/ci/build_libtiledb.sh
@@ -25,6 +25,17 @@
 #
 set -xeuo pipefail
 
+# Check for clean configure with tests disabled
+# This will not validate linkage, but we can't
+# build all variations separately right now.
+
+CONFIG_TMP_PATH=`mktemp -d`
+pushd $CONFIG_TMP_PATH
+$GITHUB_WORKSPACE/bootstrap $bootstrap_args --disable-tests
+popd
+
+###############################################
+
 # Build and test libtiledb
 
 # Set up arguments for bootstrap.sh
@@ -38,6 +49,7 @@ echo "Bootstrapping with '$bootstrap_args'"
 $GITHUB_WORKSPACE/bootstrap $bootstrap_args
 
 make -j4
+
 make -C tiledb install
 
 cd $GITHUB_WORKSPACE/build
