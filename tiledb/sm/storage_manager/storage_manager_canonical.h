1bff294bc65c178aa764707af09cf1ea42fce077
dhoke4tdb <david.hoke@tiledb.com>
2022-11-11 20:39:23 -0500

    clang format

--- tiledb/sm/storage_manager/storage_manager_canonical.h
+++ tiledb/sm/storage_manager/storage_manager_canonical.h
@@ -1272,7 +1272,7 @@ class StorageManagerCanonical {
           array_schemas_all,
       const EncryptionKey& encryption_key,
       const std::vector<TimestampedURI>& fragments_to_load,
-      const std::unordered_map<std::string, std::pair<Tile *, uint64_t>>&
+      const std::unordered_map<std::string, std::pair<Tile*, uint64_t>>&
           offsets);
 
   /**
