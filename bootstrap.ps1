9049891ab764a74dd57591871fcd203f60af6bd5
Paul J. Davis <paul.davis@tiledb.com>
2022-12-02 13:14:57 -0600

    Add --enable-wdocumentation bootstrap flag

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -66,6 +66,9 @@ Enables building of Abseil and simple linkage test
 .Parameter DisableWerror
 Disable use of warnings-as-errors (/WX) during build.
 
+.Parameter EnableWdocumentation
+Enable the use of -Wdocumentation during build.
+
 .PARAMETER _EnableCrc32
 Enables building of Crc32 and simple linkage test
 
@@ -106,6 +109,7 @@ Param(
     [switch]$EnableAbseil,
     [switch]$EnableBuildDeps,
     [switch]$_EnableCrc32,
+    [switch]$EnableWdocumentation,
     [switch]$DisableWerror,
     [switch]$DisableCppApi,
     [switch]$DisableTests,
@@ -176,6 +180,11 @@ if ($EnableSerialization.IsPresent) {
 $Werror = "ON"
 if ($DisableWerror.IsPresent) {
     $Werror = "OFF"
+}
+
+$Wdocumentation = "OFF"
+if ($EnableWdocumentation.IsPresent) {
+    $Wdocumentation = "ON"
 }
 
 $CppApi = "ON"
@@ -261,7 +270,7 @@ if ($CMakeGenerator -eq $null) {
 
 # Run CMake.
 # We use Invoke-Expression so we can echo the command to the user.
-$CommandString = "cmake -A X64 -DCMAKE_BUILD_TYPE=$BuildType -DCMAKE_INSTALL_PREFIX=""$InstallPrefix"" -DCMAKE_PREFIX_PATH=""$DependencyDir"" -DMSVC_MP_FLAG=""/MP$BuildProcesses"" -DTILEDB_ASSERTIONS=$AssertionMode -DTILEDB_VERBOSE=$Verbosity -DTILEDB_AZURE=$UseAzure -DTILEDB_S3=$UseS3 -DTILEDB_SERIALIZATION=$UseSerialization -DTILEDB_WERROR=$Werror -DTILEDB_CPP_API=$CppApi -DTILEDB_TESTS=$Tests -DTILEDB_STATS=$Stats -DTILEDB_STATIC=$TileDBStatic -DTILEDB_FORCE_ALL_DEPS=$TileDBBuildDeps -DTILEDB_TOOLS=$TileDBTools -DTILEDB_EXPERIMENTAL_FEATURES=$TileDBExperimentalFeatures -DTILEDB_WEBP=$BuildWebP -DTILEDB_ABSEIL=$BuildAbseil -DTILEDB_CRC32=$BuildCrc32 $GeneratorFlag ""$SourceDirectory"""
+$CommandString = "cmake -A X64 -DCMAKE_BUILD_TYPE=$BuildType -DCMAKE_INSTALL_PREFIX=""$InstallPrefix"" -DCMAKE_PREFIX_PATH=""$DependencyDir"" -DMSVC_MP_FLAG=""/MP$BuildProcesses"" -DTILEDB_ASSERTIONS=$AssertionMode -DTILEDB_VERBOSE=$Verbosity -DTILEDB_AZURE=$UseAzure -DTILEDB_S3=$UseS3 -DTILEDB_SERIALIZATION=$UseSerialization -DTILEDB_WERROR=$Werror -DTILEDB_CPP_API=$CppApi -DTILEDB_TESTS=$Tests -DTILEDB_STATS=$Stats -DTILEDB_STATIC=$TileDBStatic -DTILEDB_FORCE_ALL_DEPS=$TileDBBuildDeps -DTILEDB_TOOLS=$TileDBTools -DTILEDB_EXPERIMENTAL_FEATURES=$TileDBExperimentalFeatures -DTILEDB_WEBP=$BuildWebP -DTILEDB_ABSEIL=$BuildAbseil -DTILEDB_CRC32=$BuildCrc32 -DTILEDB_WDOCUMENTATION=$Wdocumentation $GeneratorFlag ""$SourceDirectory"""
 Write-Host $CommandString
 Write-Host
 Invoke-Expression "$CommandString"
