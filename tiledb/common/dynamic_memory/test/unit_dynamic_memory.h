ec8e09c0d84248c69c6cbe4fe89f2dfb8543f576
Abigale Kim <abigale.kim@tiledb.com>
2023-01-03 23:38:40 +0000

    test modification

--- tiledb/common/dynamic_memory/test/unit_dynamic_memory.h
+++ tiledb/common/dynamic_memory/test/unit_dynamic_memory.h
@@ -75,7 +75,7 @@ struct NullTracer {
 
 struct TestTraceEntry {
   std::string_view event_;
-  void* p_;
+  uint64_t p_;
   size_t type_size_;
   size_t n_elements_;
   TracingLabel label_;
@@ -84,12 +84,12 @@ struct TestTraceEntry {
 struct TestTracer {
   static void allocate(
       void* p, size_t type_size, size_t n_elements, const TracingLabel& label) {
-    log_.push_back({"allocate", p, type_size, n_elements, label});
+    log_.push_back({"allocate", static_cast<uint64_t>(reinterpret_cast<uintptr_t>(p)), type_size, n_elements, label});
   }
 
   static void deallocate(
       void* p, size_t type_size, size_t n_elements, const TracingLabel& label) {
-    log_.push_back({"deallocate", p, type_size, n_elements, label});
+    log_.push_back({"deallocate", static_cast<uint64_t>(reinterpret_cast<uintptr_t>(p)), type_size, n_elements, label});
   }
 
   static std::vector<TestTraceEntry> log_;
