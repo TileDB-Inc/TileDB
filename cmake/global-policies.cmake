a25317b29fcc2c1fb0ba4f0f9eac3c23ff09387e
Isaiah Norton <isaiah@tiledb.io>
2023-01-20 21:36:22 -0500

    Set CMP0135 to fix correctness/silence CMake 3.25 warning

---
+++ cmake/global-policies.cmake
@@ -0,0 +1,39 @@
+#
+# cmake/global-policies.cmake
+#
+# The MIT License
+#
+# Copyright (c) 2022 TileDB, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+# This file should be included at the start of any independent CMake project.
+
+# -------------------------------------------------------
+# CMake Global Policies
+# -------------------------------------------------------
+
+if (POLICY CMP0135)
+    # This policy instructs CMake to set the timestamp of files extracted
+    # from source archives by ExternalProject to match the extraction time.
+    # (OLD preserves timestamps from the archive, which may prevent correct
+    # rebuilds if files are re-extracted)
+    cmake_policy(SET CMP0135 NEW)
+endif()
