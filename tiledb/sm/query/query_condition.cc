9492573f7e6ed3736d3c0467b02af0f930072e8e
Andrew Lumsdaine <al75@uw.edu>
2023-01-25 13:17:05 -0800

    post rebase from dev [skip ci]

--- tiledb/sm/query/query_condition.cc
+++ tiledb/sm/query/query_condition.cc
@@ -74,7 +74,7 @@ QueryCondition::QueryCondition(const QueryCondition& rhs)
     , tree_(rhs.tree_ == nullptr ? nullptr : rhs.tree_->clone()) {
 }
 
-QueryCondition::QueryCondition(QueryCondition&& rhs) noexcept
+giQueryCondition::QueryCondition(QueryCondition&& rhs) noexcept
     : condition_marker_(std::move(rhs.condition_marker_))
     , condition_index_(rhs.condition_index_)
     , tree_(std::move(rhs.tree_)) {
@@ -86,7 +86,6 @@ QueryCondition& QueryCondition::operator=(const QueryCondition& rhs) {
     condition_index_ = rhs.condition_index_;
     tree_ = rhs.tree_ == nullptr ? nullptr : rhs.tree_->clone();
   }
-
   return *this;
 }
 
