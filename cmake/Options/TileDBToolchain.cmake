c6f355a1a0a1a8528ec303611335b5fc3768dc5a
Paul J. Davis <paul.davis@tiledb.com>
2023-02-22 12:50:57 -0600

    Only install vcpkg if VCPKG_ROOT isn't set

---
+++ cmake/Options/TileDBToolchain.cmake
@@ -0,0 +1,17 @@
+############################################################
+# TileDB Toolchain Setup
+############################################################
+
+if (TILEDB_GCS)
+    if(DEFINED ENV{VCPKG_ROOT})
+        set(CMAKE_TOOLCHAIN_FILE
+            "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
+            CACHE STRING "Vcpkg toolchain file"
+        )
+    else()
+        include(init-submodule.cmake)
+    endif()
+
+    set(VCPKG_INSTALL_OPTIONS "--no-print-usage")
+    list(APPEND VCPKG_MANIFEST_FEATURES "gcs")
+endif()
