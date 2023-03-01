924599884180fb90e35009ffbd54a7fe4cb16427
Paul J. Davis <paul.davis@tiledb.com>
2023-02-28 19:06:41 -0600

    Set the toolchain file when using vcpkg submodule

--- cmake/Options/TileDBToolchain.cmake
+++ cmake/Options/TileDBToolchain.cmake
@@ -2,16 +2,21 @@
 # TileDB Toolchain Setup
 ############################################################
 
-if (TILEDB_GCS)
-    if(DEFINED ENV{VCPKG_ROOT})
-        set(CMAKE_TOOLCHAIN_FILE
-            "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
-            CACHE STRING "Vcpkg toolchain file"
-        )
-    else()
-        include(init-submodule)
-    endif()
-
-    set(VCPKG_INSTALL_OPTIONS "--no-print-usage")
-    list(APPEND VCPKG_MANIFEST_FEATURES "gcs")
+# Only enable vcpkg on GCS builds for now
+if (NOT TILEDB_GCS)
+    return()
 endif()
+
+if(DEFINED ENV{VCPKG_ROOT})
+    set(CMAKE_TOOLCHAIN_FILE
+        "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
+        CACHE STRING "Vcpkg toolchain file")
+else()
+    include(init-submodule)
+    set(CMAKE_TOOLCHAIN_FILE
+        "${CMAKE_CURRENT_SOURCE_DIR}/external/vcpkg/scripts/buildsystems/vcpkg.cmake"
+        CACHE STRING "Vcpkg toolchain file")
+endif()
+
+set(VCPKG_INSTALL_OPTIONS "--no-print-usage")
+list(APPEND VCPKG_MANIFEST_FEATURES "gcs")
