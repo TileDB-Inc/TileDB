bd6cdc90e5766025a84f340b6920d77a81e74a34
Paul J. Davis <paul.davis@tiledb.com>
2023-03-06 13:25:15 -0600

    First working azure storage port

--- ports/azure-storage-cpplite/portfile.cmake
+++ ports/azure-storage-cpplite/portfile.cmake
@@ -5,27 +5,24 @@ vcpkg_from_github(
     REF v0.3.0
     SHA512 14a476fb9dcff0f8fe2e659cf81ad29433befba723b6440d21f7f563307f116726a15325b53cdc05e827634c00f90a7a8be6d5d212a1e7e442860f7075206b70
     HEAD_REF master
+    PATCHES
+        001-CMakeLists.patch
+        002-base64.cpp.patch
+        003-misc.patch
 )
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}/Microsoft.WindowsAzure.Storage
+    SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
         -DBUILD_TESTS=OFF
         -DBUILD_SAMPLES=OFF
         -DCURL_NO_CURL_CMAKE=ON
-    OPTIONS_RELEASE
-        -DGETTEXT_LIB_DIR=${CURRENT_INSTALLED_DIR}/lib
-    OPTIONS_DEBUG
-        -DGETTEXT_LIB_DIR=${CURRENT_INSTALLED_DIR}/debug/lib
 )
 
 vcpkg_install_cmake()
 
-file(INSTALL
-    ${SOURCE_PATH}/LICENSE.txt
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/LICENSE ${SOURCE_PATH}/NOTICE.txt)
 
 vcpkg_copy_pdbs()
