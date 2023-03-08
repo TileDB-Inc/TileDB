2b0bfdd130560c13711beb89851133d521cf682f
Ypatia Tsavliri <ypatia@tiledb.com>
2023-03-08 16:48:42 +0200

    Fix support for empty strings for Dictionary and RLE encodings (#3938)
    This is a problem found in VCF where if using a string dimension compressed with a dictionary filter, we get an error if the value of the string dimension is always an empty string ("") :
    
    "terminate called after throwing an instance of 'tiledb::TileDBError'
      what():  [TileDB::Task] Error: Caught std::exception: Failed decompressing dictionary-encoded strings; empty input arguments."
    
    The failure would not occur when reading an array that was written using a string dimension data buffer that contained N entries with value 0, but only when writing using an empty string dimension data buffer .
    
    I also added support for encoding empty strings with RLE by removing a check (this was previously done for Dictionary in https://github.com/TileDB-Inc/TileDB/pull/3493/files .
    
    Co-authored-by: Paul J. Davis <paul.davis@tiledb.com>

--- test/src/unit-cppapi-filter.cc
+++ test/src/unit-cppapi-filter.cc
@@ -601,3 +601,104 @@ TEST_CASE(
   if (vfs.is_dir(array_name))
     vfs.remove_dir(array_name);
 }
+
+TEST_CASE(
+    "C++ API: Filter buffer with some empty strings with RLE or Dictionary "
+    "encoding",
+    "[cppapi][filter][rle-strings][dict-strings][empty-strings]") {
+  using namespace tiledb;
+  Context ctx;
+  VFS vfs(ctx);
+  std::string array_name = "cpp_unit_array";
+
+  if (vfs.is_dir(array_name))
+    vfs.remove_dir(array_name);
+
+  auto f = GENERATE(TILEDB_FILTER_RLE, TILEDB_FILTER_DICTIONARY);
+
+  // Create array with string dimension and one attribute
+  ArraySchema schema(ctx, TILEDB_SPARSE);
+
+  FilterList filters(ctx);
+  filters.add_filter({ctx, f});
+
+  auto d0 = Dimension::create(ctx, "d0", TILEDB_STRING_ASCII, nullptr, nullptr);
+  d0.set_filter_list(filters);
+
+  Domain domain(ctx);
+  domain.add_dimensions(d0);
+  schema.set_domain(domain);
+
+  auto a0 = Attribute::create<int32_t>(ctx, "a0");
+  schema.add_attributes(a0);
+  schema.set_allows_dups(true);
+
+  Array::create(array_name, schema);
+
+  std::vector<char> d0_buf;
+  std::vector<uint64_t> d0_offsets_buf;
+  std::vector<int> a0_buf;
+
+  SECTION("Only empty strings in data buffer") {
+    // Write this data buffer to the array:
+    // ["\0","\0","\0","\0","\0","\0","\0","\0","\0","\0"]
+    // As data we use the d0 array empty, as already defined.
+    d0_offsets_buf.assign(10, 0);
+    a0_buf.assign(10, 42);
+  }
+
+  SECTION("Combine empty and strings of nulls in data buffer") {
+    // Write this data buffer to the array:
+    // ["\0","\0","\0","\0","\0","\0","\0","\0","\0","\0\0\0\0\0\0\0\0\0\0"]
+    d0_buf.assign(10, 0);
+    d0_offsets_buf.assign(10, 0);
+    a0_buf.assign(10, 42);
+  }
+
+  SECTION("Combine empty and non-empty strings in data buffer") {
+    // Write this data buffer to the array of strings: ["a", "bb", "", "c", ""]
+    d0_buf.assign({'a', 'b', 'b', 'c'});
+    d0_offsets_buf.assign({0, 1, 3, 3, 4});
+    a0_buf.assign({42, 42, 42, 42, 42});
+  }
+
+  Array array_w(ctx, array_name, TILEDB_WRITE);
+  Query query_w(ctx, array_w);
+  query_w.set_layout(TILEDB_UNORDERED)
+      .set_data_buffer("d0", d0_buf)
+      .set_offsets_buffer("d0", d0_offsets_buf)
+      .set_data_buffer("a0", a0_buf);
+  query_w.submit();
+  array_w.close();
+
+  // Read all data and check no error and data correct
+  std::vector<std::byte> d0_read_buf(1 << 20);
+  std::vector<uint64_t> d0_offsets_read_buf(1 << 20);
+  std::vector<int32_t> a0_read_buf(1 << 20);
+
+  Array array_r(ctx, array_name, TILEDB_READ);
+  Query query_r(ctx, array_r);
+  query_r.set_layout(TILEDB_UNORDERED);
+  query_r.set_data_buffer("d0", d0_read_buf)
+      .set_offsets_buffer("d0", d0_offsets_read_buf)
+      .set_data_buffer("a0", a0_read_buf);
+
+  auto st = query_r.submit();
+  REQUIRE(st == Query::Status::COMPLETE);
+
+  auto results = query_r.result_buffer_elements();
+  auto num_offsets = results["d0"].first;
+  CHECK(num_offsets == d0_offsets_buf.size());
+  auto str_len = results["d0"].second;
+  CHECK(str_len == d0_buf.size());
+
+  for (uint64_t i = 0; i < num_offsets; i++) {
+    CHECK(a0_read_buf[i] == 42);
+  }
+
+  array_r.close();
+
+  // Clean up
+  if (vfs.is_dir(array_name))
+    vfs.remove_dir(array_name);
+}
