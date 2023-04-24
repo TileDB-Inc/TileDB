f05987437ad15560c4a5ae50dc101448959dcaee
Eric Hughes <eric.hughes@tiledb.com>
2023-04-24 17:19:04 -0600

    Move a class out of an anonymous namespace to satisfy gcc. Finish a refactoring that had been left incomplete.

--- tiledb/api/c_api_test_support/testsupport_capi_context.h
+++ tiledb/api/c_api_test_support/testsupport_capi_context.h
@@ -65,10 +65,7 @@ struct ordinary_context {
 
 }  // namespace tiledb::api::test_support
 
-/*
- * Anonymous namespace hides base classes not for public use.
- */
-namespace {
+namespace tiledb::api::test_support {
 
 /**
  * Ordinary dimension base class
@@ -83,7 +80,7 @@ class ordinary_dimension {
    * Initialization function for `dimension` data member. It's separate to
    * allow either an external context or a just-created internal one.
    */
-  void allocate_context(tiledb_ctx_handle_t* context, std::string name) {
+  void allocate_dimension(tiledb_ctx_handle_t* context, std::string name) {
     capi_return_t rc{tiledb_dimension_alloc(
         context,
         name.data(),
@@ -98,31 +95,20 @@ class ordinary_dimension {
 
  protected:
   const std::array<uint32_t, 2> constraint{0, 10};
-  ordinary_dimension(tiledb_ctx_handle_t* ctx, std::string name) {
-    capi_return_t rc = tiledb_dimension_alloc(
-        ctx,
-        name.data(),
-        TILEDB_UINT32,
-        constraint.data(),
-        nullptr,
-        &dimension);
-    if (tiledb_status(rc) != TILEDB_OK) {
-      throw std::runtime_error("error creating test dimension");
-    }
+  ordinary_dimension(tiledb_ctx_handle_t* ctx, std::string name)
+      : ctx{} {
+    allocate_dimension(ctx, name);
   }
   explicit ordinary_dimension(std::string name)
-      : ordinary_dimension(ctx.context, name) {
+      : ctx{} {
+    allocate_dimension(ctx.context, name);
   }
   ~ordinary_dimension() {
     tiledb_dimension_free(&dimension);
   }
 };
 
-}  // namespace
-
-namespace tiledb::api::test_support {
-
-struct ordinary_dimension_d1 : public ::ordinary_dimension {
+struct ordinary_dimension_d1 : public ordinary_dimension {
   /**
    * Default constructor for standalone use.
    */
@@ -132,12 +118,12 @@ struct ordinary_dimension_d1 : public ::ordinary_dimension {
   /**
    * Constructor with context for use inside a domain
    */
-  ordinary_dimension_d1(tiledb_ctx_handle_t* ctx)
+  explicit ordinary_dimension_d1(tiledb_ctx_handle_t* ctx)
       : ordinary_dimension{ctx, "d1"} {
   }
 };
 
-struct ordinary_dimension_d2 : public ::ordinary_dimension {
+struct ordinary_dimension_d2 : public ordinary_dimension {
   /**
    * Default constructor for standalone use.
    */
@@ -147,7 +133,7 @@ struct ordinary_dimension_d2 : public ::ordinary_dimension {
   /**
    * Constructor with context for use inside a domain
    */
-  ordinary_dimension_d2(tiledb_ctx_handle_t* ctx)
+  explicit ordinary_dimension_d2(tiledb_ctx_handle_t* ctx)
       : ordinary_dimension{ctx, "d2"} {
   }
 };
