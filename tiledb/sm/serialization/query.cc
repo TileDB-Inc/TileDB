d4a37a6ec6420aa5dc607f6727a46a25c814c099
Shaun M Reed <shaunrd0@gmail.com>
2023-06-23 17:07:22 -0400

    Fix UnorderedWriter fragment metadata deserialization (#4105)
    Allocates fragment metadata during deserialization only if the serialized strategy has metadata to initialize it with. This change also updates `Query::allow_separate_attribute_writes_` during initialization for remote arrays, since the Query constructor called from REST isn't aware of the config until it's deserialized. Fixes [SC-29371](https://app.shortcut.com/tiledb-inc/story/29371/segfault-serializing-unordered-writer-fragment-metadata). With these changes I was able to run partial writes examples and UTs through rest.
    
    I also included a fix for [SC-29369](https://app.shortcut.com/tiledb-inc/story/29369/rest-calls-query-set-config-on-all-deserialized-queries), which allows setting the config for initialized queries to support REST setting config memory budget options. This was a small change, but I can factor this out into another PR if needed. The description for SC-29369 provides some more detailed information.
    
    ---
    TYPE: BUG
    DESC: Small fixes for UnorderedWriter deserialization, allow setting the config on remote arrays.

--- tiledb/sm/serialization/query.cc
+++ tiledb/sm/serialization/query.cc
@@ -1332,12 +1332,6 @@ Status writer_from_capnp(
   } else if (query.layout() == Layout::UNORDERED) {
     auto unordered_writer = dynamic_cast<UnorderedWriter*>(writer);
 
-    // Fragment metadata is not allocated when deserializing into a new Query
-    // object.
-    if (unordered_writer->frag_meta() == nullptr) {
-      RETURN_NOT_OK(unordered_writer->alloc_frag_meta());
-    }
-
     RETURN_NOT_OK(unordered_write_state_from_capnp(
         query,
         writer_reader.getUnorderedWriterState(),
@@ -2971,6 +2965,11 @@ Status unordered_write_state_from_capnp(
   }
 
   if (state_reader.hasFragMeta()) {
+    // Fragment metadata is not allocated when deserializing into a new Query
+    // object.
+    if (unordered_writer->frag_meta() == nullptr) {
+      RETURN_NOT_OK(unordered_writer->alloc_frag_meta());
+    }
     auto frag_meta = unordered_writer->frag_meta();
     auto frag_meta_reader = state_reader.getFragMeta();
     RETURN_NOT_OK(fragment_metadata_from_capnp(
