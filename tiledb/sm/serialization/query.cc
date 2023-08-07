cefda2bd6791a1919be227cee6641e96b6c71cb3
Luc Rancourt <lucrancourt@gmail.com>
2023-08-07 16:20:35 +0200

    Only add written buffers for unordered queries.

--- tiledb/sm/serialization/query.cc
+++ tiledb/sm/serialization/query.cc
@@ -2194,10 +2194,10 @@ Status query_from_capnp(
   } else if (
       query_type == QueryType::WRITE &&
       query_status == QueryStatus::COMPLETED &&
-      context == SerializationContext::SERVER) {
+      context == SerializationContext::SERVER && layout == Layout::UNORDERED) {
     // Handle pre 2.16 clients...
-    // For pre-2.16 clients writes always had to write all attributes +
-    // dimensions at once so we just list all fields here.
+    // For pre-2.16 clients, unordered writes always had to write all attributes
+    // + dimensions at once so we just list all fields here.
     auto& written_buffers = query->get_written_buffers();
     written_buffers.clear();
     for (const auto& it : schema.attributes()) {
