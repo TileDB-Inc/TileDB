760c41ab1570a27f101dad0e3699258006b86cc5
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2023-08-09 17:59:10 +0300

    Update try_assert.cc

--- test/ci/try_assert.cc
+++ test/ci/try_assert.cc
@@ -3,7 +3,11 @@
 
 int main(int, char**) {
 #if defined(_MSC_VER)
-  // Do not display message box on abort.
+  // We disable the following events on abort:
+  // _WRITE_ABORT_MSG: Display message box with Abort, Retry, Ignore
+  // _CALL_REPORTFAULT: Send an error report to Microsoft
+  // The second parameter specifies which flags to change, and the first
+  // the value of these flags.
   _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
 #endif
   assert(false);
