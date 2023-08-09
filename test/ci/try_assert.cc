f787efcde65667bafff627644c68b7abc0ad60fa
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2023-08-09 13:30:46 +0300

    Do not display message box on abort.

--- test/ci/try_assert.cc
+++ test/ci/try_assert.cc
@@ -2,6 +2,10 @@
 #include <iostream>
 
 int main(int, char**) {
+#if defined(_MSC_VER)
+  // Do not display message box on abort.
+  _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+#endif
   assert(false);
 
   std::cout << "Assert did not exit!" << std::endl;
