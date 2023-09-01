462b6b1f9b11327e2a7858a2d2915316d4824709
Luc Rancourt <lucrancourt@gmail.com>
2023-09-01 13:27:29 +0200

    Adding array creation code.

--- test/src/unit-backwards_compat.cc
+++ test/src/unit-backwards_compat.cc
@@ -1368,6 +1368,36 @@ TEST_CASE(
 TEST_CASE(
     "Backwards compatibility: 0 var chunks",
     "[backwards-compat][zero-var-chunks]") {
+  // The following code created the zero_var_chunks_v10 array for this test.
+  // #include <tiledb/tiledb>
+  // using namespace tiledb;
+
+  // int main() {
+  //   Context ctx;
+
+  //   Domain domain(ctx);
+  //   domain.add_dimension(
+  //       Dimension::create(ctx, "d", TILEDB_STRING_ASCII, nullptr, nullptr));
+  //   ArraySchema schema(ctx, TILEDB_SPARSE);
+  //   schema.set_domain(domain).set_order({{TILEDB_ROW_MAJOR,
+  //   TILEDB_ROW_MAJOR}}); schema.add_attribute(Attribute::create<int32_t>(ctx,
+  //   "a")); Array::create("zero_var_chunks_v10", schema);
+
+  //   std::vector<char> d = {};
+  //   std::vector<uint64_t> d_offsets = {0};
+  //   std::vector<int32_t> data = {1};
+
+  //   Array array(ctx, "zero_var_chunks_v10", TILEDB_WRITE);
+  //   Query query(ctx, array, TILEDB_WRITE);
+  //   query.set_layout(TILEDB_UNORDERED)
+  //       .set_data_buffer("a", data)
+  //       .set_data_buffer("d", d)
+  //       .set_offsets_buffer("d", d_offsets);
+  //   query.submit();
+  //   array.close();
+  //   return 0;
+  // }
+
   // This array has a var file for the "d" dimension with 0 chunks. This was
   // only possible if a fragment had all empty values for a variable string in
   // versions earlier than v10.
