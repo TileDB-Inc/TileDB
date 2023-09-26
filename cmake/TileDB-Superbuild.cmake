3f0d78bb77be8ac9d611d00baa92145c1be93968
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2023-09-27 01:49:46 +0300

    Fix non-Windows non-vcpkg builds when only S3 is enabled.

--- cmake/TileDB-Superbuild.cmake
+++ cmake/TileDB-Superbuild.cmake
@@ -122,7 +122,7 @@ if (NOT WIN32)
   include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Modules/FindOpenSSL_EP.cmake)
 endif()
 
-if (TILEDB_AZURE OR TILEDB_GCS OR TILEDB_SERIALIZATION)
+if ((TILEDB_S3 AND NOT WIN32 AND NOT TILEDB_VCPKG) OR TILEDB_AZURE OR TILEDB_GCS OR TILEDB_SERIALIZATION)
   # Need libcurl either with GCS or serialization support.
   include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Modules/FindCurl_EP.cmake)
 endif()
