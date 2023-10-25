489a7c617ebedb57f4bf663f5977c101cb070b26
eric-hughes-tiledb <82400964+eric-hughes-tiledb@users.noreply.github.com>
2023-10-25 10:36:27 -0600

    Add an aspect template argument to `class CAPIFunction`. (#4430)
    This PR makes two changes to the template signature of `class
    CAPIFunction`. The central one is to add an aspect argument that can
    provide hooks into `CAPIFunction::function`. In this initial version
    there's a single aspect point (the aspect member function `call`) that
    is only an observer. The `call` of the default aspect does nothing. The
    second change in template signature is to infer the return value of the
    wrapper function and to suppress return statements if that type is
    `void`. This allows the elimination of subsidiary wrapper for `void`
    functions to give them a (constant) return value.
    
    Tests for the aspect implement `class LoggingAspect`, a primitive
    call-logger sufficient for testing, and provide an override header to
    activate it. The override header also generates a traits class for each
    C API implementation function that provides data for `LoggingAspect`.
    These elements are exercised by two new test runners that share a common
    set of tests; only the results differ depending on whether the hook is
    active or not.
    
    In order to automatically generate metadata for API functions, the PR
    introduces a set of macros to define the C API interface functions.
    These functions are already essentially boilerplate, but they cannot be
    defined with function templates because they must have "C" linkage. The
    macros provide a pair of macro hooks for generating code either before
    or after the function definition.
    
    ---
    TYPE: IMPROVEMENT
    DESC: Add an aspect template argument to `class CAPIFunction`
    
    (cherry picked from commit ea7e8ef1bb558d93b2a8866fe803fd3b751a3f37)

--- tiledb/api/c_api_support/exception_wrapper/capi_definition.h
+++ tiledb/api/c_api_support/exception_wrapper/capi_definition.h
@@ -1,11 +1,11 @@
 /**
- * @file tiledb/api/c_api_support/c_api_support.h
+ * @file tiledb/api/c_api_support/exception_wrapper/hook.h
  *
  * @section LICENSE
  *
  * The MIT License
  *
- * @copyright Copyright (c) 2022 TileDB, Inc.
+ * @copyright Copyright (c) 2023 TileDB, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,20 +27,35 @@
  *
  * @section DESCRIPTION
  *
- * This file includes all the support functions that appear generally in C API
- * implementation functions:
- *   - Exception wrappers and error handling
- *   - Argument validation
+ * This file defines macros to define C API functions.
  */
 
-#ifndef TILEDB_CAPI_SUPPORT_H
-#define TILEDB_CAPI_SUPPORT_H
+#ifndef TILEDB_CAPI_DEFINITION_H
+#define TILEDB_CAPI_DEFINITION_H
 
-#include "argument_validation.h"
-#include "tiledb/api/c_api_support/exception_wrapper/capi_definition.h"
-#include "tiledb/api/c_api_support/exception_wrapper/exception_wrapper.h"
-#if __has_include("capi_function_override.h")
-#include "capi_function_override.h"
-#endif
+/* clang-format off */
 
-#endif  // TILEDB_CAPI_SUPPORT_H
+/*
+ * Hook for additional code to be generated before each C API interface
+ * function.
+ */
+#define CAPI_PREFIX(name)
+
+/*
+ * Declaration clause for a C API interface function. Follow with a `{ ... }`
+ * block defining the body.
+ */
+#define CAPI_INTERFACE(root, ...) \
+CAPI_PREFIX(root)                 \
+capi_return_t tiledb_##root(__VA_ARGS__) noexcept
+
+/*
+ * A variant of CAPI_INTERFACE for the handful of `void` returns.
+ */
+#define CAPI_INTERFACE_VOID(root, ...) \
+CAPI_PREFIX(root)                      \
+void tiledb_##root(__VA_ARGS__) noexcept
+
+/* clang-format on */
+
+#endif  // TILEDB_CAPI_DEFINITION_H
