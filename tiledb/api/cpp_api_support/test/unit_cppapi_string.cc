002d98a8c68baa0aa3d6fd4c82612f146b17dc00
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2023-10-26 23:01:41 +0300

    Add more tests.

--- tiledb/api/cpp_api_support/test/unit_cppapi_string.cc
+++ tiledb/api/cpp_api_support/test/unit_cppapi_string.cc
@@ -32,6 +32,7 @@
 
 #include <test/support/tdb_catch.h>
 
+#include "tiledb/api/c_api/string/string_api_internal.h"
 #include "tiledb/sm/cpp_api/capi_string.h"
 
 using namespace tiledb::impl;
@@ -48,4 +49,30 @@ TEST_CASE(
     "[capi_string][null-param-ptr]") {
   tiledb_string_t* string = nullptr;
   REQUIRE_THROWS_AS(CAPIString(&string), std::invalid_argument);
+}
+
+TEST_CASE(
+    "CAPIString: Test creating string handle and getting its value",
+    "[capi_string][get]") {
+  const std::string test_string = "hello";
+  tiledb_string_t* handle = tiledb_string_t::make_handle(test_string);
+  std::string result;
+
+  SECTION("convert_to_string") {
+    auto result_maybe = convert_to_string(&handle);
+    REQUIRE(result_maybe);
+    result = *result_maybe;
+  }
+  SECTION("CAPIString") {
+    result = CAPIString(&handle).str();
+  }
+
+  REQUIRE(handle == nullptr);
+  REQUIRE(result == test_string);
+}
+
+TEST_CASE(
+    "CAPIString: Test convert_to_string with null handle", "[capi_string]") {
+  tiledb_string_t* handle = nullptr;
+  REQUIRE(!convert_to_string(&handle).has_value());
 }
