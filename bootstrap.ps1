7f523408273302a9d26ebe61992c069381ddfe56
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2023-11-20 13:48:18 +0200

    Remove `TILEDB_CRC32`.
    Its value has declined with the advent of vcpkg, and it is not used by anything in CI.

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -68,9 +68,6 @@ Enables building TileDB CLI tools (experimental)
 .PARAMETER EnableExperimentalFeatures
 Enables building TileDB Experimental features
 
-.PARAMETER _EnableCrc32
-Enables building of Crc32 and simple linkage test
-
 .PARAMETER EnableArrowTests
 Enables the compilation of the arrow adapter unit tests
 
@@ -135,7 +132,6 @@ Param(
     [switch]$EnableTools,
     [switch]$EnableExperimentalFeatures,
     [switch]$EnableBuildDeps,
-    [switch]$_EnableCrc32,
     [switch]$EnableArrowTests,
     [switch]$EnableRestTests,
     [switch]$EnableAwsS3Config,
@@ -276,11 +272,6 @@ if ($EnableBuildDeps.IsPresent) {
     $TileDBBuildDeps = "ON"
 }
 
-$BuildCrc32="OFF"
-if ($_EnableCrc32.IsPresent) {
-  $BuildCrc32="ON"
-}
-
 $ArrowTests="OFF"
 if ($EnableArrowTests.IsPresent) {
     $ArrowTests="ON"
@@ -329,7 +320,7 @@ if ($CMakeGenerator -eq $null) {
 
 # Run CMake.
 # We use Invoke-Expression so we can echo the command to the user.
-$CommandString = "cmake -A X64 -DTILEDB_VCPKG=$UseVcpkg -DCMAKE_BUILD_TYPE=$BuildType -DCMAKE_INSTALL_PREFIX=""$InstallPrefix"" -DCMAKE_PREFIX_PATH=""$DependencyDir"" -DMSVC_MP_FLAG=""/MP$BuildProcesses"" -DTILEDB_ASSERTIONS=$AssertionMode -DTILEDB_VERBOSE=$Verbosity -DTILEDB_AZURE=$UseAzure -DTILEDB_S3=$UseS3 -DTILEDB_GCS=$UseGcs -DTILEDB_SERIALIZATION=$UseSerialization -DTILEDB_WERROR=$Werror -DTILEDB_CPP_API=$CppApi -DTILEDB_TESTS=$Tests -DTILEDB_STATS=$Stats -DTILEDB_STATIC=$TileDBStatic -DTILEDB_FORCE_ALL_DEPS=$TileDBBuildDeps -DTILEDB_REMOVE_DEPRECATIONS=$RemoveDeprecations -DTILEDB_TOOLS=$TileDBTools -DTILEDB_EXPERIMENTAL_FEATURES=$TileDBExperimentalFeatures -DTILEDB_WEBP=$BuildWebP -DTILEDB_CRC32=$BuildCrc32 -DTILEDB_ARROW_TESTS=$ArrowTests -DTILEDB_TESTS_ENABLE_REST=$RestTests -DTILEDB_TESTS_AWS_S3_CONFIG=$ConfigureS3 -DTILEDB_INSTALL_FIND_DEPENDENCIES=$InstallFindDependencies $GeneratorFlag ""$SourceDirectory"""
+$CommandString = "cmake -A X64 -DTILEDB_VCPKG=$UseVcpkg -DCMAKE_BUILD_TYPE=$BuildType -DCMAKE_INSTALL_PREFIX=""$InstallPrefix"" -DCMAKE_PREFIX_PATH=""$DependencyDir"" -DMSVC_MP_FLAG=""/MP$BuildProcesses"" -DTILEDB_ASSERTIONS=$AssertionMode -DTILEDB_VERBOSE=$Verbosity -DTILEDB_AZURE=$UseAzure -DTILEDB_S3=$UseS3 -DTILEDB_GCS=$UseGcs -DTILEDB_SERIALIZATION=$UseSerialization -DTILEDB_WERROR=$Werror -DTILEDB_CPP_API=$CppApi -DTILEDB_TESTS=$Tests -DTILEDB_STATS=$Stats -DTILEDB_STATIC=$TileDBStatic -DTILEDB_FORCE_ALL_DEPS=$TileDBBuildDeps -DTILEDB_REMOVE_DEPRECATIONS=$RemoveDeprecations -DTILEDB_TOOLS=$TileDBTools -DTILEDB_EXPERIMENTAL_FEATURES=$TileDBExperimentalFeatures -DTILEDB_WEBP=$BuildWebP -DTILEDB_ARROW_TESTS=$ArrowTests -DTILEDB_TESTS_ENABLE_REST=$RestTests -DTILEDB_TESTS_AWS_S3_CONFIG=$ConfigureS3 -DTILEDB_INSTALL_FIND_DEPENDENCIES=$InstallFindDependencies $GeneratorFlag ""$SourceDirectory"""
 Write-Host $CommandString
 Write-Host
 Invoke-Expression "$CommandString"
