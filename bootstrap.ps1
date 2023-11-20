35732e80034535127fc7b8763c2e8ba45b7dcc97
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2023-11-20 18:39:12 +0200

    Add bootstrap script options to set `BUILD_SHARED_LIBS`.

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -58,6 +58,10 @@ Enables building with serialization support.
 
 .PARAMETER EnableStaticTileDB
 Enables building TileDB as a static library.
+Deprecated, this is now the default behavior.
+
+.PARAMETER EnableSharedTileDB
+Enables building TileDB as a shared library.
 
 .PARAMETER EnableBuildDeps
 Enables building TileDB dependencies from source (superbuild)
@@ -132,6 +136,7 @@ Param(
     [switch]$EnableGcs,
     [switch]$EnableSerialization,
     [switch]$EnableStaticTileDB,
+    [switch]$EnableSharedTileDB,
     [switch]$EnableTools,
     [switch]$EnableExperimentalFeatures,
     [switch]$EnableAbseil,
@@ -248,9 +253,12 @@ if ($DisableWebP.IsPresent) {
   $BuildWebP="OFF"
 }
 
-$TileDBStatic = "OFF";
+$BuildSharedLibs = "OFF";
 if ($EnableStaticTileDB.IsPresent) {
-    $TileDBStatic = "ON"
+    Write-Warning "-EnableStaticTileDB is deprecated and will be removed in a future version. TileDB is now built as a static library by default. Use -EnableSharedTileDB to build a shared library."
+}
+elseif ($EnableSharedTileDB.IsPresent) {
+    $BuildSharedLibs = "ON"
 }
 
 $TileDBTools = "OFF";
@@ -326,7 +334,7 @@ if ($CMakeGenerator -eq $null) {
 
 # Run CMake.
 # We use Invoke-Expression so we can echo the command to the user.
-$CommandString = "cmake -A X64 -DTILEDB_VCPKG=$UseVcpkg -DCMAKE_BUILD_TYPE=$BuildType -DCMAKE_INSTALL_PREFIX=""$InstallPrefix"" -DCMAKE_PREFIX_PATH=""$DependencyDir"" -DMSVC_MP_FLAG=""/MP$BuildProcesses"" -DTILEDB_ASSERTIONS=$AssertionMode -DTILEDB_VERBOSE=$Verbosity -DTILEDB_AZURE=$UseAzure -DTILEDB_S3=$UseS3 -DTILEDB_GCS=$UseGcs -DTILEDB_SERIALIZATION=$UseSerialization -DTILEDB_WERROR=$Werror -DTILEDB_CPP_API=$CppApi -DTILEDB_TESTS=$Tests -DTILEDB_STATS=$Stats -DTILEDB_STATIC=$TileDBStatic -DTILEDB_FORCE_ALL_DEPS=$TileDBBuildDeps -DTILEDB_REMOVE_DEPRECATIONS=$RemoveDeprecations -DTILEDB_TOOLS=$TileDBTools -DTILEDB_EXPERIMENTAL_FEATURES=$TileDBExperimentalFeatures -DTILEDB_WEBP=$BuildWebP -DTILEDB_CRC32=$BuildCrc32 -DTILEDB_ARROW_TESTS=$ArrowTests -DTILEDB_TESTS_ENABLE_REST=$RestTests -DTILEDB_TESTS_AWS_S3_CONFIG=$ConfigureS3 $GeneratorFlag ""$SourceDirectory"""
+$CommandString = "cmake -A X64 -DTILEDB_VCPKG=$UseVcpkg -DCMAKE_BUILD_TYPE=$BuildType -DCMAKE_INSTALL_PREFIX=""$InstallPrefix"" -DCMAKE_PREFIX_PATH=""$DependencyDir"" -DMSVC_MP_FLAG=""/MP$BuildProcesses"" -DTILEDB_ASSERTIONS=$AssertionMode -DTILEDB_VERBOSE=$Verbosity -DTILEDB_AZURE=$UseAzure -DTILEDB_S3=$UseS3 -DTILEDB_GCS=$UseGcs -DTILEDB_SERIALIZATION=$UseSerialization -DTILEDB_WERROR=$Werror -DTILEDB_CPP_API=$CppApi -DTILEDB_TESTS=$Tests -DTILEDB_STATS=$Stats -DBUILD_SHARED_LIBS=$BuildSharedLibs -DTILEDB_FORCE_ALL_DEPS=$TileDBBuildDeps -DTILEDB_REMOVE_DEPRECATIONS=$RemoveDeprecations -DTILEDB_TOOLS=$TileDBTools -DTILEDB_EXPERIMENTAL_FEATURES=$TileDBExperimentalFeatures -DTILEDB_WEBP=$BuildWebP -DTILEDB_CRC32=$BuildCrc32 -DTILEDB_ARROW_TESTS=$ArrowTests -DTILEDB_TESTS_ENABLE_REST=$RestTests -DTILEDB_TESTS_AWS_S3_CONFIG=$ConfigureS3 $GeneratorFlag ""$SourceDirectory"""
 Write-Host $CommandString
 Write-Host
 Invoke-Expression "$CommandString"
