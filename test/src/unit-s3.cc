4e7b97826f7f634e8d332f4192e33c73aaa8a5fb
Beka Davis <31743465+bekadavis9@users.noreply.github.com>
2024-01-12 12:10:04 -0500

    Remove uuid in favor of random_label (#4589)
    Remove uuid in favor of random_label
    
    ---
    TYPE: IMPROVEMENT
    DESC: Remove uuid in favor of random_label

--- test/src/unit-s3.cc
+++ test/src/unit-s3.cc
@@ -55,7 +55,7 @@ struct S3Fx {
 
   const std::string S3_PREFIX = "s3://";
   const tiledb::sm::URI S3_BUCKET =
-      tiledb::sm::URI(S3_PREFIX + random_label("tiledb-") + "/");
+      tiledb::sm::URI(S3_PREFIX + "tiledb-" + random_label() + "/");
   const std::string TEST_DIR = S3_BUCKET.to_string() + "tiledb_test_dir/";
   ThreadPool thread_pool_{2};
   tiledb::sm::S3 s3_{&g_helper_stats, &thread_pool_, set_config_params()};
