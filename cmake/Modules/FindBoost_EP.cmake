7ef8d09561dc52d40bb16f773d3a5033adb0d069
Paul J. Davis <paul.davis@tiledb.com>
2024-02-13 12:23:32 -0600

    Fix EP_Add arguments

--- cmake/Modules/FindBoost_EP.cmake
+++ cmake/Modules/FindBoost_EP.cmake
@@ -85,23 +85,28 @@ if (NOT BOOST_FOUND)
     set(BOOST_LIB_DIR ${boost_INSTALL}/lib)
 
     if (WIN32)
-      set(boost_CONFIGURE_COMMAND "bootstrap --with-libraries=container --prefix=${boost_INSTALL}")
-      set(boost_BUILD_COMMAND ".\\b2 -d0 install link=statu variant=release threading=multi runtime-link=static")
+      ExternalProject_Add(ep_boost
+        PREFIX boost
+        URL ${boost_URL}
+        URL_HASH SHA256=${boost_SHA256}
+        BUILD_IN_SOURCE 1
+        CONFIGURE_COMMAND bootstrap --with-libraries=container --prefix=${boost_INSTALL}
+        BUILD_COMMAND .\b2 -d0 install link=statu variant=release threading=multi runtime-link=static
+        INSTALL_COMMAND ""
+        INSTALL_DIR ${boost_INSTALL}
+      )
     else()
-      set(boost_CONFIGURE_COMMAND "./bootstrap.sh --with-libraries=container --prefix=${boost_INSTALL}")
-      set(boost_BUILD_COMMAND "./b2 -d0 install link=statu variant=release threading=multi runtime-link=static")
+      ExternalProject_Add(ep_boost
+        PREFIX boost
+        URL ${boost_URL}
+        URL_HASH SHA256=${boost_SHA256}
+        BUILD_IN_SOURCE 1
+        CONFIGURE_COMMAND ./bootstrap.sh --with-libraries=container --prefix=${boost_INSTALL}
+        BUILD_COMMAND ./b2 -d0 install link=statu variant=release threading=multi runtime-link=static
+        INSTALL_COMMAND ""
+        INSTALL_DIR ${boost_INSTALL}
+      )
     endif()
-
-    ExternalProject_Add(ep_boost
-      PREFIX boost
-      URL ${boost_URL}
-      URL_HASH SHA256=${boost_SHA256}
-      BUILD_IN_SOURCE 1
-      CONFIGURE_COMMAND ${boost_CONFIGURE_COMMAND}
-      BUILD_COMMAND ${boost_BUILD_COMMAND}
-      INSTALL_COMMAND ""
-      INSTALL_DIR ${boost_INSTALL}
-    )
 
     list(APPEND FORWARD_EP_CMAKE_ARGS -DTILEDB_BOOST_EP_BUILT=TRUE)
     list(APPEND TILEDB_EXTERNAL_PROJECTS ep_boost)
