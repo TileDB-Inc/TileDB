82433dfa7955aa40b84ddca18d115b1d4eccfc7d
KiterLuc <67824247+KiterLuc@users.noreply.github.com>
2024-02-16 20:49:39 +0100

    Update history for 2.20.1. (#4741)
    ---
    TYPE: NO_HISTORY
    DESC: Update history for 2.20.1.

--- tiledb/doxygen/source/conf.py
+++ tiledb/doxygen/source/conf.py
@@ -78,7 +78,7 @@ author = 'TileDB, Inc.'
 # The short X.Y version.
 version = '2.20'
 # The full version, including alpha/beta/rc tags.
-release = '2.20.0'
+release = '2.20.1'
 
 # Breathe extension configuration.
 doxygen_xml_dir = os.path.join(TILEDB_BUILD_DIR, 'xml/')
