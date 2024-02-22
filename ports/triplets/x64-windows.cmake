53c320bd078abeb607144fa5fd4fa80f45010ac4
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2024-02-22 14:50:15 +0200

    Temporarily pin the MSVC toolset used by vcpkg to 14.38.

--- ports/triplets/x64-windows.cmake
+++ ports/triplets/x64-windows.cmake
@@ -3,3 +3,7 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 set(X_VCPKG_APPLOCAL_DEPS_INSTALL ON)
+
+# Temporarily pinning to 14.38 until the main build tree can pick
+# 14.39 with the Visual Studio generator.
+set(VCPKG_PLATFORM_TOOLSET_VERSION "14.38")
