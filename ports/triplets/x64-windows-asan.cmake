4e383e9837ef7305a3d4594acb9d4f18ac8e07fc
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2024-02-23 12:47:36 +0200

    Revert "Temporarily pin the MSVC toolset used by vcpkg to 14.38."
    This reverts commit 53c320bd078abeb607144fa5fd4fa80f45010ac4.

--- ports/triplets/x64-windows-asan.cmake
+++ ports/triplets/x64-windows-asan.cmake
@@ -7,7 +7,3 @@ set(X_VCPKG_APPLOCAL_DEPS_INSTALL ON)
 # bigobj is needed for capnp.
 set(VCPKG_C_FLAGS "/fsanitize=address /bigobj")
 set(VCPKG_CXX_FLAGS "/fsanitize=address /bigobj")
-
-# Temporarily pinning to 14.38 until the main build tree can pick
-# 14.39 with the Visual Studio generator.
-set(VCPKG_PLATFORM_TOOLSET_VERSION "14.38")
--- ports/triplets/x64-windows-release.cmake
+++ ports/triplets/x64-windows-release.cmake
@@ -5,7 +5,3 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(X_VCPKG_APPLOCAL_DEPS_INSTALL ON)
 
 set(VCPKG_BUILD_TYPE release)
-
-# Temporarily pinning to 14.38 until the main build tree can pick
-# 14.39 with the Visual Studio generator.
-set(VCPKG_PLATFORM_TOOLSET_VERSION "14.38")
