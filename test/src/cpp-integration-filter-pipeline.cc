e8e85174d1e07b58d3fa82439f6d47d9baa92908
Julia Dark <24235303+jp-dark@users.noreply.github.com>
2024-03-12 11:15:28 -0400

    Update copyright date
    Co-authored-by: Ypatia Tsavliri <ypatia@tiledb.com>

--- test/src/cpp-integration-filter-pipeline.cc
+++ test/src/cpp-integration-filter-pipeline.cc
@@ -5,7 +5,7 @@
  *
  * The MIT License
  *
- * @copyright Copyright (c) 2017-2022 TileDB, Inc.
+ * @copyright Copyright (c) 2017-2024 TileDB, Inc.
  * @copyright Copyright (c) 2016 MIT and Intel Corporation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
