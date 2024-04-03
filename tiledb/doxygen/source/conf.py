b2fd98d79970fe5a7d3843d6b39e9eb8d23cc3b2
Luc Rancourt <lucrancourt@gmail.com>
2024-04-03 20:47:13 +0200

    Update history for 2.21.2.
    ---
    TYPE: NO_HISTORY
    DESC: Update history for 2.21.2.

--- tiledb/doxygen/source/conf.py
+++ tiledb/doxygen/source/conf.py
@@ -78,7 +78,7 @@ author = 'TileDB, Inc.'
 # The short X.Y version.
 version = '2.21'
 # The full version, including alpha/beta/rc tags.
-release = '2.21.1'
+release = '2.21.2'
 
 # Breathe extension configuration.
 doxygen_xml_dir = os.path.join(TILEDB_BUILD_DIR, 'xml/')
