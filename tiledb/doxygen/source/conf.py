9588acfef0ffdb36f29d42c5ba64883298c3ce04
KiterLuc <67824247+KiterLuc@users.noreply.github.com>
2024-04-03 16:38:49 +0200

    Bump dev version to 2.23.0. (#4849)
    ---
    TYPE: NO_HISTORY
    DESC: Bump dev version to 2.23.0.

--- tiledb/doxygen/source/conf.py
+++ tiledb/doxygen/source/conf.py
@@ -76,9 +76,9 @@ copyright = '2023 TileDB, Inc'
 author = 'TileDB, Inc.'
 
 # The short X.Y version.
-version = '2.22'
+version = '2.23'
 # The full version, including alpha/beta/rc tags.
-release = '2.22.0'
+release = '2.23.0'
 
 # Breathe extension configuration.
 doxygen_xml_dir = os.path.join(TILEDB_BUILD_DIR, 'xml/')
