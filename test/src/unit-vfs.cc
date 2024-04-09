db6e1abd10d3062bbd28fd8b443bc906c6ec3174
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2024-04-09 23:08:18 +0300

    Generalize workflow and tests to be able to work in any cloud provider.

--- test/src/unit-vfs.cc
+++ test/src/unit-vfs.cc
@@ -547,48 +547,24 @@ TEMPLATE_LIST_TEST_CASE("VFS: File I/O", "[vfs][uri][file_io]", AllBackends) {
   }
 }
 
-TEST_CASE(
-    "VFS: Test Azure EntraID auth", "[.][ci_only][vfs][azure_entra][allowed]") {
-  URI container_path("azure://entratest/");
-  URI file_path("azure://entratest/test_file");
+TEST_CASE("VFS: Test end-to-end", "[.][vfs][e2e]") {
+  auto test_file_ptr = getenv("TILEDB_VFS_E2E_TEST_FILE");
+  if (test_file_ptr == nullptr) {
+    FAIL("TILEDB_VFS_E2E_TEST_FILE variable is not specified");
+  }
+  URI test_file{test_file_ptr};
 
-  ThreadPool compute_tp(4);
-  ThreadPool io_tp(4);
+  ThreadPool compute_tp(1);
+  ThreadPool io_tp(1);
+  // Will be configured from environment variables.
   Config config;
-  require_tiledb_ok(config.set("vfs.azure.storage_account_name", "rbindar"));
 
   VFS vfs{&g_helper_stats, &compute_tp, &io_tp, config};
-  if (!vfs.supports_uri_scheme(container_path)) {
-    return;
-  }
-
-  bool exists = false;
-  require_tiledb_ok(vfs.is_bucket(container_path, &exists));
-  CHECK(exists);
+  REQUIRE(vfs.supports_uri_scheme(test_file));
 
   uint64_t nbytes = 0;
-  require_tiledb_ok(vfs.file_size(file_path, &nbytes));
-  CHECK(nbytes == 19);
-}
-
-TEST_CASE(
-    "VFS: Test Azure EntraID auth",
-    "[.][ci_only][vfs][azure_entra][fallback]") {
-  URI file_path("azure://entratest/test_file_denied");
-
-  ThreadPool compute_tp(4);
-  ThreadPool io_tp(4);
-  Config config;
-  require_tiledb_ok(config.set("vfs.azure.storage_account_name", "rbindar"));
-
-  VFS vfs{&g_helper_stats, &compute_tp, &io_tp, config};
-  if (!vfs.supports_uri_scheme(file_path)) {
-    return;
-  }
-
-  uint64_t nbytes = 0;
-  require_tiledb_ok(vfs.file_size(file_path, &nbytes));
-  CHECK(nbytes == 19);
+  require_tiledb_ok(vfs.file_size(test_file, &nbytes));
+  CHECK(nbytes > 0);
 }
 
 TEST_CASE("VFS: test ls_with_sizes", "[vfs][ls-with-sizes]") {
