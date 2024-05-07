dc85b1764998b246b01b41f0cb6f614f7ce7c7fe
dimitrisstaratzis <jstaratzi@icloud.com>
2024-05-07 16:22:49 +0300

    format

--- tiledb/sm/query/readers/sparse_unordered_with_dups_reader.cc
+++ tiledb/sm/query/readers/sparse_unordered_with_dups_reader.cc
@@ -252,8 +252,7 @@ void SparseUnorderedWithDupsReader<BitmapType>::load_tile_offsets_data() {
   bool initial_load =
       tile_offsets_min_frag_idx_ == std::numeric_limits<unsigned>::max() &&
       tile_offsets_max_frag_idx_ == 0;
-  uint64_t available_memory =
-      array_memory_tracker_->get_memory_available();
+  uint64_t available_memory = array_memory_tracker_->get_memory_available();
   auto& relevant_fragments = subarray_.relevant_fragments();
 
   if (!partial_tile_offsets_loading_) {
