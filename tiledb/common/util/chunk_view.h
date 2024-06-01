b6d30978bd694516f91c7c4023042b6274cb848e
Andrew Lumsdaine <andrew.lumsdaine@tiledb.com>
2024-06-01 08:24:19 -0700

    Initial chunk_view implementation and first unit tests

--- tiledb/common/util/chunk_view.h
+++ tiledb/common/util/chunk_view.h
@@ -27,17 +27,209 @@
  *
  * @section DESCRIPTION
  *
- * This file implements a zip view for zipping together a set of ranges.
- * It is intended to implement the zip view as defined for C++23.  From
- * https://en.cppreference.com/w/cpp/ranges/zip_view:
- *  1) A zip_view is a range adaptor that takes one or more views, and produces
- * a view whose ith element is a tuple-like value consisting of the ith elements
- * of all views. The size of produced view is the minimum of sizes of all
- * adapted views. 2) zip is a customization point object that constructs a
- * zip_view.
- *
+ * Quick and dirty implementation of a chunk view for C++23.  This is a view
+ * that splits a view into subranges of uniform length, as given by the
+ * constructor argument num_chunks.
  */
 
 #ifndef TILEDB_CHUNK_VIEW_H
 #define TILEDB_CHUNK_VIEW_H
-#endif  // TILEDB_BLOCK_VIEW_H
+
+#include <concepts>
+#include <ranges>
+#include "iterator_facade.h"
+
+/**
+ * A view that splits a view into subranges of uniform length, as delimited by
+ * a chunk size value. The resulting view is a range of subranges, each of which
+ * is a view into the original data range. The iterator over the chunk_view
+ * is a random_access_iterator that dereferences to a subrange of the data
+ * range.
+ *
+ * @tparam R Type of the data range, assumed to be a random access range.
+ *
+ * @todo R could be a view rather than a range.
+ */
+template <std::ranges::random_access_range R>
+class chunk_view : public std::ranges::view_interface<chunk_view<R>> {
+  // Forward reference of the iterator over the data range data
+  template <class V>
+  struct private_iterator;
+
+  /** The type of the iterator over the chunked data range */
+  using data_iterator_type = std::ranges::iterator_t<R>;
+
+  /** The type that can index into the chunked data range */
+  using data_index_type = std::ranges::range_difference_t<R>;
+
+  /** The type dereferenced by the iterator is a subrange */
+  using chunk_type = std::ranges::subrange<data_iterator_type>;
+
+  /** The type of the iterator over the chunk view */
+  using chunk_iterator = private_iterator<chunk_type>;
+
+  /** The type of the iterator over the chunk view */
+  using chunk_const_iterator = private_iterator<chunk_type const>;
+
+ public:
+  /** Default constructor */
+  chunk_view() = default;
+  chunk_view(const chunk_view&) = default;
+  chunk_view(chunk_view&&) = default;
+  chunk_view& operator=(const chunk_view&) = default;
+  chunk_view& operator=(chunk_view&&) = default;
+
+  /**
+   * Constructor taking ranges for the data and index ranges, along with sizes
+   */
+  chunk_view(R& data, std::ranges::range_difference_t<R> num_chunks)
+      : data_begin_(std::ranges::begin(data))
+      , num_chunks_(num_chunks) {
+  }
+
+  /*
+   * Define the necessary members for view_interface.
+   */
+
+  /** Return iterator to the beginning of the chunk view */
+  auto begin() {
+    return chunk_iterator(data_begin_, chunk_size_, 0);
+  }
+
+  /** Return iterator to the end of the chunk view */
+  auto end() {
+    return chunk_iterator(data_begin_, num_chunks_, num_chunks_);
+  }
+
+  /** Return const iterator to the beginning of the chunk view */
+  auto begin() const {
+    return chunk_const_terator(data_begin_, num_chunks_, 0);
+  }
+
+  /** Return const iterator to the end of the chunk view */
+  auto end() const {
+    return chunk_const_iterator(data_begin_, num_chunks_, num_chunks_);
+  }
+
+  /** Return const iterator to the beginning of the chunk view */
+  auto cbegin() const {
+    return chunk_const_terator(data_begin_, 0);
+  }
+
+  /** Return const iterator to the end of the chunk view */
+  auto cend() const {
+    return chunk_const_iterator(data_begin_, num_chunks_);
+  }
+
+  /** Return the number of subranges in the chunk view */
+  auto size() const {
+    return num_chunks_;
+  }
+
+ private:
+  template <class V>
+  struct private_iterator : public iterator_facade<private_iterator<V>> {
+    using value_type = V;
+    using reference_type = V&;
+
+    /** Default constructor */
+    private_iterator() = default;
+
+    /** Primary constructor */
+    private_iterator(
+        data_iterator_type data_begin,
+        data_index_type chunk_size,
+        data_index_type index = 0)
+        : data_begin_(data_begin)
+        , index_(index)
+        , chunk_size_(chunk_size) {
+    }
+
+    /** Primary constructor */
+    private_iterator(chunk_view&& parent, data_index_type index = 0)
+        : index_(index)
+        , data_begin_(std::ranges::begin(parent)) {
+    }
+
+    /*************************************************************************
+     * Functions needed for iterator_facade
+     * Here we just supply the minimum needed to make the iterator work
+     *************************************************************************/
+
+    /*
+     * The critical function for defining the iterator as it determines the
+     * value type, etc and also does the dereference.  In the case of the
+     * chunk_view, the value type is a subrange of the data range, and
+     * the subrage return value must be a proxy object, which we return by
+     * value.  This is fine, since we can't assign a subrange to another.
+     * We do, however, want to be able to modify the contents of the subrange.
+     */
+    value_type dereference() const {
+      return {data_begin_, data_begin_ + chunk_size_};
+    }
+
+    /** Advance the iterator by n */
+    auto advance(data_index_type n) {
+      index_ += n;
+      return *this;
+    }
+
+    /** Return the distance to another iterator */
+    auto distance_to(const private_iterator& other) const {
+      return other.index_ - index_;
+    }
+
+    /** Compare two iterators for equality */
+    bool operator==(const private_iterator& other) const {
+      return data_begin_ == other.data_begin_ && index_ == other.index_;
+    }
+
+    /** Flag to indicate that the iterator is not a single pass iterator */
+    static const bool single_pass_iterator = false;
+
+    /**
+     * Iterator to the beginning of the data range.  This is a copy of the
+     * parent data_begin_.  We maintain the copy to avoid an indirection through
+     * the parent pointer.
+     */
+    data_iterator_type data_begin_;
+
+    /** The index to the current location of the iterator */
+    data_index_type index_;
+
+    /** The chunk size.  Also cached from parent. */
+    data_index_type chunk_size_;
+  };
+
+  /** The beginning of the data range */
+  std::ranges::iterator_t<R> data_begin_;
+
+  /** The number of chunks in the chunk_range */
+  std::ranges::range_difference_t<R> chunk_size_;
+
+  /** The number of chunks in the chunk_range */
+  std::ranges::range_difference_t<R> num_chunks_;
+};
+
+/** Deduction guide for chunk_view */
+template <class R, class I>
+chunk_view(R, I) -> chunk_view<std::ranges::subrange<R>>;
+
+/**
+ * Define "chunk()" cpo for creating chunk views
+ */
+namespace _chunk {
+struct _fn {
+  // @todo Should this take a viewable range?
+  // template <std::ranges::viewable_range T>
+  template <std::ranges::random_access_range T>
+  auto constexpr operator()(T&& t) const {
+    return chunk_view<T>{std::forward<T>(t)};
+  }
+};
+}  // namespace _chunk
+inline namespace _cpo {
+inline constexpr auto chunk = _chunk::_fn{};
+}  // namespace _cpo
+
+#endif  // TILEDB_CHUNK_VIEW_H
--- tiledb/common/util/test/unit_chunk_view.cc
+++ tiledb/common/util/test/unit_chunk_view.cc
@@ -36,3 +36,24 @@
 TEST_CASE("chunk_view: null test", "[chunk_view][null test]") {
   REQUIRE(true);
 }
+
+// Test that the chunk_view satisfies the expected concepts
+TEST_CASE("chunk_view: Range concepts", "[chunk_view][concepts]") {
+  using test_type = chunk_view<std::vector<double>>;
+
+  CHECK(std::ranges::range<test_type>);
+  CHECK(!std::ranges::borrowed_range<test_type>);
+  CHECK(std::ranges::sized_range<test_type>);
+  CHECK(std::ranges::view<test_type>);
+  CHECK(std::ranges::input_range<test_type>);
+  CHECK(!std::ranges::
+            output_range<test_type, std::ranges::range_value_t<test_type>>);
+  CHECK(std::ranges::forward_range<test_type>);
+  CHECK(std::ranges::bidirectional_range<test_type>);
+  CHECK(std::ranges::random_access_range<test_type>);
+  CHECK(!std::ranges::contiguous_range<test_type>);
+  CHECK(std::ranges::common_range<test_type>);
+  CHECK(std::ranges::viewable_range<test_type>);
+
+  CHECK(std::ranges::view<test_type>);
+}
