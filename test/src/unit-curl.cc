1e0a5ab3311116cb32de66a550d3391b77ee0e6e
KiterLuc <67824247+KiterLuc@users.noreply.github.com>
2024-06-20 04:55:44 +0200

    [Backport release-2.24] Add config option to override the payer namespace for REST requests. (#5105) (#5126)
    Backport bdfc19c0aa746a265fd32b2872b0304621afbb2d from #5105.
    
    ---
    TYPE: CONFIG
    DESC: Add `rest.payer_namespace` config option to set the namespace to
    be charged for REST requests.
    
    Co-authored-by: Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
    Co-authored-by: Seth Shelnutt <seth@tiledb.io>
    Co-authored-by: eric-hughes-tiledb <82400964+eric-hughes-tiledb@users.noreply.github.com>

--- test/src/unit-curl.cc
+++ test/src/unit-curl.cc
@@ -138,9 +138,19 @@ TEST_CASE(
 
   ContextResources resources(
       cfg, tiledb::test::g_helper_logger(), 1, 1, "test");
-  // We copy because the [] operator is not const-qualified.
-  auto extra_headers = resources.rest_client()->extra_headers();
-  CHECK(extra_headers.size() == 2);
-  CHECK(extra_headers["abc"] == "def");
-  CHECK(extra_headers["ghi"] == "jkl");
+  const auto& extra_headers = resources.rest_client()->extra_headers();
+  CHECK(extra_headers.at("abc") == "def");
+  CHECK(extra_headers.at("ghi") == "jkl");
+}
+
+TEST_CASE(
+    "RestClient: Ensure payer namespace is set",
+    "[rest-client][payer-namespace]") {
+  tiledb::sm::Config cfg;
+  REQUIRE(cfg.set("rest.payer_namespace", "foo").ok());
+
+  ContextResources resources(
+      cfg, tiledb::test::g_helper_logger(), 1, 1, "test");
+  const auto& extra_headers = resources.rest_client()->extra_headers();
+  CHECK(extra_headers.at("X-Payer") == "foo");
 }
