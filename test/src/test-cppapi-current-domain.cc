91df9849db8921e2a60143a9d8f5336772c5f133
KiterLuc <67824247+KiterLuc@users.noreply.github.com>
2024-07-09 12:04:35 +0300

    Disallow writing outside of the current domain. (#5165)
    This change disallow the user to write outside of the current domain.
    
    ---
    TYPE: FEATURE
    DESC: Disallow writing outside of the current domain.

--- test/src/test-cppapi-current-domain.cc
+++ test/src/test-cppapi-current-domain.cc
@@ -240,3 +240,117 @@ TEST_CASE_METHOD(
     vfs.remove_dir(array_name);
   }
 }
+
+TEST_CASE_METHOD(
+    CurrentDomainFx,
+    "C++ API: CurrentDomain - Write outside of current domain",
+    "[cppapi][ArraySchema][currentDomain]") {
+  const std::string array_name = "test_current_domain_write";
+
+  tiledb::VFS vfs(ctx_);
+  if (vfs.is_dir(array_name)) {
+    vfs.remove_dir(array_name);
+  }
+
+  // Create domain
+  tiledb::Domain domain(ctx_);
+  auto d1 = tiledb::Dimension::create<int32_t>(ctx_, "dim1", {{0, 100}}, 10);
+  auto d2 = tiledb::Dimension::create(
+      ctx_, "dim2", TILEDB_STRING_ASCII, nullptr, nullptr);
+  domain.add_dimension(d1);
+  domain.add_dimension(d2);
+
+  // Create an NDRectangle and set ranges
+  tiledb::NDRectangle ndrect(ctx_, domain);
+  int range_one[] = {10, 20};
+  ndrect.set_range(0, range_one[0], range_one[1]);
+  ndrect.set_range(1, std::string("b"), std::string("db"));
+
+  // Create a currentDomain and set the NDRectangle
+  tiledb::CurrentDomain current_domain(ctx_);
+  current_domain.set_ndrectangle(ndrect);
+
+  // Create array schema.
+  tiledb::ArraySchema schema(ctx_, TILEDB_SPARSE);
+  schema.set_domain(domain);
+  schema.add_attribute(tiledb::Attribute::create<int>(ctx_, "a"));
+
+  tiledb::ArraySchemaExperimental::set_current_domain(
+      ctx_, schema, current_domain);
+
+  // Create array
+  tiledb::Array::create(array_name, schema);
+
+  tiledb::Array array(ctx_, array_name, TILEDB_WRITE);
+
+  // Some of the data here is outside of the current domain.
+  std::vector<int32_t> dim1 = {12, 14};
+  std::vector<int32_t> dim1_out = {12, 22};
+  std::vector<char> dim2 = {'b', 'c'};
+  std::vector<uint64_t> dim2_offsets = {0, 1};
+  std::vector<char> dim2_out = {'a', 'c'};
+  std::vector<int> a1 = {1, 2};
+
+  // All data in current domain.
+  tiledb::Query query1(ctx_, array, TILEDB_WRITE);
+  query1.set_layout(TILEDB_UNORDERED)
+      .set_data_buffer("a", a1)
+      .set_data_buffer("dim1", dim1)
+      .set_offsets_buffer("dim2", dim2_offsets)
+      .set_data_buffer("dim2", dim2);
+  query1.submit();
+
+  // Dimension 1 data out of current domain.
+  tiledb::Query query2(ctx_, array, TILEDB_WRITE);
+  query2.set_layout(TILEDB_UNORDERED)
+      .set_data_buffer("a", a1)
+      .set_data_buffer("dim1", dim1_out)
+      .set_offsets_buffer("dim2", dim2_offsets)
+      .set_data_buffer("dim2", dim2);
+  CHECK_THROWS_WITH(
+      query2.submit(),
+      Catch::Matchers::ContainsSubstring(
+          "Cells are written outside of the defined current domain."));
+
+  // Dimension 2 data out of current domain.
+  tiledb::Query query3(ctx_, array, TILEDB_WRITE);
+  query3.set_layout(TILEDB_UNORDERED)
+      .set_data_buffer("a", a1)
+      .set_data_buffer("dim1", dim1)
+      .set_offsets_buffer("dim2", dim2_offsets)
+      .set_data_buffer("dim2", dim2_out);
+  CHECK_THROWS_WITH(
+      query3.submit(),
+      Catch::Matchers::ContainsSubstring(
+          "Cells are written outside of the defined current domain."));
+  array.close();
+
+  // Create new currentDomain to allow all data
+  tiledb::CurrentDomain new_current_domain(ctx_);
+  int range_two[] = {10, 22};
+  tiledb::NDRectangle ndrect_two(ctx_, domain);
+  ndrect_two.set_range(0, range_two[0], range_two[1]);
+  ndrect_two.set_range(1, std::string("a"), std::string("db"));
+  new_current_domain.set_ndrectangle(ndrect_two);
+
+  // Schema evolution
+  tiledb::ArraySchemaEvolution se(ctx_);
+  se.expand_current_domain(new_current_domain);
+  se.array_evolve(array_name);
+
+  // Now try data that didn't succeed before.
+  tiledb::Array array2(ctx_, array_name, TILEDB_WRITE);
+  tiledb::Query query4(ctx_, array2, TILEDB_WRITE);
+  query4.set_layout(TILEDB_UNORDERED)
+      .set_data_buffer("a", a1)
+      .set_data_buffer("dim1", dim1_out)
+      .set_offsets_buffer("dim2", dim2_offsets)
+      .set_data_buffer("dim2", dim2_out);
+  query4.submit();
+  array2.close();
+
+  // Clean up.
+  if (vfs.is_dir(array_name)) {
+    vfs.remove_dir(array_name);
+  }
+}
