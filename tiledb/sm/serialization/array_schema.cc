a3b5c4408e3095fef9b244b4c53a5a170e4f994a
Shaun Reed <shaunrd0@gmail.com>
2024-07-11 15:19:18 -0400

    Fix redefinition for stub function.

--- tiledb/sm/serialization/array_schema.cc
+++ tiledb/sm/serialization/array_schema.cc
@@ -2152,7 +2152,10 @@ void serialize_load_array_schema_response(
   throw ArraySchemaSerializationDisabledException();
 }
 
-shared_ptr<ArraySchema> deserialize_load_array_schema_response(
+std::tuple<
+    shared_ptr<ArraySchema>,
+    std::unordered_map<std::string, shared_ptr<ArraySchema>>>
+deserialize_load_array_schema_response(
     SerializationType, const Buffer&, shared_ptr<MemoryTracker>) {
   throw ArraySchemaSerializationDisabledException();
 }
