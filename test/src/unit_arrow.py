785b91392582027317d782a88f968292a84bfb4b
Agisilaos Kounelis <kounelisagis@gmail.com>
2024-08-13 20:39:48 +0300

    Implement arrow array with 'large' strings

--- test/src/unit_arrow.py
+++ test/src/unit_arrow.py
@@ -53,10 +53,10 @@ def rand_ascii_bytes(size=5, printable=False):
     return b''.join([gen_chr(127, printable).encode('utf-8') for _ in range(0,size)])
 
 # ************************************************************************** #
-#           Test class                                                       #
+#           Test classes                                                     #
 # ************************************************************************** #
 
-class DataFactory():
+class DataFactory32():
   def __init__(self, col_size):
     self.results = {}
     self.col_size = col_size
@@ -75,28 +75,65 @@ class DataFactory():
     for dt in (np.int8, np.uint8, np.int16, np.uint16, np.int32, np.uint32, np.int64, np.uint64):
         key = np.dtype(dt).name
         dtinfo = np.iinfo(dt)
-        self.data[key] = np.random.randint(dtinfo.min, dtinfo.max, size=col_size, dtype=dt)
+        self.data[key] = pa.array(np.random.randint(dtinfo.min, dtinfo.max, size=col_size, dtype=dt))
 
     for dt in (np.float32, np.float64):
         key = np.dtype(dt).name
-        self.data[key] = np.random.rand(col_size).astype(dt)
+        self.data[key] = pa.array(np.random.rand(col_size).astype(dt))
 
     # var-len (strings)
-    self.data['tiledb_char'] = np.array([rand_ascii_bytes(np.random.randint(1,100))
-                                       for _ in range(col_size)]).astype("S1")
-    self.data['utf_string1'] = np.array([rand_utf8(np.random.randint(1, 100))
+    self.data['tiledb_char'] = pa.array(np.array([rand_ascii_bytes(np.random.randint(1,100))
+                                       for _ in range(col_size)]).astype("S1"))
+    utf_strings = np.array([rand_utf8(np.random.randint(1, 100))
                                        for _ in range(col_size)]).astype("U0")
+    self.data['utf_string1'] = pa.array(utf_strings)
 
-    # another version with some important cells set to empty
-    utf_strings_with_empty = np.copy(self.data['utf_string1'])
-    utf_strings_with_empty[np.random.randint(0, col_size, size=col_size//2)] = ''
-    self.data['utf_string2'] = utf_strings_with_empty
+    # another version with some cells set to empty
+    utf_strings[np.random.randint(0, col_size, size=col_size//2)] = ''
+    self.data['utf_string2'] = pa.array(utf_strings)
 
-    self.data['datetime_ns'] = rand_datetime64_array(col_size)
+    self.data['datetime_ns'] = pa.array(rand_datetime64_array(col_size))
 
     ##########################################################################
 
-    self.arrays = [pa.array(val) for val in self.data.values()]
+    self.arrays = list(self.data.values())
+    self.names = list(self.data.keys())
+
+  def import_result(self, name, c_array, c_schema):
+    self.results[name] = pa.Array._import_from_c(c_array, c_schema)
+
+  def check(self):
+    for key,val in self.data.items():
+      assert (key in self.results), "Expected key '{}' not found in results!".format(key)
+
+      res_val = self.results[key]
+      assert_array_equal(val, res_val)
+
+    return True
+
+
+class DataFactory64():
+  def __init__(self, col_size):
+    self.results = {}
+    self.col_size = col_size
+    self.create()
+
+  def __len__(self):
+    if not self.data:
+      raise ValueError("Uninitialized data")
+    return len(self.data)
+
+  def create(self):
+    # generate test data for all columns
+    col_size = self.col_size
+    self.data = {}
+
+    self.data['utf_big_string'] = pa.array([rand_utf8(np.random.randint(1, 100))
+                                       for _ in range(col_size)], type=pa.large_utf8())
+
+    ##########################################################################
+
+    self.arrays = list(self.data.values())
     self.names = list(self.data.keys())
 
   def import_result(self, name, c_array, c_schema):
