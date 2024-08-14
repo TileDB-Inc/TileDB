813ed1783b3330df6779d91e740cf9031f8508b8
Shaun Reed <shaunrd0@gmail.com>
2024-08-14 09:20:33 -0400

    Add config docs to cpp_api/config.h

--- tiledb/api/c_api/config/config_api_external.h
+++ tiledb/api/c_api/config/config_api_external.h
@@ -750,7 +750,7 @@ TILEDB_EXPORT void tiledb_config_free(tiledb_config_t** config) TILEDB_NOEXCEPT;
  *    **Default**: true
  * - `rest.use_refactored_array_open_and_query_submit` <br>
  *    If true, the new REST routes and APIs for opening an array and submitting
- * a query will be used <br>
+ *    a query will be used <br>
  *    **Default**: true
  * - `rest.curl.buffer_size` <br>
  *    Set curl buffer size for REST requests <br>
