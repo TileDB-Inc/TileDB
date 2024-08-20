8acd911ae4b5e2457ad1b1d32c0e8f5df3fb43e1
Robert Bindar <robertbindar@gmail.com>
2024-08-20 17:25:06 +0300

    more tests

--- tiledb/common/evaluator/test/unit_evaluator.cc
+++ tiledb/common/evaluator/test/unit_evaluator.cc
@@ -32,25 +32,310 @@
 
 using namespace tiledb::common;
 
-TEST_CASE("constructor", "[evaluator]") {
-  auto f = [](std::string key) { return key; };
+template <class Key, class Value>
+class WhiteBoxPolicy : public virtual CachePolicyBase<Key, Value> {
+  using base = CachePolicyBase<Key, Value>;
 
-  Evaluator<MaxEntriesCache<std::string, std::string, 3>, decltype(f)>
-      max_entries_eval(f);
-  max_entries_eval("key");   // miss
-  max_entries_eval("key");   // hit
-  max_entries_eval("key2");  // miss
-  max_entries_eval("key3");  // miss
-  max_entries_eval("key4");  // evict
+ public:
+  [[nodiscard]] bool has_entry(const Key& key) const {
+    return base::entries_.find(key) != base::entries_.end();
+  }
+  [[nodiscard]] bool entry_in_progress(const Key& key) const {
+    return base::inprogress_.find(key) != base::inprogress_.end();
+  }
+  [[nodiscard]] size_t entries_size() const {
+    return base::entries_.size();
+  }
+  [[nodiscard]] size_t inprogress_size() const {
+    return base::inprogress_.size();
+  }
+  [[nodiscard]] bool is_mru(const Key& key) const {
+    return base::lru_.back() == key;
+  }
+  [[nodiscard]] bool lru_contains(const Key& key) const {
+    return std::find(base::lru_.begin(), base::lru_.end(), key) !=
+           base::lru_.end();
+  }
+};
 
-  Evaluator<ImmediateEvaluation<std::string, std::string>, decltype(f)>
+template <class Key, class Value, size_t N = 0>
+class WhiteBoxMaxEntriesPolicy : public WhiteBoxPolicy<Key, Value>,
+                                 public MaxEntriesCache<Key, Value, N> {
+  using base = MaxEntriesCache<Key, Value, N>;
+
+ public:
+  [[nodiscard]] size_t num_entries() const {
+    return base::num_entries_;
+  }
+  void evict_lru() {
+    base::enforce_policy(Value{});
+  }
+};
+
+template <class Key, class Value>
+class WhiteBoxMemoryBudgetPolicy : public WhiteBoxPolicy<Key, Value>,
+                                   public MemoryBudgetedCache<Key, Value> {
+  using base = MemoryBudgetedCache<Key, Value>;
+  using SizeFn = typename base::SizeFn;
+
+ public:
+  WhiteBoxMemoryBudgetPolicy(SizeFn size_fn, size_t budget)
+      : base(size_fn, budget) {
+  }
+
+  [[nodiscard]] size_t memory_consumed() const {
+    return base::memory_consumed_;
+  }
+  void evict_lru() {
+    base::enforce_policy(Value{});
+  }
+};
+
+template <class Key, class Value>
+class WhiteBoxImmediateEvaluationPolicy
+    : public ImmediateEvaluation<Key, Value> {
+  using base = ImmediateEvaluation<Key, Value>;
+
+ public:
+  using return_type = typename base::return_type;
+
+  WhiteBoxImmediateEvaluationPolicy()
+      : num_executions_(0) {
+  }
+
+  template <class F>
+  return_type operator()(F f, Key key) {
+    ++num_executions_;
+    return base::operator()(f, key);
+  }
+
+  uint64_t num_executions() const {
+    return num_executions_;
+  }
+
+ private:
+  uint64_t num_executions_;
+};
+
+template <class Policy, class F>
+class WhiteBoxEvaluator : public Evaluator<Policy, F> {
+  using base = Evaluator<Policy, F>;
+
+ public:
+  template <class... Args>
+  WhiteBoxEvaluator(F f, Args&&... args)
+      : base(f, std::forward<Args>(args)...) {
+  }
+
+  [[nodiscard]] Policy& policy() {
+    return base::caching_policy_;
+  }
+};
+
+TEST_CASE("Testing immediate evaluation", "[evaluator][immediate]") {
+  auto f = [](std::string) {
+    static uint64_t counter = 0;  // safe only in this testing scenario
+    return "test" + std::to_string(counter++);
+  };
+
+  WhiteBoxEvaluator<
+      WhiteBoxImmediateEvaluationPolicy<std::string, std::string>,
+      decltype(f)>
       no_cache_eval(f);
-  no_cache_eval("key");  // miss
-  no_cache_eval("key");  // miss
+  REQUIRE(no_cache_eval.policy().num_executions() == 0);
+  auto v1 = no_cache_eval("key");
+  CHECK(no_cache_eval.policy().num_executions() == 1);
+  CHECK(*v1 == "test0");
+  auto v2 = no_cache_eval("key");
+  CHECK(no_cache_eval.policy().num_executions() == 2);
+  CHECK(*v2 == "test1");
+}
 
-  auto size_fn = [](const std::string& val) { return val.size(); };
+TEST_CASE("Ready cached items are stored properly", "[evaluator][entries]") {
+  auto f = [](std::string) { return "test"; };
+  WhiteBoxEvaluator<
+      WhiteBoxMaxEntriesPolicy<std::string, std::string, 2>,
+      decltype(f)>
+      max_entries_eval(f);
 
-  Evaluator<MemoryBudgetedCache<std::string, std::string>, decltype(f)>
-      memory_budgeted_cache(f, size_fn, static_cast<size_t>(1024));
-  memory_budgeted_cache("key");  // miss
+  max_entries_eval("key");
+  CHECK(max_entries_eval.policy().entries_size() == 1);
+  CHECK(max_entries_eval.policy().has_entry("key"));
+
+  max_entries_eval("key2");
+  CHECK(max_entries_eval.policy().entries_size() == 2);
+  CHECK(max_entries_eval.policy().has_entry("key"));
+  CHECK(max_entries_eval.policy().has_entry("key2"));
+
+  // A cache hit doesn't change the content of the entries map
+  max_entries_eval("key");
+  CHECK(max_entries_eval.policy().entries_size() == 2);
+  CHECK(max_entries_eval.policy().has_entry("key"));
+  CHECK(max_entries_eval.policy().has_entry("key2"));
+
+  // Capacity reached, key should be evicted
+  max_entries_eval("key3");
+  CHECK(max_entries_eval.policy().entries_size() == 2);
+  CHECK(max_entries_eval.policy().has_entry("key"));
+  CHECK(max_entries_eval.policy().has_entry("key3"));
+}
+
+TEST_CASE("Testing LRU list is accurate", "[evaluator][lru]") {
+  auto f = [](std::string) { return "test"; };
+  WhiteBoxEvaluator<
+      WhiteBoxMaxEntriesPolicy<std::string, std::string, 2>,
+      decltype(f)>
+      max_entries_eval(f);
+
+  max_entries_eval("key");
+  CHECK(max_entries_eval.policy().entries_size() == 1);
+  CHECK(max_entries_eval.policy().has_entry("key"));
+  CHECK(max_entries_eval.policy().is_mru("key"));
+  CHECK(max_entries_eval.policy().inprogress_size() == 0);
+
+  max_entries_eval("key2");
+  CHECK(max_entries_eval.policy().is_mru("key2"));
+
+  max_entries_eval("key");
+  CHECK(max_entries_eval.policy().is_mru("key"));
+
+  // Evict, key is now MRU
+  CHECK(max_entries_eval.policy().entries_size() == 2);
+  max_entries_eval.policy().evict_lru();
+  CHECK(max_entries_eval.policy().entries_size() == 1);
+  CHECK(max_entries_eval.policy().is_mru("key"));
+
+  max_entries_eval("key3");
+  CHECK(max_entries_eval.policy().is_mru("key3"));
+
+  // Max entries reached, key should be evicted
+  CHECK(max_entries_eval.policy().lru_contains("key"));
+  max_entries_eval("key4");
+  CHECK(max_entries_eval.policy().lru_contains("key") == false);
+  CHECK(max_entries_eval.policy().is_mru("key4"));
+  CHECK(max_entries_eval.policy().entries_size() == 2);
+}
+
+TEST_CASE(
+    "Inprogress list contains items that are in the process of being evaluated",
+    "[evaluator][inprogress]") {
+  std::promise<void> finish_I_promise;
+  auto f = [&finish_I_promise](std::string) {
+    finish_I_promise.get_future().wait();
+    return "test";
+  };
+  WhiteBoxEvaluator<
+      WhiteBoxMaxEntriesPolicy<std::string, std::string, 2>,
+      decltype(f)>
+      eval(f);
+
+  std::thread t([&eval]() {
+    // This blocks until f(key) is evaluated
+    eval("key");
+  });
+
+  // Give the thread some heads up
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+  // Now that key is in the process of evaluation, let's do some checks
+  CHECK(eval.policy().inprogress_size() == 1);
+  CHECK(eval.policy().entry_in_progress("key"));
+  CHECK(eval.policy().entries_size() == 0);
+
+  // Unblock the callback so that the evaluation can finish and the entry gets
+  // cached
+  finish_I_promise.set_value();
+
+  t.join();
+
+  // The item moved from inprogress to ready
+  CHECK(eval.policy().entries_size() == 1);
+  CHECK(eval.policy().inprogress_size() == 0);
+}
+
+TEST_CASE(
+    "Readers that come once an entry is in progress are blocked until the "
+    "entry is ready",
+    "[evaluator][inprogress]") {
+  std::promise<void> finish_I_promise;
+  bool first_thread = true;
+  bool second_exec = false;
+  auto f = [&finish_I_promise, &first_thread, &second_exec](std::string) {
+    if (first_thread) {
+      finish_I_promise.get_future().wait();
+    } else {
+      second_exec = true;
+    }
+    return "test";
+  };
+
+  WhiteBoxEvaluator<
+      WhiteBoxMaxEntriesPolicy<std::string, std::string, 2>,
+      decltype(f)>
+      eval(f);
+
+  std::thread t([&eval]() {
+    // This blocks until f(key) is evaluated
+    eval("key");
+  });
+
+  // Give the thread some heads up
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+  first_thread = false;
+  std::thread t2([&eval]() {
+    // This should wait within the evaluator until the value becomes available
+    eval("key");
+  });
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+  // Check a single entry is in progress and no ready entries are present
+  CHECK(eval.policy().entries_size() == 0);
+  CHECK(eval.policy().inprogress_size() == 1);
+  CHECK(eval.policy().entry_in_progress("key"));
+
+  // Unblock the first thread so that the evaluation can finish and f(key) gets
+  // cached, second reader gets the value as well and becomes unblocked
+  finish_I_promise.set_value();
+
+  t.join();
+  t2.join();
+
+  // Make sure the second reader got a cached value and didn't just execute the
+  // callback again.
+  CHECK(second_exec == false);
+}
+
+TEST_CASE("Memory budgeting policy is enforced", "[evaluator][memory_budget]") {
+  size_t budget = 4096;
+  auto f = [](std::string) { return "test"; };
+  auto sizefn = [](const std::string& val) {
+    return sizeof(std::string) + val.size();
+  };
+
+  WhiteBoxEvaluator<
+      WhiteBoxMemoryBudgetPolicy<std::string, std::string>,
+      decltype(f)>
+      mem_budgeted_eval(f, sizefn, budget);
+
+  CHECK(mem_budgeted_eval.policy().memory_consumed() == 0);
+
+  // TODO: unit size is incorrect here
+  size_t unit_size = sizeof(std::shared_ptr<std::string>) + sizefn("test");
+  for (size_t c = 0; c < budget; c += unit_size) {
+    mem_budgeted_eval("key" + std::to_string(c));
+    CHECK(mem_budgeted_eval.policy().memory_consumed() == c + unit_size);
+  }
+  CHECK(mem_budgeted_eval.policy().entries_size() == budget / unit_size);
+
+  mem_budgeted_eval.policy().enforce_policy("test");
+  CHECK(mem_budgeted_eval.policy().entries_size() == budget / unit_size - 1);
+
+  // Bring the memory consumption over the budget, trigger automatic eviction
+  mem_budgeted_eval("key");
+  mem_budgeted_eval("key");
+  CHECK(mem_budgeted_eval.policy().entries_size() == budget / unit_size);
+  CHECK(mem_budgeted_eval.policy().memory_consumed() == budget);
+
+  // TODO: test eviction of multiple entries until the new value fits
 }
