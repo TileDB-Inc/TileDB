ef35123d614364e1e84632601a4fa561bc22af2c
Luc Rancourt <lucrancourt@gmail.com>
2024-09-10 20:28:44 +0200

    Revert "[Backport release-2.26] Fix schema timestamps used when loading enumerations from REST. (#5291) (#5293)"
    This reverts commit 96835840d0244313b9ddc824a3aa7e9d99592780.

--- test/src/unit-cppapi-schema-evolution.cc
+++ test/src/unit-cppapi-schema-evolution.cc
@@ -810,7 +810,7 @@ TEST_CASE(
 
 TEST_CASE(
     "SchemaEvolution Error Handling Tests",
-    "[cppapi][schema][evolution][errors][rest]") {
+    "[cppapi][schema][evolution][errors]") {
   auto ase = make_shared<tiledb::sm::ArraySchemaEvolution>(
       HERE(), tiledb::test::create_test_memory_tracker());
   REQUIRE_THROWS(ase->evolve_schema(nullptr));
