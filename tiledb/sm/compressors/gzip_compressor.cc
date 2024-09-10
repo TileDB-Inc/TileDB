d350260eb69320cd72d5710e30fbfa820e2b6600
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-09-10 13:53:21 +0300

    Clean-up.

--- tiledb/sm/compressors/gzip_compressor.cc
+++ tiledb/sm/compressors/gzip_compressor.cc
@@ -151,8 +151,8 @@ uint64_t GZip::overhead(uint64_t buffer_size) {
   // The zlib encoding adds 6 bytes (we don't use compression dictionary)
   // and the overhead of deflate was taken from
   // https://stackoverflow.com/a/23578269.
-  return 6 + 5 * uint64_t((std::floor(buffer_size / 16834.0)) + 1);
+  return 6 + 5 * uint64_t(std::floor(buffer_size / 16834.0) + 1);
 }
 
-};  // namespace sm
+}  // namespace sm
 }  // namespace tiledb
