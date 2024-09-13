6b410a2639ebad76b28cb9076eee04f68ba972dd
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-09-13 19:25:25 +0300

    Remove `rest.ignore_ssl_validation` config option.

--- tiledb/sm/cpp_api/config.h
+++ tiledb/sm/cpp_api/config.h
@@ -885,9 +885,6 @@ class Config {
    *    If true, incomplete queries received from server are automatically
    *    resubmitted before returning to user control. <br>
    *    **Default**: "true"
-   * - `rest.ignore_ssl_validation` <br>
-   *    Have curl ignore ssl peer and host validation for REST server. <br>
-   *    **Default**: false
    * - `rest.creation_access_credentials_name` <br>
    *    The name of the registered access key to use for creation of the REST
    *    server. <br>
