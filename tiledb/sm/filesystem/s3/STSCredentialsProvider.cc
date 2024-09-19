6d2c907921b6012bee02ca14a963876e1339a462
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-09-20 02:32:18 +0300

    Always use the region specified in the client config for the STS credentials provider endpoint.

--- tiledb/sm/filesystem/s3/STSCredentialsProvider.cc
+++ tiledb/sm/filesystem/s3/STSCredentialsProvider.cc
@@ -77,21 +77,15 @@ STSAssumeRoleWebIdentityCredentialsProvider::
         Aws::Client::ClientConfiguration config)
     : m_initialized(false) {
   // check environment variables
-  Aws::String tmpRegion = Aws::Environment::GetEnv("AWS_DEFAULT_REGION");
   m_roleArn = Aws::Environment::GetEnv("AWS_ROLE_ARN");
   m_tokenFile = Aws::Environment::GetEnv("AWS_WEB_IDENTITY_TOKEN_FILE");
   m_sessionName = Aws::Environment::GetEnv("AWS_ROLE_SESSION_NAME");
 
   // check profile_config if either m_roleArn or m_tokenFile is not loaded from
-  // environment variable region source is not enforced, but we need it to
-  // construct sts endpoint, if we can't find from environment, we should check
-  // if it's set in config file.
-  if (m_roleArn.empty() || m_tokenFile.empty() || tmpRegion.empty()) {
+  // environment variable
+  if (m_roleArn.empty() || m_tokenFile.empty()) {
     auto profile =
         Aws::Config::GetCachedConfigProfile(Aws::Auth::GetConfigProfileName());
-    if (tmpRegion.empty()) {
-      tmpRegion = profile.GetRegion();
-    }
     // If either of these two were not found from environment, use whatever
     // found for all three in config file
     if (m_roleArn.empty() || m_tokenFile.empty()) {
@@ -125,18 +119,6 @@ STSAssumeRoleWebIdentityCredentialsProvider::
         STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
         "Resolved role_arn from profile_config or environment variable to be "
             << m_roleArn);
-  }
-
-  if (config.region.empty()) {
-    if (tmpRegion.empty()) {
-      tmpRegion = Aws::Region::US_EAST_1;
-    } else {
-      AWS_LOGSTREAM_DEBUG(
-          STS_ASSUME_ROLE_WEB_IDENTITY_LOG_TAG,
-          "Resolved region from profile_config or environment variable to be "
-              << tmpRegion);
-    }
-    config.region = tmpRegion;
   }
 
   if (m_sessionName.empty()) {
