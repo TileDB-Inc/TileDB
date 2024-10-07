95ea2b65d17b8caf209554105b02c7e1027bba01
Dušan Baran <work.dusanbaran@gmail.com>
2024-10-07 11:16:53 +0200

    Update cmake/arm64_linux_toolchain.cmake
    Co-authored-by: Theodore Tsirpanis <teo@tsirpanis.gr>

--- cmake/arm64_linux_toolchain.cmake
+++ cmake/arm64_linux_toolchain.cmake
@@ -4,8 +4,7 @@
 # Target operating system name.
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
-set(CMAKE_CROSSCOMPILING TRUE)
 
 # Name of C compiler.
-set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc")
-set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++")
+set(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
+set(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
