ae4c9a1e3e7160a2a432aecd321ebcc3da41fb0d
Dušan Baran <work.dusanbaran@gmail.com>
2024-10-14 13:06:45 +0200

    Enable aarch64 release (#5271)
    Produce release binaries for linux aarch64 platform.
    
    Produced artifacts can be seen here:
    https://github.com/dudoslav/TileDB/actions/runs/11179687129
    
    ---
    TYPE: IMPROVEMENT 
    DESC: Linux aarch64 release
    
    ---------
    
    Co-authored-by: Theodore Tsirpanis <teo@tsirpanis.gr>

---
+++ ports/triplets/arm64-linux.cmake
@@ -0,0 +1,15 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+# For AWS SDK. Remove after update from 1.74
+set(VCPKG_CXX_FLAGS "-Wno-error=nonnull -Wno-error=deprecated-declarations")
+set(VCPKG_C_FLAGS "-Wno-error=nonnull -Wno-error=deprecated-declarations")
+
+# Hide symbols in the AWS SDK. Fixes symbol collisions with other libraries
+# like arrow (https://github.com/apache/arrow/issues/42154).
+if("${PORT}" MATCHES "^aws-")
+    set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_CXX_VISIBILITY_PRESET=hidden;-DCMAKE_C_VISIBILITY_PRESET=hidden")
+endif()
