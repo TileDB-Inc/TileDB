6c750b7143ab91ced576bb9b89634a7d9f43c11b
Dusan Baran <work.dusanbaran@gmail.com>
2024-10-16 15:55:22 +0200

    TEMP: Set hardcoded compiler

--- cmake/arm64_linux_toolchain.cmake
+++ cmake/arm64_linux_toolchain.cmake
@@ -5,17 +5,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(TARGET_CC $ENV{TARGET_CC})
-set(TARGET_CXX $ENV{TARGET_CXX})
-
-if (NOT TARGET_CC)
-  set(TARGET_CC "aarch64-unknown-linux-gnu-gcc")
-endif()
-
-if (NOT TARGET_CXX)
-  set(TARGET_CXX "aarch64-unknown-linux-gnu-g++")
-endif()
-
 # Name of C compiler.
-set(CMAKE_C_COMPILER "${TARGET_CC}")
-set(CMAKE_CXX_COMPILER "${TARGET_CXX}")
+set(CMAKE_C_COMPILER "aarch64-unknown-linux-gnu-gcc")
+set(CMAKE_CXX_COMPILER "aarch64-unknown-linux-gnu-g++")
