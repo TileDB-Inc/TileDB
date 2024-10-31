f0727b9b92ebd584f6d9a79e1f8fd3a391728094
Ryan Roelke <rroelke@users.noreply.github.com>
2024-10-31 15:16:30 -0400

    Update tiledb/api/c_api/array_schema/array_schema_api_experimental.h
    Co-authored-by: Shaun M Reed <shaunrd0@gmail.com>

--- tiledb/api/c_api/array_schema/array_schema_api_experimental.h
+++ tiledb/api/c_api/array_schema/array_schema_api_experimental.h
@@ -78,7 +78,7 @@ TILEDB_EXPORT capi_return_t tiledb_array_schema_timestamp_range(
  * tiledb_enumeration_t* enmr;
  * tiledb_array_schema_get_enumeration_from_name(ctx,
  *         array_schema, "states", &enmr);
- * tiledb_attribute_free(&enmr);
+ * tiledb_enumeration_free(&enmr);
  * @endcode
  *
  * @param[in] ctx The TileDB context.
