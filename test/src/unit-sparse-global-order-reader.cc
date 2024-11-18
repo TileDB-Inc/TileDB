0bb8cdd29a0742b807e34f2252a36148ea0a81bc
Ypatia Tsavliri <ypatia@tiledb.com>
2024-11-18 13:20:26 +0200

    Fix budget tests, g.o.r. result tile is now 3496 bytes in size

--- test/src/unit-sparse-global-order-reader.cc
+++ test/src/unit-sparse-global-order-reader.cc
@@ -564,7 +564,7 @@ TEST_CASE_METHOD(
 
   // Specific relationship for failure not known, but these values
   // will result in failure with data being written.
-  total_budget_ = "10000";
+  total_budget_ = "30000";
   // Failure here occurs with the value of 0.1 for ratio_tile_ranges_.
   update_config();
 
@@ -653,7 +653,7 @@ TEST_CASE_METHOD(
 
   // specific relationship for failure not known, but these values
   // will result in failure with data being written.
-  total_budget_ = "15000";
+  total_budget_ = "40000";
   // Failure here occurs with the value of 0.1 for ratio_tile_ranges_.
   update_config();
 
@@ -789,9 +789,9 @@ TEST_CASE_METHOD(
     write_1d_fragment(coords, &coords_size, data, &data_size);
   }
 
-  // Two result tile (2 * (~3000 + 8) will be bigger than the per fragment
+  // Two result tiles (2 * (~3500 + 8) will be bigger than the per fragment
   // budget (1000).
-  total_budget_ = "35000";
+  total_budget_ = "70000";
   ratio_coords_ = "0.11";
   update_config();
 
