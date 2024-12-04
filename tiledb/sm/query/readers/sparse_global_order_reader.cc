bc7930792b6b3dbe60446840169031b5ea66461d
Ypatia Tsavliri <ypatia@tiledb.com>
2024-12-04 16:25:32 +0200

    Fix deadlock in merge_result_cell_slabs

--- tiledb/sm/query/readers/sparse_global_order_reader.cc
+++ tiledb/sm/query/readers/sparse_global_order_reader.cc
@@ -814,6 +814,13 @@ bool SparseGlobalOrderReader<BitmapType>::add_next_cell_to_queue(
                      frag_idx, rc, result_tiles)) {
       return true;
     }
+
+    // This enforces all the coords unfiltering results to be available before
+    // taking the lock on tile_queue_mutex_. This is to avoid a deadlock where a
+    // lock is held forever while waiting for a result to be available, while
+    // the next scheduled task is deadlocking on that lock
+    rc.tile_->wait_all_coords();
+
     std::unique_lock<std::mutex> ul(tile_queue_mutex_);
 
     // Add all the cells in this tile with the same coordinates as this cell
