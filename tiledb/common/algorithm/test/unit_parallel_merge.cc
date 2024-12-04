7230f556aea9d3bfa06b867805a5116a7dd0ec6e
Ryan Roelke <ryan.roelke@tiledb.com>
2024-12-04 15:04:12 -0500

    Move template parallel merge impl into header and template Compare function

--- tiledb/common/algorithm/test/unit_parallel_merge.cc
+++ tiledb/common/algorithm/test/unit_parallel_merge.cc
@@ -36,31 +36,65 @@ using namespace tiledb::algorithm;
 using namespace tiledb::common;
 
 TEST_CASE("parallel merge example", "[algorithm]") {
-  std::vector<std::vector<uint64_t>> streambufs = {
-      {123, 456, 789, 890, 901},
-      {135, 357, 579, 791, 913},
-      {24, 246, 468, 680, 802},
-      {100, 200, 300, 400, 500}};
-
-  std::vector<std::span<uint64_t>> streams = {
-      std::span(streambufs[0]),
-      std::span(streambufs[1]),
-      std::span(streambufs[2]),
-      std::span(streambufs[3]),
-  };
-
   std::vector<uint64_t> output(20);
 
   ParallelMergeOptions options = {.parallel_factor = 4, .min_merge_items = 4};
 
-  ThreadPool pool(options.parallel_factor);
+  ThreadPool pool(4);
 
-  std::unique_ptr<ParallelMergeFuture> future =
-      parallel_merge(pool, options, streams, &output[0]);
+  SECTION("less") {
+    auto cmp = std::less<uint64_t>{};
 
-  future->block();
+    std::vector<std::vector<uint64_t>> streambufs = {
+        {123, 456, 789, 890, 901},
+        {135, 357, 579, 791, 913},
+        {24, 246, 468, 680, 802},
+        {100, 200, 300, 400, 500}};
+    const std::vector<uint64_t> expect = {24,  100, 123, 135, 200, 246, 300,
+                                          357, 400, 456, 468, 500, 579, 680,
+                                          789, 791, 802, 890, 901, 913};
+    std::vector<std::span<uint64_t>> streams = {
+        std::span(streambufs[0]),
+        std::span(streambufs[1]),
+        std::span(streambufs[2]),
+        std::span(streambufs[3]),
+    };
 
-  CHECK(output == std::vector<uint64_t>{24,  100, 123, 135, 200, 246, 300,
-                                        357, 400, 456, 468, 500, 579, 680,
-                                        789, 791, 802, 890, 901, 913});
+    std::unique_ptr<ParallelMergeFuture> future =
+        parallel_merge<uint64_t, decltype(cmp)>(
+            pool, options, streams, cmp, &output[0]);
+
+    future->block();
+
+    CHECK(output.size() == expect.size());
+    CHECK(output == expect);
+  }
+
+  SECTION("greater") {
+    auto cmp = std::greater<uint64_t>{};
+
+    std::vector<std::vector<uint64_t>> streambufs = {
+        {901, 890, 789, 456, 123},
+        {913, 791, 579, 357, 135},
+        {802, 680, 468, 246, 24},
+        {500, 400, 300, 200, 100}};
+    const std::vector<uint64_t> expect = {913, 901, 890, 802, 791, 789, 680,
+                                          579, 500, 468, 456, 400, 357, 300,
+                                          246, 200, 135, 123, 100, 24};
+    std::vector<std::span<uint64_t>> streams = {
+        std::span(streambufs[0]),
+        std::span(streambufs[1]),
+        std::span(streambufs[2]),
+        std::span(streambufs[3]),
+    };
+
+    std::unique_ptr<ParallelMergeFuture> future =
+        parallel_merge<uint64_t, decltype(cmp)>(
+            pool, options, streams, cmp, &output[0]);
+
+    future->block();
+
+    CHECK(output.size() == expect.size());
+    CHECK(output == expect);
+  }
 }
