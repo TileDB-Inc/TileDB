60612591e6b7574673eb9586c6bdbdc02d502f4f
Ypatia Tsavliri <ypatia@tiledb.com>
2024-12-04 12:23:34 +0200

    Fix unit test

--- tiledb/common/thread_pool/test/unit_thread_pool.cc
+++ tiledb/common/thread_pool/test/unit_thread_pool.cc
@@ -98,17 +98,12 @@ size_t random_ms(size_t max = 3) {
  */
 void wait_all(
     ThreadPool& pool, bool use_wait, std::vector<ThreadPool::Task>& results) {
-  std::vector<ThreadPool::ThreadPoolTask*> result_ptrs;
-  for (auto& r : results) {
-    result_ptrs.emplace_back(&r);
-  }
-
   if (use_wait) {
     for (auto& r : results) {
-      REQUIRE(pool.wait(&r).ok());
+      REQUIRE(r.wait().ok());
     }
   } else {
-    REQUIRE(pool.wait_all(result_ptrs).ok());
+    REQUIRE(pool.wait_all(results).ok());
   }
 }
 
@@ -119,15 +114,10 @@ void wait_all(
  */
 Status wait_all_status(
     ThreadPool& pool, bool use_wait, std::vector<ThreadPool::Task>& results) {
-  std::vector<ThreadPool::ThreadPoolTask*> result_ptrs;
-  for (auto& r : results) {
-    result_ptrs.emplace_back(&r);
-  }
-
   if (use_wait) {
     Status ret;
     for (auto& r : results) {
-      auto st = pool.wait(&r);
+      auto st = r.wait();
       if (ret.ok() && !st.ok()) {
         ret = st;
       }
@@ -135,7 +125,7 @@ Status wait_all_status(
 
     return ret;
   } else {
-    return pool.wait_all(result_ptrs);
+    return pool.wait_all(results);
   }
 }
 
@@ -146,18 +136,13 @@ Status wait_all_status(
  */
 uint64_t wait_all_num_status(
     ThreadPool& pool, bool use_wait, std::vector<ThreadPool::Task>& results) {
-  std::vector<ThreadPool::ThreadPoolTask*> result_ptrs;
-  for (auto& r : results) {
-    result_ptrs.emplace_back(&r);
-  }
-
   int num_ok = 0;
   if (use_wait) {
     for (auto& r : results) {
-      num_ok += pool.wait(&r).ok() ? 1 : 0;
+      num_ok += r.wait().ok() ? 1 : 0;
     }
   } else {
-    std::vector<Status> statuses = pool.wait_all_status(result_ptrs);
+    std::vector<Status> statuses = pool.wait_all_status(results);
     for (const auto& st : statuses) {
       num_ok += st.ok() ? 1 : 0;
     }
