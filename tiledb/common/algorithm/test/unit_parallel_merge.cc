fc8936e5fc15628623dda7545f5330c0eded17fd
Ryan Roelke <ryan.roelke@tiledb.com>
2024-12-10 10:51:04 -0500

    Use pmr in parallel merge

--- tiledb/common/algorithm/test/unit_parallel_merge.cc
+++ tiledb/common/algorithm/test/unit_parallel_merge.cc
@@ -30,11 +30,14 @@
 
 #include "tiledb/common/algorithm/parallel_merge.h"
 
+#include <test/support/src/mem_helpers.h>
 #include <test/support/tdb_catch.h>
 #include <test/support/tdb_rapidcheck.h>
 
 using namespace tiledb::algorithm;
 using namespace tiledb::common;
+using namespace tiledb::sm;
+using namespace tiledb::test;
 
 namespace tiledb::algorithm {
 
@@ -50,6 +53,8 @@ using Streams = std::vector<std::vector<T>>;
  */
 template <typename T>
 struct VerifySplitPointStream {
+  std::shared_ptr<MemoryTracker> memory;
+
   Streams<T> streams;
   uint64_t which;
   MergeUnit search_bounds;
@@ -67,7 +72,11 @@ struct VerifySplitPointStream {
 
     auto cmp = std::less<T>{};
     auto result = ParallelMerge<T>::split_point_stream_bounds(
-        spans, cmp, which, search_bounds);
+        spans,
+        cmp,
+        *memory->get_resource(MemoryType::PARALLEL_MERGE_CONTROL),
+        which,
+        search_bounds);
 
     RC_ASSERT(result.ends[which] > 0);
 
@@ -95,35 +104,39 @@ struct VerifyIdentifyMergeUnit {
       spans.push_back(std::span(stream));
     }
 
+    auto memory_tracker = get_test_memory_tracker();
+    auto& memory =
+        *memory_tracker->get_resource(MemoryType::PARALLEL_MERGE_CONTROL);
+
     auto cmp = std::less<T>{};
-    auto result =
-        ParallelMerge<T>::identify_merge_unit(spans, &cmp, target_items);
-    RC_ASSERT(target_items == result->num_items());
+    auto result = ParallelMerge<T>::identify_merge_unit(
+        spans, &cmp, memory, target_items);
+    RC_ASSERT(target_items == result.num_items());
 
     for (size_t s = 0; s < streams.size(); s++) {
       if (streams[s].empty()) {
-        RC_ASSERT(result->starts[s] == 0);
-        RC_ASSERT(result->ends[s] == 0);
-      } else if (result->starts[s] == result->ends[s]) {
+        RC_ASSERT(result.starts[s] == 0);
+        RC_ASSERT(result.ends[s] == 0);
+      } else if (result.starts[s] == result.ends[s]) {
         // this stream is not used at all, its first item must exceed
         // what lies at the boundary for all other streams
         for (size_t s2 = 0; s2 < streams.size(); s2++) {
-          if (result->starts[s2] != result->ends[s2]) {
-            RC_ASSERT(streams[s][0] >= streams[s2][result->ends[s2] - 1]);
+          if (result.starts[s2] != result.ends[s2]) {
+            RC_ASSERT(streams[s][0] >= streams[s2][result.ends[s2] - 1]);
           }
         }
       } else {
         // items from this stream in range `[starts[s], ends[s])` are included
         // in the merge unit, so everything outside of the merge unit must
         // exceed it
-        auto& bound_item = streams[s][result->ends[s] - 1];
+        auto& bound_item = streams[s][result.ends[s] - 1];
         for (size_t s2 = 0; s2 < streams.size(); s2++) {
-          if (result->ends[s2] == streams[s2].size()) {
+          if (result.ends[s2] == streams[s2].size()) {
             // the entirety of `s2` is included in the merge unit,
             // we can infer no relation to `bound_item`
             continue;
           } else {
-            RC_ASSERT(bound_item <= streams[s2][result->ends[s2]]);
+            RC_ASSERT(bound_item <= streams[s2][result.ends[s2]]);
           }
         }
       }
@@ -133,16 +146,19 @@ struct VerifyIdentifyMergeUnit {
 
 template <typename T>
 struct VerifyTournamentMerge {
+  std::shared_ptr<MemoryTracker> memory_tracker;
+  // appears unused but is needed for `MergeUnit` lifetime
+
   Streams<T> streams;
-  std::shared_ptr<MergeUnit> unit;
+  MergeUnit unit;
 
   void verify() {
     auto cmp = std::less<T>{};
 
-    std::vector<T> output(unit->num_items());
+    std::vector<T> output(unit.num_items());
 
-    // SAFETY: the merge unit will begin writing at index `unit->output_start()`
-    T* output_ptr = &output[-unit->output_start()];
+    // SAFETY: the merge unit will begin writing at index `unit.output_start()`
+    T* output_ptr = &output[-unit.output_start()];
 
     std::vector<std::span<T>> spans;
     for (size_t s = 0; s < streams.size(); s++) {
@@ -160,8 +176,8 @@ struct VerifyTournamentMerge {
     for (size_t s = 0; s < streams.size(); s++) {
       inputcmp.insert(
           inputcmp.end(),
-          streams[s].begin() + unit->starts[s],
-          streams[s].begin() + unit->ends[s]);
+          streams[s].begin() + unit.starts[s],
+          streams[s].begin() + unit.ends[s]);
     }
     std::sort(inputcmp.begin(), inputcmp.end());
 
@@ -200,9 +216,12 @@ struct VerifyParallelMerge {
 
     std::vector<T> output(total_items);
 
+    auto memory_tracker = get_test_memory_tracker();
+    ParallelMergeMemoryResources resources(*memory_tracker.get());
+
     ThreadPool pool(pool_concurrency);
-    auto future =
-        parallel_merge<T, decltype(cmp)>(pool, options, spans, cmp, &output[0]);
+    auto future = parallel_merge<T, decltype(cmp)>(
+        pool, resources, options, spans, cmp, &output[0]);
 
     std::optional<uint64_t> prev_bound;
     std::optional<uint64_t> bound;
@@ -257,7 +276,8 @@ Gen<Streams<T>> streams_non_empty() {
 }
 
 template <typename T>
-Gen<MergeUnit> merge_unit(const Streams<T>& streams) {
+Gen<MergeUnit> merge_unit(
+    std::shared_ptr<MemoryTracker> memory_tracker, const Streams<T>& streams) {
   std::vector<Gen<std::pair<uint64_t, uint64_t>>> all_stream_bounds;
   for (const auto& stream : streams) {
     Gen<std::pair<uint64_t, uint64_t>> bounds = gen::apply(
@@ -279,9 +299,13 @@ Gen<MergeUnit> merge_unit(const Streams<T>& streams) {
         return bounds;
       });
 
+  auto memory =
+      memory_tracker->get_resource(MemoryType::PARALLEL_MERGE_CONTROL);
+
   return gen::map(
-      gen_stream_bounds, [](std::vector<std::pair<uint64_t, uint64_t>> bounds) {
-        MergeUnit unit;
+      gen_stream_bounds,
+      [memory](std::vector<std::pair<uint64_t, uint64_t>> bounds) {
+        MergeUnit unit(*memory);
         unit.starts.reserve(bounds.size());
         unit.ends.reserve(bounds.size());
         for (const auto& bound : bounds) {
@@ -327,11 +351,16 @@ struct Arbitrary<VerifySplitPointStream<T>> {
         }
       }
 
+      auto memory_tracker = get_test_memory_tracker();
+
       auto which = gen::elementOf(which_candidates);
-      auto search_bounds = merge_unit(streams);
+      auto search_bounds = merge_unit(memory_tracker, streams);
 
       return gen::apply(
-          [](Streams<T> streams, uint64_t which, MergeUnit search_bounds) {
+          [](std::shared_ptr<MemoryTracker> memory_tracker,
+             Streams<T> streams,
+             uint64_t which,
+             MergeUnit search_bounds) {
             for (size_t s = 0; s < streams.size(); s++) {
               if (s == which &&
                   search_bounds.starts[s] == search_bounds.ends[s]) {
@@ -344,10 +373,12 @@ struct Arbitrary<VerifySplitPointStream<T>> {
               }
             }
             return VerifySplitPointStream{
+                .memory = memory_tracker,
                 .streams = streams,
                 .which = which,
                 .search_bounds = search_bounds};
           },
+          gen::just(memory_tracker),
           gen::just(streams),
           which,
           search_bounds);
@@ -378,11 +409,14 @@ template <typename T>
 struct Arbitrary<VerifyTournamentMerge<T>> {
   static Gen<VerifyTournamentMerge<T>> arbitrary() {
     return gen::mapcat(streams<T>(), [](Streams<T> streams) {
-      auto unit = merge_unit(streams);
+      auto memory_tracker = get_test_memory_tracker();
+      auto unit = merge_unit(memory_tracker, streams);
       return gen::apply(
-          [](Streams<T> streams, MergeUnit unit) {
-            return VerifyTournamentMerge{
-                .streams = streams, .unit = std::make_shared<MergeUnit>(unit)};
+          [memory_tracker](Streams<T> streams, MergeUnit unit) {
+            return VerifyTournamentMerge<T>{
+                .memory_tracker = memory_tracker,
+                .streams = streams,
+                .unit = unit};
           },
           gen::just(streams),
           unit);
@@ -427,11 +461,11 @@ void show<VerifyTournamentMerge<uint64_t>>(
   show<decltype(instance.streams)>(instance.streams, os);
   os << "," << std::endl;
   os << "\t\"starts\": [";
-  show(instance.unit->starts, os);
+  show(instance.unit.starts, os);
   os << "]" << std::endl;
   os << "," << std::endl;
   os << "\t\"ends\": [";
-  show(instance.unit->ends, os);
+  show(instance.unit.ends, os);
   os << "]" << std::endl;
   os << "}";
 }
@@ -576,6 +610,9 @@ TEST_CASE("parallel merge example", "[algorithm][parallel_merge]") {
 
   ParallelMergeOptions options = {.parallel_factor = 4, .min_merge_items = 4};
 
+  auto memory_tracker = get_test_memory_tracker();
+  ParallelMergeMemoryResources resources(*memory_tracker.get());
+
   ThreadPool pool(4);
 
   SECTION("less") {
@@ -596,9 +633,8 @@ TEST_CASE("parallel merge example", "[algorithm][parallel_merge]") {
         std::span(streambufs[3]),
     };
 
-    std::unique_ptr<ParallelMergeFuture> future =
-        parallel_merge<uint64_t, decltype(cmp)>(
-            pool, options, streams, cmp, &output[0]);
+    auto future = parallel_merge<uint64_t, decltype(cmp)>(
+        pool, resources, options, streams, cmp, &output[0]);
 
     future->block();
 
@@ -624,9 +660,8 @@ TEST_CASE("parallel merge example", "[algorithm][parallel_merge]") {
         std::span(streambufs[3]),
     };
 
-    std::unique_ptr<ParallelMergeFuture> future =
-        parallel_merge<uint64_t, decltype(cmp)>(
-            pool, options, streams, cmp, &output[0]);
+    auto future = parallel_merge<uint64_t, decltype(cmp)>(
+        pool, resources, options, streams, cmp, &output[0]);
 
     future->block();
 
