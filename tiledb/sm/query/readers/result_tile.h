cf92e21dc3846169bb667a140c27fed515222451
Ypatia Tsavliri <ypatia@tiledb.com>
2024-12-19 10:28:04 +0200

    Fix ASAN: Destructors of base classes must be virtual

--- tiledb/sm/query/readers/result_tile.h
+++ tiledb/sm/query/readers/result_tile.h
@@ -478,7 +478,7 @@ class ResultTile {
   DISABLE_COPY_AND_COPY_ASSIGN(ResultTile);
   DISABLE_MOVE_AND_MOVE_ASSIGN(ResultTile);
 
-  /** Default destructor. */
+  /** Destructor needs to be virtual, this is a base class. */
   virtual ~ResultTile();
 
   /* ********************************* */
@@ -942,6 +942,9 @@ class ResultTileWithBitmap : public ResultTile {
 
   DISABLE_COPY_AND_COPY_ASSIGN(ResultTileWithBitmap);
   DISABLE_MOVE_AND_MOVE_ASSIGN(ResultTileWithBitmap);
+
+  /** Default destructor needs to be virtual, this is a base class. */
+  virtual ~ResultTileWithBitmap() = default;
 
  public:
   /* ********************************* */
