239f6057bae22edc2027f5e791afc071d2610beb
Ryan Roelke <ryan.roelke@tiledb.com>
2025-01-21 16:41:50 -0500

    Fix type_traits.h copyright

--- tiledb/sm/misc/type_traits.h
+++ tiledb/sm/misc/type_traits.h
@@ -5,8 +5,7 @@
  *
  * The MIT License
  *
- * @copyright Copyright (c) 2017-2024 TileDB, Inc.
- * @copyright Copyright (c) 2016 MIT and Intel Corporation
+ * @copyright Copyright (c) 2025 TileDB, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
