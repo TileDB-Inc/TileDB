c4325e451210bb45a3c5d0b9d66a7281c79dbc87
Isaiah Norton <isaiah@tiledb.io>
2025-02-20 10:55:11 -0500

    Update HISTORY for 2.27.1

--- tiledb/doxygen/source/conf.py
+++ tiledb/doxygen/source/conf.py
@@ -78,7 +78,7 @@ author = 'TileDB, Inc.'
 # The short X.Y version.
 version = '2.27'
 # The full version, including alpha/beta/rc tags.
-release = '2.27.0'
+release = '2.27.1'
 
 # Breathe extension configuration.
 doxygen_xml_dir = os.path.join(TILEDB_BUILD_DIR, 'xml/')
