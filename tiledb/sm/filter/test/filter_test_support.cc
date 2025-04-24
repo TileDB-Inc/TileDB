3eb074b89114fc80311f0f0f3928f454007718d2
Ryan Roelke <ryan@tiledb.com>
2025-04-24 12:22:04 -0400

    const FilterPipeline&

--- tiledb/sm/filter/test/filter_test_support.cc
+++ tiledb/sm/filter/test/filter_test_support.cc
@@ -114,7 +114,7 @@ void check_run_pipeline_roundtrip(
     ThreadPool& tp,
     shared_ptr<WriterTile> tile,
     std::optional<shared_ptr<WriterTile>>& offsets_tile,
-    FilterPipeline& pipeline,
+    const FilterPipeline& pipeline,
     const TileDataGenerator* test_data,
     shared_ptr<MemoryTracker> memory_tracker) {
   // Run the pipeline forward.
