09099eb2fd2ce59a5842287f509ca3fb3302ded5
Paul J. Davis <paul.davis@tiledb.com>
2025-04-25 13:32:13 -0500

    Checkpoint

--- tiledb/sm/filter/test/oxidize/wrap_cpp/src/lib.rs
+++ tiledb/sm/filter/test/oxidize/wrap_cpp/src/lib.rs
@@ -2,13 +2,13 @@
 mod ffi {
     #[namespace = "tiledb::sm"]
     unsafe extern "C++" {
-        include!("tiledb/sm/filter/filter_pipeline.h");
+        include!("../../filter_pipeline.h");
         type FilterPipeline;
     }
 
     #[namespace = "tiledb::sm::test"]
     unsafe extern "C++" {
-        include!("tiledb/sm/filter/test/oxidize.h");
+        include!("../oxidize.h");
         fn build_pipeline_65154() -> UniquePtr<FilterPipeline>;
 
         fn filter_pipeline_roundtrip(pipeline: &FilterPipeline, data: &[u8]) -> Result<()>;
