97db19f69fb190e070748f6247aecf805ba591d4
Paul J. Davis <paul.davis@tiledb.com>
2025-04-27 12:40:21 -0500

    Add another proptest
    This is a silly proptest, but I just wanted to show another example
    of adding a Rust test in isolation from setting up the build system. And
    now I'm going to redo that build system so that Rust can be selectively
    enabled at configuration time.

---
+++ tiledb/oxidized/tests/test_enumeration_rs/src/lib.rs
@@ -0,0 +1,52 @@
+use cxx::let_cxx_string;
+use proptest::prelude::*;
+use proptest::test_runner::FileFailurePersistence;
+
+#[cxx::bridge]
+mod ffi {
+    #[namespace = "tiledb::oxidized::test"]
+    extern "Rust" {
+        fn enumeration_name_prop() -> bool;
+    }
+
+    #[namespace = "tiledb::sm"]
+    unsafe extern "C++" {
+        include!("tiledb/sm/array_schema/enumeration.h");
+        include!("tiledb/sm/array_schema/test/unit_enumeration_rs.h");
+        type ConstEnumeration;
+
+        fn name(self: &ConstEnumeration) -> &CxxString;
+    }
+
+    #[namespace = "tiledb::sm::test"]
+    unsafe extern "C++" {
+        fn create_enumeration(
+            name: &CxxString,
+        ) -> Result<SharedPtr<ConstEnumeration>>;
+    }
+}
+
+pub fn prop_enumeration_name() -> impl Strategy<Value = String> {
+    proptest::string::string_regex(".*")
+        .expect("Error creating enumeration name strategy")
+}
+
+pub fn enumeration_name_prop() -> bool {
+    let config = ProptestConfig {
+        source_file: None,
+        failure_persistence: Some(Box::new(FileFailurePersistence::Off)),
+        ..Default::default()
+    };
+    proptest!(config, |(name in prop_enumeration_name())| {
+        run_enumeration_name_prop(name).expect("Error testing property.")
+    });
+
+    true
+}
+
+fn run_enumeration_name_prop(name: String) -> anyhow::Result<()> {
+    let_cxx_string!(name = name);
+    let enmr = ffi::create_enumeration(&name)?;
+    assert_eq!(name.to_str(), enmr.name().to_str());
+    Ok(())
+}
---
+++ tiledb/sm/array_schema/test/unit_enumeration_rs.cc
@@ -0,0 +1,61 @@
+/**
+ * @file tiledb/sm/array_schema/test/unit_enumertion.cc
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2022-2025 TileDB, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ * This file contains unit tests for enumerations.
+ */
+
+#include "unit_enumeration_rs.h"
+#include <test/support/tdb_catch.h>
+#include "cxxbridge/test_enumeration_rs/src/lib.rs.h"
+#include "test/support/src/mem_helpers.h"
+#include "tiledb/sm/array_schema/enumeration.h"
+
+using namespace tiledb::sm;
+
+namespace tiledb::sm::test {
+
+/**
+ * @return an Enumeration
+ */
+std::shared_ptr<ConstEnumeration> create_enumeration(const std::string& name) {
+  auto tracker = tiledb::test::create_test_memory_tracker();
+  return Enumeration::create(
+      name, Datatype::INT32, 1, false, nullptr, 0, nullptr, 0, tracker);
+}
+
+}  // namespace tiledb::sm::test
+
+/*
+ * Every attribute must appear in both attribute containers. Indices and
+ * pointers must be coherent. For example, if attribute(i).name() == s, then
+ * attribute(i) == attribute(s).
+ */
+TEST_CASE("Enumeration name", "[oxidized][enumeration][name]") {
+  tiledb::oxidized::test::enumeration_name_prop();
+}
