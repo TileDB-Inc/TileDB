1276765da79e9af04a4de5ee0b01ee099b0591b7
Ryan Roelke <ryan@tiledb.com>
2025-05-01 11:01:37 -0400

    Add stencil for converting query condition AST to datafusion expr

---
+++ tiledb/sm/query/ast/test/unit-query-to-datafusion.cc
@@ -0,0 +1,9 @@
+#include "build/tiledb/oxidize/expr.h"
+
+#include <test/support/tdb_catch.h>
+
+TEST_CASE(
+    "Query condition AST to datafusion expr",
+    "[QueryCondition][ast][datafusion]") {
+}
+ 
