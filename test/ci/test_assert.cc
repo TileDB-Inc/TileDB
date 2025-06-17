16fa68b012252591ecde2fd050f1df374af4b0ee
Ryan Roelke <ryan@tiledb.com>
2025-06-17 10:29:05 -0400

    Update mac macro and do nothing for windows

--- test/ci/test_assert.cc
+++ test/ci/test_assert.cc
@@ -73,12 +73,30 @@ TEST_CASE("CI: Test libc assertions configuration", "[ci][assertions]") {
   std::cout << "retval is " << retval << " (0x" << std::hex << retval
             << ") from " << TILEDB_PATH_TO_TRY_LIBC_ASSERT << std::endl;
 
-#ifdef TILEDB_ASSERTIONS
-  REQUIRE(
-      std::find(assert_exit_codes.begin(), assert_exit_codes.end(), retval) !=
-      assert_exit_codes.end());
+  // This is a little awkward but comes down to the fact that:
+  // 1) on windows the standard library assertions are enabled in the
+  //    debug runtime with no clear way to enable them for release
+  // 2) on windows/max the standard library assertions are toggled
+  //    by a macro, indepdently of the build configuration
+#if defined(_WIN32)
+#ifndef NDEBUG
+  const bool expectAssertFailed = true;
 #else
-  (void)assert_exit_codes;
-  REQUIRE(retval == 0);
+  const bool expectAssertFailed = false;
 #endif
+#else
+#ifdef TILEDB_ASSERTIONS
+  const bool expectAssertFailed = true;
+#else
+  const bool expectAssertFailed = false;
+#endif
+#endif
+
+  if (expectAssertFailed) {
+    REQUIRE(
+        std::find(assert_exit_codes.begin(), assert_exit_codes.end(), retval) !=
+        assert_exit_codes.end());
+  } else {
+    REQUIRE(retval == 0);
+  }
 }
