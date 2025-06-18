c6bea411c1765337f8b7c2c5c5ec95ff2b15120f
Ryan Roelke <ryan@tiledb.com>
2025-06-18 11:25:26 -0400

    Remove unit-query-to-datafusion.cc

--- tiledb/sm/query/ast/test/unit-query-to-datafusion.cc
+++
@@ -1,8 +0,0 @@
-#include "build/tiledb/oxidize/target/cxxbridge/expr/src/lib.rs.h"
-
-#include <test/support/tdb_catch.h>
-
-TEST_CASE(
-    "Query condition AST to datafusion expr",
-    "[QueryCondition][ast][datafusion]") {
-}
