161562cb1830742a71feb44b5f061bdae788ace5
Ryan Roelke <ryan@tiledb.com>
2025-06-20 12:29:20 -0400

    Stub for LogicalExpr::is_predicate

--- tiledb/oxidize/expr/src/logical_expr.rs
+++ tiledb/oxidize/expr/src/logical_expr.rs
@@ -3,9 +3,16 @@
 use std::fmt::{Display, Formatter, Result as FmtResult};
 
 use datafusion::logical_expr::Expr;
+use tiledb_cxx_interface::sm::array_schema::ArraySchema;
 
 /// Wraps a DataFusion [Expr] for passing across the FFI boundary.
 pub struct LogicalExpr(pub Expr);
+
+impl LogicalExpr {
+    pub fn is_predicate(&self, schema: &ArraySchema) -> bool {
+        todo!()
+    }
+}
 
 impl Display for LogicalExpr {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
