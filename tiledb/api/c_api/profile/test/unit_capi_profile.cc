09883e09b0b8ed1beb36d111f8d466eb76367943
Agisilaos Kounelis <kounelisagis@gmail.com>
2025-06-20 18:55:06 +0300

    Avoid throwing on missing Profile parameters in getter APIs

--- tiledb/api/c_api/profile/test/unit_capi_profile.cc
+++ tiledb/api/c_api/profile/test/unit_capi_profile.cc
@@ -201,6 +201,11 @@ TEST_CASE_METHOD(
     rc = tiledb_profile_get_param(profile, "rest.username", &value, &err);
     REQUIRE(tiledb_status(rc) == TILEDB_OK);
   }
+  SECTION("param not found") {
+    rc = tiledb_profile_get_param(profile, "does.not.exist", &value, &err);
+    REQUIRE(tiledb_status(rc) == TILEDB_OK);
+    REQUIRE(value == nullptr);
+  }
   SECTION("null param") {
     rc = tiledb_profile_get_param(profile, nullptr, &value, &err);
     REQUIRE(tiledb_status(rc) == TILEDB_ERR);
