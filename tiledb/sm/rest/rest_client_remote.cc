01ca5f7ac7f6b9fe2d074a0e76e38632a1745271
Ypatia Tsavliri <ypatia@tiledb.com>
2025-06-27 14:18:52 +0300

    Some fixes for arrays: untested

--- tiledb/sm/rest/rest_client_remote.cc
+++ tiledb/sm/rest/rest_client_remote.cc
@@ -290,8 +290,12 @@ Status RestClientRemote::post_array_schema_to_rest(
     const ArraySchema& array_schema) {
   BufferList serialized{memory_tracker_};
   auto& buff = serialized.emplace_buffer();
+
+  URI::RESTURIComponents rest_uri;
+  RETURN_NOT_OK(uri.get_rest_components(rest_legacy(), &rest_uri));
+
   RETURN_NOT_OK(serialization::array_schema_serialize(
-      array_schema, serialization_type_, buff, false));
+      array_schema, serialization_type_, buff, false, rest_uri.asset_storage));
 
   const auto creation_access_credentials_name{
       config_->get<std::string>("rest.creation_access_credentials_name")};
@@ -303,8 +307,6 @@ Status RestClientRemote::post_array_schema_to_rest(
 
   // Init curl and form the URL
   Curl curlc(logger_);
-  URI::RESTURIComponents rest_uri;
-  RETURN_NOT_OK(uri.get_rest_components(rest_legacy(), &rest_uri));
   const std::string cache_key =
       rest_uri.server_namespace + ":" + rest_uri.server_path;
   // We don't want to cache the URI used for array creation as it will
