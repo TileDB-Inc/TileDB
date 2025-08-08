a6a5337b1dfd866f36bc53bfc6094f43f3a55474
Theodore Tsirpanis <teo@tsirpanis.gr>
2025-08-08 18:37:52 +0300

    Bump vcpkg `builtin-baseline`.

--- ports/triplets/arm64-linux.cmake
+++ ports/triplets/arm64-linux.cmake
@@ -4,12 +4,11 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-# For AWS SDK. Remove after update from 1.74
-set(VCPKG_CXX_FLAGS "-Wno-error=nonnull -Wno-error=deprecated-declarations")
-set(VCPKG_C_FLAGS "-Wno-error=nonnull -Wno-error=deprecated-declarations")
+# Fix CMake 4.0 errors in ports supporting very old CMake verions.
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_POLICY_VERSION_MINIMUM=3.5")
 
-# Hide symbols in the AWS SDK. Fixes symbol collisions with other libraries
-# like arrow (https://github.com/apache/arrow/issues/42154).
-if("${PORT}" MATCHES "^aws-")
-    set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_CXX_VISIBILITY_PRESET=hidden;-DCMAKE_C_VISIBILITY_PRESET=hidden")
+# Hide symbols in spdlog. Fixes symbol collisions in downstream projects
+# (https://github.com/gabime/spdlog/pull/3322 was rejected).
+if("${PORT}" MATCHES "spdlog")
+    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_CXX_VISIBILITY_PRESET=hidden;-DCMAKE_C_VISIBILITY_PRESET=hidden")
 endif()
