bc03c27fa5d00e23c974e14342c95302ac4dca65
Ryan Roelke <ryan@tiledb.com>
2025-08-18 17:09:58 -0400

    tiledb_unit use tiledb_tracing_init

--- test/support/tdb_catch_tracing.cc
+++ test/support/tdb_catch_tracing.cc
@@ -1,25 +1,7 @@
 #ifdef HAVE_TRACING
-#include <opentelemetry/exporters/ostream/span_exporter_factory.h>
-#include <opentelemetry/sdk/trace/exporter.h>
-#include <opentelemetry/sdk/trace/processor.h>
-#include <opentelemetry/sdk/trace/simple_processor_factory.h>
-#include <opentelemetry/sdk/trace/tracer_provider_factory.h>
-#include <opentelemetry/trace/provider.h>
 #include <catch2/reporters/catch_reporter_event_listener.hpp>
 #include <catch2/reporters/catch_reporter_registrars.hpp>
-
-static void initTracing() {
-  auto exporter =
-      opentelemetry::exporter::trace::OStreamSpanExporterFactory::Create();
-  auto processor =
-      opentelemetry::sdk::trace::SimpleSpanProcessorFactory::Create(
-          std::move(exporter));
-  std::shared_ptr<opentelemetry::trace::TracerProvider> provider =
-      opentelemetry::sdk::trace::TracerProviderFactory::Create(
-          std::move(processor));
-  // set the global trace provider
-  trace_api::Provider::SetTracerProvider(provider);
-}
+#include "tiledb.h"
 
 class TracingListener : public Catch::EventListenerBase {
  public:
@@ -27,7 +9,7 @@ class TracingListener : public Catch::EventListenerBase {
 
   void testRunStarting(Catch::TestRunInfo const&) override {
     // TODO: can we use the argument to configure?
-    initTracing();
+    tiledb_tracing_init("localhost:4317");
   }
 };
 
