bb4ebaad3bea0ae59059753e362418e5902ee86b
Ryan Roelke <ryan@tiledb.com>
2025-09-22 11:14:46 -0400

    make_fragment_3d

--- test/support/rapidcheck/array_templates.h
+++ test/support/rapidcheck/array_templates.h
@@ -264,6 +264,60 @@ Gen<Fragment2D<D1, D2, Att...>> make_fragment_2d(
   });
 }
 
+template <
+    DimensionType D1,
+    DimensionType D2,
+    DimensionType D3,
+    AttributeType... Att>
+Gen<Fragment3D<D1, D2, D3, Att...>> make_fragment_3d(
+    bool allow_duplicates,
+    std::optional<Domain<D1>> d1,
+    std::optional<Domain<D2>> d2,
+    std::optional<Domain<D3>> d3) {
+  auto coord_d1 =
+      (d1.has_value() ? make_coordinate(d1.value()) : gen::arbitrary<D1>());
+  auto coord_d2 =
+      (d2.has_value() ? make_coordinate(d2.value()) : gen::arbitrary<D2>());
+  auto coord_d3 =
+      (d3.has_value() ? make_coordinate(d3.value()) : gen::arbitrary<D3>());
+
+  using Cell = std::tuple<D1, D2, D3, Att...>;
+
+  auto cell =
+      gen::tuple(coord_d1, coord_d2, coord_d3, gen::arbitrary<Att>()...);
+
+  auto uniqueCoords = [](const Cell& cell) {
+    return std::make_tuple(
+        std::get<0>(cell), std::get<1>(cell), std::get<2>(cell));
+  };
+
+  auto cells = gen::nonEmpty(
+      allow_duplicates ? gen::container<std::vector<Cell>>(cell) :
+                         gen::uniqueBy<std::vector<Cell>>(cell, uniqueCoords));
+
+  return gen::map(cells, [](std::vector<Cell> cells) {
+    std::vector<D1> coords_d1;
+    std::vector<D2> coords_d2;
+    std::vector<D3> coords_d3;
+    std::tuple<std::vector<Att>...> atts;
+
+    std::apply(
+        [&](std::vector<D1> tup_d1,
+            std::vector<D2> tup_d2,
+            std::vector<D3> tup_d3,
+            auto... tup_atts) {
+          coords_d1 = tup_d1;
+          coords_d2 = tup_d2;
+          coords_d3 = tup_d3;
+          atts = std::make_tuple(tup_atts...);
+        },
+        stdx::transpose(cells));
+
+    return Fragment3D<D1, D2, D3, Att...>{
+        std::make_tuple(coords_d1, coords_d2, coords_d3), atts};
+  });
+}
+
 void showValue(const templates::Domain<int>& domain, std::ostream& os);
 void showValue(const templates::Domain<int64_t>& domain, std::ostream& os);
 void showValue(const templates::Domain<uint64_t>& domain, std::ostream& os);
