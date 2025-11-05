4fddca79e47f8c6272f927e591a4272f7bcb1ffe
Ryan Roelke <rroelke@users.noreply.github.com>
2025-11-05 10:55:00 -0500

    Fix dense fragment domains during global order write with maximum fragment size (#5655)
    Fixes CORE-290.
    
    ## Report
    
    In CORE-290 a customer reported issues with corrupt arrays after running
    consolidation. The symptom was memory allocation errors when opening an
    array. The root cause turned out to be the consolidation itself was
    writing new fragments where the fragment domain did not match the number
    of tiles in the fragment.
    
    ## Root cause analysis
    
    Stepping through a minimal reproducer revealed that consolidation is not
    at fault at all - instead it is the global order writer
    `max_fragment_size_` field. This field, presumably meant to be used for
    consolidation only but valid for other writes, instructs the writer to
    write multiple fragments each under the requested size as necessary.
    When a write splits its data into multiple fragments this way, nothing
    need be done for sparse arrays. But dense fragment metadata relies on
    the domain to determine the number of tiles, and the global order writer
    did not update the fragment metadata domain when splitting its write
    into multiple fragments.
    
    This pull request fixes that issue by ensuring that the global order
    writer updates the fragment metadata domain to reflect what was actually
    written into that fragment.
    
    This is easier said than done. In CORE-290 I offered four ways we can
    fix this. The chosen solution:
    
    ## Design
    
    The fragment metadata domain is a bounding rectangle. This means that
    the global order writer must split the tiles of its input into fragments
    at tile boundaries which bisect the bounding rectangle into two smaller
    bounding rectangles.
    
    To do so, we add a first pass `identify_fragment_tile_boundaries` which
    returns a list of tile offsets where new fragments will begin. Upon
    finishing a fragment, we use that tile offset to determine which
    rectangle within the target subarray the fragment actually represents,
    and update the fragment metadata accordingly. We use new functions
    `is_rectangular_domain` to determine whether a `(start_tile, num_tiles)`
    pair identifies a rectangle, and `domain_tile_offset` to compute that
    rectangle.
    
    Much of the complexity comes from the usage of the global order writer
    which does happen in consolidation: multi-part writes. A user (or a
    consolidation operation) can set a domain `D` which it intends to write
    into, and then actually fill in all of the cells over multiple `submit`
    calls which stream in the cells to write. It is not required for these
    cells to be tile aligned. Because of that, and the need to write
    rectangle fragments, a single `submit` cannot always determine whether a
    tail of tiles belongs to its current fragment or must be deferred to the
    next. To get around this we keep those tiles in memory in the
    `global_write_state_` and prepend them to the user input in the next
    `submit`.
    
    ## Testing
    
    We add unit tests for `is_rectangular_domain` and `domain_tile_offset`,
    including both example tests as well as rapidcheck properties to assert
    general claims.
    
    We add tests for the global order writer to make broad claims about what
    the writer is supposed to do with respect to the `max_fragment_size`
    parameter. We add examples and a rapidcheck test to exercise these
    claims. In particular:
    - no fragment should be created larger than this size
    - two adjacent fragments must exceed that size (otherwise they could be
    one fragment)
    - fragments correctly populate the domain of the write query
    
    Since the original report originated from consolidation, we also add
    some consolidation tests **(TODO)** mimicking the customer input.
    
    ## TODO
    - [x] consolidation test which mimics the customer report
    - [x] test the scenario in the `FIXME` comment in
    `global_order_writer.cc`
    - [x] test with variable-length attributes
    - [x] more careful test of `last_tiles_` buffering which observes the
    amount of data buffered for a 3D write
    
    ---
    TYPE: BUG
    DESC: Fix dense global order writer use of `max_fragment_size_`

---
+++ tiledb/sm/tile/arithmetic.h
@@ -0,0 +1,228 @@
+/**
+ * @file   tiledb/sm/tile/arithmetic.h
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2025 TileDB, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ * This file provides template definitions for doing tile arithmetic,
+ * e.g. computing new domains based on offsets and such.
+ *
+ * Definitions:
+ *
+ * **Hyperrectangle**:
+ * The generalization of a rectangle to higher dimensions.
+ * This is a standard term from mathematical literature.
+ *
+ * **Hyperrow**:
+ * The generalization of a row to higher dimensions.
+ * This does not appear to be a standard term from mathematical literature.
+ * A row in a 2D domain is a rectangle of height 1, i.e. spanning a single
+ * coordinate of the outermost "row" dimension. So, in a higher-dimensional
+ * plane, a hyperrow is a hyperrectangle which spans a single coordinate of the
+ * outermost dimension. For example, in a 3D domain a hyperrow is a plane.
+ */
+#ifndef TILEDB_TILE_ARITHMETIC_H
+#define TILEDB_TILE_ARITHMETIC_H
+
+#include "tiledb/common/arithmetic.h"
+#include "tiledb/sm/array_schema/dimension.h"
+#include "tiledb/sm/enums/layout.h"
+#include "tiledb/sm/misc/types.h"
+#include "tiledb/type/range/range.h"
+
+namespace tiledb::sm {
+
+/**
+ * Ternary value for the result of `is_rectangular_domain`.
+ * Describes whether a `[start_tile, start_tile + num_tiles)` range
+ * over a given domain forms a rectangle.
+ */
+enum class IsRectangularDomain {
+  /** The range is not a rectangle, but extending it could create one. */
+  No,
+  /** The range is not a rectangle, and extending it can never create one. */
+  Never,
+  /** The range is a rectangle. */
+  Yes
+};
+
+/**
+ * @return true if the range `[start_tile, start_tile + num_tiles)` represents
+ * a hyper-rectangle inside `domain` with tile sizes given by `tile_extents`
+ */
+template <typename T>
+static IsRectangularDomain is_rectangular_domain(
+    Layout tile_order,
+    std::span<const T> tile_extents,
+    const sm::NDRange& domain,
+    uint64_t start_tile,
+    uint64_t num_tiles) {
+  for (uint64_t d_outer = 0; d_outer < tile_extents.size(); d_outer++) {
+    uint64_t hyperrow_num_tiles = 1;
+    for (uint64_t d_inner = d_outer + 1; d_inner < tile_extents.size();
+         d_inner++) {
+      const uint64_t d =
+          (tile_order == Layout::ROW_MAJOR ? d_inner :
+                                             tile_extents.size() - d_inner - 1);
+      const uint64_t d_inner_num_tiles =
+          sm::Dimension::tile_idx<T>(
+              domain[d].end_as<T>(), domain[d].start_as<T>(), tile_extents[d]) +
+          1;
+
+      const auto maybe = checked_arithmetic<uint64_t>::mul(
+          hyperrow_num_tiles, d_inner_num_tiles);
+      if (maybe.has_value()) {
+        hyperrow_num_tiles = maybe.value();
+      } else {
+        throw std::overflow_error(
+            "Cannot compute subrectangle of domain due to arithmetic overflow: "
+            "domain tile extents may be too large");
+      }
+    }
+
+    const uint64_t hyperrow_offset = start_tile % hyperrow_num_tiles;
+    if (hyperrow_offset + num_tiles > hyperrow_num_tiles) {
+      if (hyperrow_offset != 0) {
+        return IsRectangularDomain::Never;
+      } else if (num_tiles % hyperrow_num_tiles != 0) {
+        return IsRectangularDomain::No;
+      }
+    }
+  }
+  return IsRectangularDomain::Yes;
+}
+
+/**
+ * Compute the number of tiles per hyperrow for the given `domain` with tiles
+ * given by `tile_extents`.
+ *
+ * For D dimensions, the returned vector contains `D+1` elements.
+ * Position 0 is the number of tiles in `domain`.
+ * For dimension `d`, position `d + 1` is the number of tiles in a hyperrow of
+ * dimension `d` (and is thus always 1 for the final dimension).
+ */
+template <typename T>
+std::vector<std::optional<uint64_t>> compute_hyperrow_sizes(
+    Layout tile_order,
+    std::span<const T> tile_extents,
+    const sm::NDRange& domain) {
+  std::vector<std::optional<uint64_t>> hyperrow_sizes(
+      tile_extents.size() + 1, 1);
+  for (uint64_t di = 0; di < tile_extents.size(); di++) {
+    const uint64_t d =
+        (tile_order == Layout::ROW_MAJOR ? di : tile_extents.size() - di - 1);
+    const uint64_t d_num_tiles =
+        sm::Dimension::tile_idx<T>(
+            domain[d].end_as<T>(), domain[d].start_as<T>(), tile_extents[d]) +
+        1;
+    hyperrow_sizes[di] = d_num_tiles;
+  }
+  for (uint64_t d = tile_extents.size(); d > 0; d--) {
+    if (hyperrow_sizes[d - 1].has_value() && hyperrow_sizes[d].has_value()) {
+      hyperrow_sizes[d - 1] = checked_arithmetic<uint64_t>::mul(
+          hyperrow_sizes[d - 1].value(), hyperrow_sizes[d].value());
+    } else {
+      hyperrow_sizes[d - 1] = std::nullopt;
+    }
+  }
+
+  return hyperrow_sizes;
+}
+
+/**
+ * @return a new range which is contained the rectangle within `domain` defined
+ * by `[start_tile, start_tile + num_tiles)` for the tile sizes given by
+ * `tile_extents`. If this does not represent a valid rectangle then
+ * `std::nullopt` is returned instead.
+ */
+template <typename T>
+static std::optional<sm::NDRange> domain_tile_offset(
+    Layout tile_order,
+    std::span<const T> tile_extents,
+    const sm::NDRange& domain,
+    uint64_t start_tile,
+    uint64_t num_tiles) {
+  sm::NDRange r;
+  r.resize(tile_extents.size());
+
+  const std::vector<std::optional<uint64_t>> dimension_sizes =
+      compute_hyperrow_sizes(tile_order, tile_extents, domain);
+
+  for (uint64_t di = 0; di < tile_extents.size(); di++) {
+    const uint64_t d =
+        (tile_order == Layout::ROW_MAJOR ? di : tile_extents.size() - di - 1);
+
+    if (!dimension_sizes[di + 1].has_value()) {
+      throw std::overflow_error(
+          "Cannot compute subrectangle of domain due to arithmetic overflow: "
+          "domain tile extents may be too large");
+    }
+    const uint64_t hyperrow_num_tiles = dimension_sizes[di + 1].value();
+
+    T this_dimension_start_tile, this_dimension_end_tile;
+    if (dimension_sizes[di].has_value()) {
+      const uint64_t outer_num_tiles = dimension_sizes[di].value();
+      this_dimension_start_tile = (start_tile / hyperrow_num_tiles) %
+                                  (outer_num_tiles / hyperrow_num_tiles);
+      this_dimension_end_tile =
+          ((start_tile + num_tiles - 1) / hyperrow_num_tiles) %
+          (outer_num_tiles / hyperrow_num_tiles);
+    } else {
+      this_dimension_start_tile = start_tile / hyperrow_num_tiles;
+      this_dimension_end_tile =
+          (start_tile + num_tiles - 1) / hyperrow_num_tiles;
+    }
+
+    if (start_tile % hyperrow_num_tiles == 0) {
+      // aligned to the start of the hyperrow
+      if (num_tiles > hyperrow_num_tiles &&
+          num_tiles % hyperrow_num_tiles != 0) {
+        return std::nullopt;
+      }
+    } else {
+      // begins in the middle of the hyperrow
+      const uint64_t offset = start_tile % hyperrow_num_tiles;
+      if (offset + num_tiles > hyperrow_num_tiles) {
+        return std::nullopt;
+      }
+    }
+
+    const T start =
+        domain[d].start_as<T>() + (this_dimension_start_tile * tile_extents[d]);
+    const T end = domain[d].start_as<T>() +
+                  (this_dimension_end_tile * tile_extents[d]) +
+                  tile_extents[d] - 1;
+    r[d] = Range(
+        std::max<T>(domain[d].start_as<T>(), start),
+        std::min<T>(domain[d].end_as<T>(), end));
+  }
+
+  return r;
+}
+
+}  // namespace tiledb::sm
+
+#endif
