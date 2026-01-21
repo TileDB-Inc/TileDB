747813bbbd43fdab35ab679945901eec11c8745b
Agisilaos Kounelis <kounelisagis@gmail.com>
2026-01-21 16:25:34 +0200

    Implement fragment writer which proceeds one column at a time

---
+++ tiledb/sm/fragment/column_fragment_writer.cc
@@ -0,0 +1,337 @@
+/**
+ * @file   column_fragment_writer.cc
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2026 TileDB, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ * This file implements class ColumnFragmentWriter.
+ */
+
+#include "tiledb/sm/fragment/column_fragment_writer.h"
+
+#include "tiledb/sm/array_schema/array_schema.h"
+#include "tiledb/sm/crypto/encryption_key.h"
+#include "tiledb/sm/filesystem/vfs.h"
+#include "tiledb/sm/fragment/fragment_identifier.h"
+#include "tiledb/sm/fragment/fragment_metadata.h"
+#include "tiledb/sm/misc/constants.h"
+#include "tiledb/sm/storage_manager/context_resources.h"
+#include "tiledb/sm/tile/tile_metadata_generator.h"
+#include "tiledb/sm/tile/writer_tile_tuple.h"
+
+using namespace tiledb::common;
+
+namespace tiledb::sm {
+
+/* ********************************* */
+/*     CONSTRUCTORS & DESTRUCTORS    */
+/* ********************************* */
+
+ColumnFragmentWriter::ColumnFragmentWriter(
+    ContextResources* resources,
+    shared_ptr<const ArraySchema> array_schema,
+    const URI& fragment_uri,
+    const NDRange& non_empty_domain,
+    uint64_t tile_count)
+    : resources_(resources)
+    , array_schema_(array_schema)
+    , fragment_uri_(fragment_uri)
+    , dense_(array_schema->dense())
+    , current_tile_idx_(0)
+    , tile_num_(0) {
+  // For dense arrays, compute tile count from domain
+  if (dense_) {
+    tile_num_ = array_schema->domain().tile_num(non_empty_domain);
+  } else {
+    tile_num_ = tile_count;
+  }
+
+  // Create fragment directory structure
+  create_fragment_directory();
+
+  // Derive timestamp range from fragment URI
+  FragmentID frag_id{fragment_uri};
+  auto timestamp_range = frag_id.timestamp_range();
+
+  // Create memory tracker from resources
+  auto memory_tracker = resources->create_memory_tracker();
+
+  // Create fragment metadata
+  frag_meta_ = make_shared<FragmentMetadata>(
+      HERE(),
+      resources_,
+      array_schema_,
+      fragment_uri_,
+      timestamp_range,
+      memory_tracker,
+      dense_,
+      false,   // has_timestamps
+      false);  // has_delete_meta
+
+  // Initialize metadata with domain and tile count
+  frag_meta_->init(non_empty_domain);
+  frag_meta_->set_num_tiles(tile_num_);
+}
+
+/* ********************************* */
+/*          FIELD OPERATIONS         */
+/* ********************************* */
+
+void ColumnFragmentWriter::open_field(const std::string& name) {
+  if (!current_field_.empty()) {
+    throw ColumnFragmentWriterException(
+        "Cannot open field '" + name + "': field '" + current_field_ +
+        "' is already open");
+  }
+
+  if (!array_schema_->is_field(name)) {
+    throw ColumnFragmentWriterException(
+        "Field '" + name + "' does not exist in array schema");
+  }
+
+  current_field_ = name;
+  current_tile_idx_ = 0;
+}
+
+void ColumnFragmentWriter::write_tile(WriterTileTuple& tile) {
+  if (current_field_.empty()) {
+    throw ColumnFragmentWriterException(
+        "Cannot write tile: no field is currently open");
+  }
+
+  if (!tile.filtered_size().has_value()) {
+    throw ColumnFragmentWriterException(
+        "Cannot write tile: tile is not filtered");
+  }
+
+  if (current_tile_idx_ >= tile_num_) {
+    throw ColumnFragmentWriterException(
+        "Cannot write tile: tile count limit (" + std::to_string(tile_num_) +
+        ") reached");
+  }
+
+  const std::string& name = current_field_;
+  const bool var_size = array_schema_->var_size(name);
+  const bool nullable = array_schema_->is_nullable(name);
+
+  const auto type = array_schema_->type(name);
+  const auto is_dim = array_schema_->is_dim(name);
+  const auto cell_val_num = array_schema_->cell_val_num(name);
+  const bool has_min_max_md = TileMetadataGenerator::has_min_max_metadata(
+      type, is_dim, var_size, cell_val_num);
+  const bool has_sum_md =
+      TileMetadataGenerator::has_sum_metadata(type, var_size, cell_val_num);
+
+  // Get URIs for tile files
+  URI uri = frag_meta_->uri(name);
+  URI var_uri = var_size ? frag_meta_->var_uri(name) : URI("");
+  URI validity_uri = nullable ? frag_meta_->validity_uri(name) : URI("");
+
+  // Write fixed/offset tile
+  auto& t = var_size ? tile.offset_tile() : tile.fixed_tile();
+  resources_->vfs().write(
+      uri, t.filtered_buffer().data(), t.filtered_buffer().size());
+  frag_meta_->set_tile_offset(
+      name, current_tile_idx_, t.filtered_buffer().size());
+
+  auto null_count = tile.null_count();
+  auto cell_num = tile.cell_num();
+
+  // Write var tile if var-size
+  if (var_size) {
+    auto& t_var = tile.var_tile();
+    resources_->vfs().write(
+        var_uri,
+        t_var.filtered_buffer().data(),
+        t_var.filtered_buffer().size());
+    frag_meta_->set_tile_var_offset(
+        name, current_tile_idx_, t_var.filtered_buffer().size());
+    frag_meta_->set_tile_var_size(
+        name, current_tile_idx_, tile.var_pre_filtered_size());
+
+    if (has_min_max_md && null_count != cell_num) {
+      frag_meta_->set_tile_min_var_size(
+          name, current_tile_idx_, tile.min().size());
+      frag_meta_->set_tile_max_var_size(
+          name, current_tile_idx_, tile.max().size());
+    }
+  } else {
+    if (has_min_max_md && null_count != cell_num) {
+      frag_meta_->set_tile_min(name, current_tile_idx_, tile.min());
+      frag_meta_->set_tile_max(name, current_tile_idx_, tile.max());
+    }
+
+    if (has_sum_md) {
+      frag_meta_->set_tile_sum(name, current_tile_idx_, tile.sum());
+    }
+  }
+
+  // Write validity tile if nullable
+  if (nullable) {
+    auto& t_val = tile.validity_tile();
+    resources_->vfs().write(
+        validity_uri,
+        t_val.filtered_buffer().data(),
+        t_val.filtered_buffer().size());
+    frag_meta_->set_tile_validity_offset(
+        name, current_tile_idx_, t_val.filtered_buffer().size());
+    frag_meta_->set_tile_null_count(name, current_tile_idx_, null_count);
+  }
+
+  current_tile_idx_++;
+}
+
+void ColumnFragmentWriter::close_field() {
+  if (current_field_.empty()) {
+    throw ColumnFragmentWriterException(
+        "Cannot close field: no field is currently open");
+  }
+
+  const std::string& name = current_field_;
+  const bool var_size = array_schema_->var_size(name);
+  const bool nullable = array_schema_->is_nullable(name);
+
+  // Close the file URIs
+  URI uri = frag_meta_->uri(name);
+  throw_if_not_ok(resources_->vfs().close_file(uri));
+
+  if (var_size) {
+    URI var_uri = frag_meta_->var_uri(name);
+    throw_if_not_ok(resources_->vfs().close_file(var_uri));
+
+    // Convert min/max var sizes to offsets
+    const auto type = array_schema_->type(name);
+    const auto is_dim = array_schema_->is_dim(name);
+    const auto cell_val_num = array_schema_->cell_val_num(name);
+    if (TileMetadataGenerator::has_min_max_metadata(
+            type, is_dim, var_size, cell_val_num)) {
+      frag_meta_->convert_tile_min_max_var_sizes_to_offsets(name);
+    }
+  }
+
+  if (nullable) {
+    URI validity_uri = frag_meta_->validity_uri(name);
+    throw_if_not_ok(resources_->vfs().close_file(validity_uri));
+  }
+
+  // Verify correct number of tiles were written
+  if (current_tile_idx_ != tile_num_) {
+    throw ColumnFragmentWriterException(
+        "Field '" + name + "' has " + std::to_string(current_tile_idx_) +
+        " tiles but expected " + std::to_string(tile_num_));
+  }
+
+  current_field_.clear();
+  current_tile_idx_ = 0;
+}
+
+/* ********************************* */
+/*       FRAGMENT-LEVEL OPERATIONS   */
+/* ********************************* */
+
+void ColumnFragmentWriter::finalize(const EncryptionKey& encryption_key) {
+  if (!current_field_.empty()) {
+    throw ColumnFragmentWriterException(
+        "Cannot finalize: field '" + current_field_ + "' is still open");
+  }
+
+  if (!dense_) {
+    throw ColumnFragmentWriterException(
+        "Cannot finalize sparse array without MBRs");
+  }
+
+  finalize_internal(encryption_key);
+}
+
+void ColumnFragmentWriter::finalize(
+    const EncryptionKey& encryption_key, const std::vector<NDRange>& mbrs) {
+  if (!current_field_.empty()) {
+    throw ColumnFragmentWriterException(
+        "Cannot finalize: field '" + current_field_ + "' is still open");
+  }
+
+  if (dense_) {
+    throw ColumnFragmentWriterException("Dense arrays should not provide MBRs");
+  }
+
+  if (mbrs.size() != tile_num_) {
+    throw ColumnFragmentWriterException(
+        "Sparse array requires " + std::to_string(tile_num_) +
+        " MBRs but got " + std::to_string(mbrs.size()));
+  }
+
+  for (uint64_t i = 0; i < mbrs.size(); ++i) {
+    frag_meta_->set_mbr(i, mbrs[i]);
+  }
+
+  finalize_internal(encryption_key);
+}
+
+void ColumnFragmentWriter::finalize_internal(
+    const EncryptionKey& encryption_key) {
+  frag_meta_->compute_fragment_min_max_sum_null_count();
+  frag_meta_->store(encryption_key);
+
+  // Create commit file
+  FragmentID frag_id{fragment_uri_};
+  URI commit_uri;
+  if (frag_id.array_format_version() >= 12) {
+    commit_uri = array_schema_->array_uri()
+                     .join_path(constants::array_commits_dir_name)
+                     .join_path(frag_id.name() + constants::write_file_suffix);
+  } else {
+    commit_uri = URI(fragment_uri_.to_string() + constants::ok_file_suffix);
+  }
+  resources_->vfs().touch(commit_uri);
+}
+
+/* ********************************* */
+/*             ACCESSORS             */
+/* ********************************* */
+
+const URI& ColumnFragmentWriter::fragment_uri() const {
+  return fragment_uri_;
+}
+
+shared_ptr<FragmentMetadata> ColumnFragmentWriter::fragment_metadata() const {
+  return frag_meta_;
+}
+
+/* ********************************* */
+/*         PRIVATE METHODS           */
+/* ********************************* */
+
+void ColumnFragmentWriter::create_fragment_directory() {
+  const URI& array_uri = array_schema_->array_uri();
+
+  resources_->vfs().create_dir(
+      array_uri.join_path(constants::array_fragments_dir_name));
+  resources_->vfs().create_dir(fragment_uri_);
+  resources_->vfs().create_dir(
+      array_uri.join_path(constants::array_commits_dir_name));
+}
+
+}  // namespace tiledb::sm
---
+++ tiledb/sm/fragment/column_fragment_writer.h
@@ -0,0 +1,211 @@
+/**
+ * @file   column_fragment_writer.h
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2026 TileDB, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ * This file defines class ColumnFragmentWriter, which writes one field
+ * (column) at a time to a fragment.
+ */
+
+#ifndef TILEDB_COLUMN_FRAGMENT_WRITER_H
+#define TILEDB_COLUMN_FRAGMENT_WRITER_H
+
+#include <string>
+#include <vector>
+
+#include "tiledb/common/common.h"
+#include "tiledb/sm/filesystem/uri.h"
+#include "tiledb/sm/misc/types.h"
+
+using namespace tiledb::common;
+
+namespace tiledb::sm {
+
+class ArraySchema;
+class ContextResources;
+class EncryptionKey;
+class FragmentMetadata;
+class WriterTileTuple;
+
+class ColumnFragmentWriterException : public StatusException {
+ public:
+  explicit ColumnFragmentWriterException(const std::string& message)
+      : StatusException("ColumnFragmentWriter", message) {
+  }
+};
+
+/**
+ * A fragment writer that writes one field (column) at a time.
+ *
+ * Usage:
+ *   1. Create a ColumnFragmentWriter with domain and tile count
+ *   2. For each field:
+ *      a. Call open_field(name)
+ *      b. Call write_tile() for each pre-filtered tile
+ *      c. Call close_field()
+ *   3. Call finalize(key) for dense or finalize(key, mbrs) for sparse
+ */
+class ColumnFragmentWriter {
+ public:
+  /* ********************************* */
+  /*     CONSTRUCTORS & DESTRUCTORS    */
+  /* ********************************* */
+
+  /**
+   * Constructor. Creates fragment directory and FragmentMetadata.
+   *
+   * Derived automatically:
+   * - timestamp_range from fragment_uri
+   * - memory_tracker from resources
+   * - dense/sparse from array_schema
+   * - tile_count from non_empty_domain (dense only)
+   *
+   * @param resources A context resources instance.
+   * @param array_schema The schema of the array the fragment belongs to.
+   * @param fragment_uri The fragment URI (must contain valid timestamps).
+   * @param non_empty_domain The non-empty domain for this fragment.
+   * @param tile_count Number of tiles (required for sparse, ignored for dense).
+   */
+  ColumnFragmentWriter(
+      ContextResources* resources,
+      shared_ptr<const ArraySchema> array_schema,
+      const URI& fragment_uri,
+      const NDRange& non_empty_domain,
+      uint64_t tile_count = 0);
+
+  ~ColumnFragmentWriter() = default;
+
+  DISABLE_COPY_AND_COPY_ASSIGN(ColumnFragmentWriter);
+  DISABLE_MOVE_AND_MOVE_ASSIGN(ColumnFragmentWriter);
+
+  /* ********************************* */
+  /*          FIELD OPERATIONS         */
+  /* ********************************* */
+
+  /**
+   * Opens a field for writing. Must be called before write_tile().
+   *
+   * @param name The name of the field (attribute or dimension).
+   * @throws ColumnFragmentWriterException if field doesn't exist in schema,
+   *         or if another field is already open.
+   */
+  void open_field(const std::string& name);
+
+  /**
+   * Writes a pre-filtered tile for the currently open field.
+   *
+   * @param tile The tile to write. Must already be filtered
+   *        (tile.filtered_size().has_value() must be true).
+   * @throws ColumnFragmentWriterException if no field is open,
+   *         tile is not filtered, or tile count limit reached.
+   */
+  void write_tile(WriterTileTuple& tile);
+
+  /**
+   * Closes the currently open field. Flushes file buffers.
+   *
+   * @throws ColumnFragmentWriterException if no field is open.
+   */
+  void close_field();
+
+  /* ********************************* */
+  /*       FRAGMENT-LEVEL OPERATIONS   */
+  /* ********************************* */
+
+  /**
+   * Finalizes a dense fragment. Stores metadata and creates commit file.
+   *
+   * @param encryption_key The encryption key for storing metadata.
+   * @throws ColumnFragmentWriterException if a field is still open,
+   *         or if this is a sparse array.
+   */
+  void finalize(const EncryptionKey& encryption_key);
+
+  /**
+   * Finalizes a sparse fragment. Stores metadata and creates commit file.
+   *
+   * @param encryption_key The encryption key for storing metadata.
+   * @param mbrs MBRs for sparse arrays (one per tile).
+   * @throws ColumnFragmentWriterException if a field is still open,
+   *         if this is a dense array, or if MBR count doesn't match tile count.
+   */
+  void finalize(
+      const EncryptionKey& encryption_key, const std::vector<NDRange>& mbrs);
+
+  /* ********************************* */
+  /*             ACCESSORS             */
+  /* ********************************* */
+
+  /** Returns the fragment URI. */
+  const URI& fragment_uri() const;
+
+  /** Returns the fragment metadata. */
+  shared_ptr<FragmentMetadata> fragment_metadata() const;
+
+ private:
+  /* ********************************* */
+  /*         PRIVATE METHODS           */
+  /* ********************************* */
+
+  /** Creates the fragment directory structure. */
+  void create_fragment_directory();
+
+  /** Internal finalize implementation. */
+  void finalize_internal(const EncryptionKey& encryption_key);
+
+  /* ********************************* */
+  /*        PRIVATE ATTRIBUTES         */
+  /* ********************************* */
+
+  /** The context resources. */
+  ContextResources* resources_;
+
+  /** The array schema. */
+  shared_ptr<const ArraySchema> array_schema_;
+
+  /** The fragment URI. */
+  URI fragment_uri_;
+
+  /** The fragment metadata. */
+  shared_ptr<FragmentMetadata> frag_meta_;
+
+  /** Whether this is a dense fragment. */
+  bool dense_;
+
+  /** Currently open field name (empty if no field is open). */
+  std::string current_field_;
+
+  /** Current tile index for the open field. */
+  uint64_t current_tile_idx_;
+
+  /** Number of tiles to be written. */
+  uint64_t tile_num_;
+};
+
+}  // namespace tiledb::sm
+
+#endif  // TILEDB_COLUMN_FRAGMENT_WRITER_H
