97b32da5e10fca73ef5a815d2cedba96a09c3b1c
Ryan Roelke <ryan@tiledb.com>
2026-01-26 11:26:52 -0500

    Remove tiledb-rest.capnp.{c++,h} from version control, generate in build dir

--- tiledb/sm/serialization/capnp_utils.h
+++ tiledb/sm/serialization/capnp_utils.h
@@ -37,7 +37,7 @@
 
 #include <capnp/compat/json.h>
 
-#include "tiledb-rest.capnp.h"
+#include "tiledb/sm/serialization/tiledb-rest.capnp.h"
 
 #include "tiledb/common/heap_memory.h"
 #include "tiledb/common/logger_public.h"
